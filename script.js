4549e40929c2387e38d0cdd3b60a246acadd1392
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-11 13:17:23 -0300

    finalizei o desafio 5

---
+++ script.js
@@ -0,0 +1,62 @@
+window.onload = function(){
+  const input = document.querySelector('#texto-tarefa');
+
+  const btn = document.querySelector('#criar-tarefa');
+
+  const ol = document.querySelector('#lista-tarefas');
+
+  // local storage
+  const storage = {
+    index: function(){
+      let todoList = JSON.parse(localStorage.getItem('todo-list'));
+
+      return todoList;
+    },
+
+    insert: function(todoList){
+      localStorage.setItem('todo-list', JSON.stringify(todoList));
+    },
+
+    delete: function(id){
+      localStorage.removeItem(todoList.id)
+    }
+  }
+
+  const todoList = storage.index() ? storage.index() : [];
+
+  const cleanInput = function(){
+    input.value = null;
+  }
+
+  // verifica se o li com id ja foi splitado
+  const verifySplited = function(list){
+    let li = document.querySelector(`#${list}`);
+    return li ? true : false;
+  }
+  
+  // função factory que carrega conteudo da lista assim que é atualizado a página
+  const automaticSplitTodoList = function(){
+    let todoList = storage.index();
+
+    if(todoList){
+      for(let index = 0; index < todoList.length; index += 1){
+        let list = document.createElement('li');
+        list.id = `${todoList[index]}`;
+        list.innerText = todoList[index];
+        if(!verifySplited(todoList[index])){
+          ol.appendChild(list);
+        }
+      }
+    }
+  }
+
+  const insertTodo = function(){
+    todoList.push(input.value);
+    storage.insert(todoList);
+    cleanInput();
+    automaticSplitTodoList();
+  }
+
+  btn.addEventListener('click', insertTodo);
+  automaticSplitTodoList();
+}
