79757458ce64e7a964f6ebdc963d6cf78e365447
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-11 14:47:55 -0300

    finalizeei o desafio 6, 7, e 8

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
   const input = document.querySelector('#texto-tarefa');
 
   const btn = document.querySelector('#criar-tarefa');
@@ -7,50 +7,67 @@ window.onload = function(){
 
   // local storage
   const storage = {
-    index: function(){
+    index: function () {
       let todoList = JSON.parse(localStorage.getItem('todo-list'));
 
       return todoList;
     },
 
-    insert: function(todoList){
+    insert: function (todoList) {
       localStorage.setItem('todo-list', JSON.stringify(todoList));
     },
 
-    delete: function(id){
+    delete: function (id) {
       localStorage.removeItem(todoList.id)
     }
   }
 
   const todoList = storage.index() ? storage.index() : [];
 
-  const cleanInput = function(){
+  const cleanInput = function () {
     input.value = null;
   }
 
   // verifica se o li com id ja foi splitado
-  const verifySplited = function(list){
+  const verifySplited = function (list) {
     let li = document.querySelector(`#${list}`);
     return li ? true : false;
   }
+
+  function splitBG(positionLi){
+    let li = document.querySelectorAll('li');
+      
+    li[positionLi].addEventListener("click", function (event) {
+      let li = event.target;
+      li.style.backgroundColor = 'rgb(128,128,128)';
   
+      for (let index = 0; index < todoList.length; index += 1) {
+        let list = document.querySelectorAll('li');
+  
+        if (list[index].id != li.id) list[index].style.backgroundColor = null;
+      }
+    });
+  }
+
   // função factory que carrega conteudo da lista assim que é atualizado a página
-  const automaticSplitTodoList = function(){
+  function automaticSplitTodoList() {
     let todoList = storage.index();
 
-    if(todoList){
-      for(let index = 0; index < todoList.length; index += 1){
+    if (todoList) {
+      for (let index = 0; index < todoList.length; index += 1) {
         let list = document.createElement('li');
         list.id = `${todoList[index]}`;
         list.innerText = todoList[index];
-        if(!verifySplited(todoList[index])){
+
+        if (!verifySplited(todoList[index])) {
           ol.appendChild(list);
         }
+        splitBG(index);
       }
     }
   }
 
-  const insertTodo = function(){
+  function insertTodo() {
     todoList.push(input.value);
     storage.insert(todoList);
     cleanInput();
