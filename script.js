e2a5c37f082fd547fb0814e8aa06a90a22a39d45
Bruno L Nogueira <nugobruno@gmail.com>
2020-09-12 14:13:29 -0300

    Selector

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 let inputTask = document.querySelector("#texto-tarefa");
 let inputTaskBtn = document.querySelector("#criar-tarefa");
 let orderedList = document.querySelector("#lista-tarefas");
+let indexTask = 0;
 
 inputTaskBtn.addEventListener("click", function() {
-  let list = document.createElement("li");
-  orderedList.appendChild(list);
-  list.className = "item-list";
+  indexTask += 1;
+  let id = "task-li" + indexTask;
+  createLi(orderedList, id);
 
-  list.innerHTML += inputTask.value;
+  let setTaskId = "#task-li" + indexTask.toString();
+  let taskLi = document.querySelector(setTaskId);
+  taskLi.innerHTML = inputTask.value;
   inputTask.value = "";
+
+  taskLi.addEventListener("click", function() {
+    selectTask(orderedList, setTaskId);
+  })
 })
+
+function selectTask(where, id) {
+  let taskStyle = document.createElement("style");
+  where.appendChild(taskStyle);
+  taskStyle.innerHTML = id + " { background-color: rgb(128, 128, 128) }";
+}
+
+function createLi(where, id) {
+  let list = document.createElement("li");
+  where.appendChild(list);
+  list.className = "item-list";
+  list.id = id;
+}
