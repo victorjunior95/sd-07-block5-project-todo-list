f8e47ded6cbadd51074a9aa79cdd9ffd5e77c9ff
Lorena Góes <goesloren@gmail.com>
2020-09-12 16:38:47 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 let header = document.getElementsByTagName('header')[0];
 let explanation = document.getElementById('funcionamento');
 let textBox = document.getElementById('texto-tarefa');
+let list = document.getElementsByClassName('list')[0];
+let item = document.getElementsByClassName('item')[0];
+
+// 2) Define the functions:
+
+// To show the text of the box in the list:
+
+function writeList () {
+  item.innerHTML = textBox.value;
+  list.appendChild(item);
+}
