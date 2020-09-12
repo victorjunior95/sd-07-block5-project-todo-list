7f534b55651fedff10db7ee9407f9af0f6c52f64
Lorena Góes <goesloren@gmail.com>
2020-09-12 15:10:42 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1)Define the variables:
+
+let header = document.getElementsByTagName('header')[0];
+let explanation = document.getElementById('funcionamento');
+let textBox = document.getElementById('texto-tarefa');
