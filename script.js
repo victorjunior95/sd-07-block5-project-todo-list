b37112806a1bcee814ee41310f28030524b782d1
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-12 21:31:47 -0300

    Requirement Bonus 12

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const buttonAddTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const buttonRemoveAll= document.getElementById('apaga-tudo');
 const buttonRemoveDone= document.getElementById('remover-finalizados');
+const buttonSaveTaskList= document.getElementById('salvar-tarefas');
 
-buttonAddTask.addEventListener('click',addTask);
+buttonAddTask.addEventListener('click',clickAddTask);
 buttonRemoveAll.addEventListener('click',removeAll);
 buttonRemoveDone.addEventListener('click',removeAllDone);
+buttonSaveTaskList.addEventListener('click',saveTaskList);
 
 // function check input
 function checkInput(inputElement) {
@@ -18,14 +20,23 @@ function checkInput(inputElement) {
   }
 }
 
+// function to clickAddTask
+function clickAddTask() {
+  addTask(inputTask.value,false);
+}
+
 // function add task
-function addTask() {
+function addTask(taskName, completed) {
   // check input is not empty
-  if (checkInput(inputTask)) {
+  if (checkInput(taskName)) {
     // create a list item
     const listItem = document.createElement('li');
-    // get text from input
-    listItem.innerText = inputTask.value;
+    // fill text from parameter
+    listItem.innerText = taskName;
+    // check if task is done
+    if (completed) {
+      addClassName(listItem,' completed');
+    }
     // add event select - one click
     listItem.addEventListener('click', selectListItem);
     // add event select - double click
@@ -87,3 +98,43 @@ function removeAllDone() {
     taskList.removeChild(listItemsDone[index]);
   }
 }
+
+// fucntion to save task list
+function saveTaskList() {
+  const itemsList = [];
+  // get all list items
+  const allListItem = document.querySelectorAll('li');
+  // for each item at task list
+  for (let index = 0; index < allListItem.length; index += 1) {
+    // get task text
+    const task = allListItem[index].innerText;
+    // get task completed
+    let completed = false;
+    if (allListItem[index].className.indexOf('completed') >= 0) {
+      completed = true;
+    }
+    // create the item list
+    const itemList = {
+      task: task,
+      completed: completed
+    };
+    // push item list on list
+    itemsList.push(itemList);
+  }
+  // save item list like tasklist
+  localStorage.setItem('taskList', JSON.stringify(itemsList));
+}
+
+// function to load the tasklist
+function loadTaskList() {
+  let itemsList = [];
+  // get the tasklist on storage
+  itemsList = JSON.parse(localStorage.getItem("taskList"));
+  // for each item at itemsList
+  for (let index = 0; index < itemsList.length; index += 1) {
+    addTask(itemsList[index].task, itemsList[index].completed);
+  }
+}
+
+// load list from storage
+loadTaskList();
