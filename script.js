8a643ec23aeb04004ac6ab6be0d6dd8ac808ab0d
Carol Bezerra <carol.arbe@gmail.com>
2020-09-13 16:50:38 -0300

    Adiantando Requisito 7

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ document.querySelector('#criar-tarefa').addEventListener('click', function () {
   document.querySelector('ol').appendChild(newTask);
   newTask.innerText = document.querySelector('input').value;
   document.querySelector('input').value = '';
+  newTask.addEventListener('click', function () {
+    newTask.style.backgroundColor = 'rgb(128, 128, 128)';
+  })
 });
+
+
+
+
+
+
+
 
 document.querySelector('#apaga-tudo').addEventListener('click', function () {
   const onlyOl = document.querySelector('ol');
   while (onlyOl.firstChild) { // para remover todos os li
     onlyOl.removeChild(onlyOl.firstChild);
-  };
-})
+  }
+});
+
+
