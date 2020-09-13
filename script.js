2ac2acc5fead152c395bd0287ad512c26feca955
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-12 21:07:50 -0300

    permite selecionar items e marcá-los como concluídos

--- script.js
+++ script.js
@@ -5,12 +5,31 @@ const buttonClearAll = document.getElementById('apaga-tudo');
 const buttonClearDone = document.getElementById('remover-finalizados');
 
 function addTask() {
-  if (inputText.value === '') return alert('Digite o texto antes de adicionar à lista!');
-
+  if (inputText.value === '') {
+    return alert('Digite o texto antes de adicionar à lista!');
+  }
   const li = document.createElement('li');
   li.textContent = inputText.value;
+  li.addEventListener('click', selectItem);
+  li.addEventListener('dblclick', markItem);
   taskList.appendChild(li);
   inputText.value = '';
+  inputText.focus();
+}
+
+function selectItem(event) {
+  for (const element of document.getElementsByTagName('li')) {
+    if (element.classList[0] === 'selected') {
+      element.classList.toggle('selected');
+    } else if (element.classList[1] === 'selected') {
+      element.classList.toggle('selected');
+    }
+  }
+  event.target.classList.toggle('selected');
+}
+
+function markItem(event) {
+  event.target.classList.toggle('completed');
 }
 
 buttonAddTask.addEventListener('click', addTask);
