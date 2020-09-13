f0baf1a9bbae7d42da6976dc523ad7a07342d8ed
Vitor Amorims <vtamorims@gmail.com>
2020-09-13 17:45:21 -0300

    Estrutura todo list

---
+++ script.js
@@ -0,0 +1,11 @@
+//os itens da lista **não tem** o estilo CSS `background-color: rgb(128, 128, 128)`
+//Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo.
+
+//### 9 - Ao clicar duas vezes em um item, ele deverá ser riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item.
+
+//- Será verificado que a ação pedida é disparada mediante duplo clique no elemento da lista e que os elementos da lista completos tem em si a classe `completed` e a propriedade `text-decoration` com o valor `line-through solid rgb(0, 0, 0)`
+
+
+//id `remover-finalizados` elementos completos
+
+//Habitue-se a pensar nos casos especiais ao construir programas. O que acontece se o usuário tentar mover o primeiro item para cima ou o último para baixo?
