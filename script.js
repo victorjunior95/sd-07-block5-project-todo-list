21d90cbf81ec37f6f1937092d13b22808d2a7a44
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-12 21:55:31 -0300

    Adicionando função para marcar o item selecionado e desmarcar o outro

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ const textInput = document.querySelector('#texto-tarefa');
 const buttonMake = document.querySelector('#criar-tarefa');
 const todoList = document.querySelector('#lista-tarefas');
 
+function removeOldSelected() {
+  const itensArray = todoList.children;
+  for (let index = 0; index < itensArray.length; index += 1) {
+    itensArray[index].style.backgroundColor = 'white';
+  }
+};
+
 buttonMake.addEventListener('click', function () {
   const liElement = document.createElement('li');
   liElement.innerText = textInput.value;
+  liElement.className = 'lista-item';
   todoList.appendChild(liElement);
   textInput.value = '';
-})
+});
+
+todoList.addEventListener('click', function (event) {
+  removeOldSelected();
+  if (event.target.className === 'lista-item') {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+});
