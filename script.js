b2c682e1e9841ae8b495452e157debfd15fc65fb
Carol Bezerra <carol.arbe@gmail.com>
2020-09-13 16:19:27 -0300

    Adiantando Requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-document.querySelector('button').addEventListener('click', function () {
-    const newTask = document.createElement('li');
-    document.querySelector('ol').appendChild(newTask);
-    newTask.innerText = document.querySelector('input').value;
-    document.querySelector('input').value = "";
+document.querySelector('#criar-tarefa').addEventListener('click', function () {
+  const newTask = document.createElement('li');
+  document.querySelector('ol').appendChild(newTask);
+  newTask.innerText = document.querySelector('input').value;
+  document.querySelector('input').value = '';
+});
+
+document.querySelector('#apaga-tudo').addEventListener('click', function () {
+  const onlyOl = document.querySelector('ol');
+  while (onlyOl.firstChild) { // para remover todos os li
+    onlyOl.removeChild(onlyOl.firstChild);
+  };
 })
