f841e4aacf47a36044c1590fe230a877839d9be2
Carlos Souza <carlos.pessoal@hotmail.com>
2020-09-13 13:38:00 -0300

    Sexto e sétimo requisito

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-document.body.onload = function() {
-    document.querySelector('#criar-tarefa').addEventListener('click', addToDo);
-};
-
 function addToDo() {
-    const input = document.querySelector('#texto-tarefa');
-    const ol = document.querySelector('ol');
-    const li = document.createElement('li');
-    li.innerText = input.value;
-    ol.appendChild(li);
-    input.value = '';
+  const input = document.querySelector("#texto-tarefa");
+  const ol = document.querySelector("ol");
+  const li = document.createElement("li");
+  li.innerText = input.value;
+  ol.appendChild(li);
+  input.value = "";
+  li.addEventListener('click', () => selectLi(li));
 }
+
+function selectLi(selectedLi) {
+  document.querySelectorAll('li').forEach(li => {
+    if (selectedLi === li) {
+      li.classList.toggle('li-clicked');
+    } else {
+      if (li.classList.contains('li-clicked')) {
+        li.classList.remove('li-clicked');
+      }
+    }
+  })
+}
+
+document.body.onload = function () {
+  document.querySelector("#criar-tarefa").addEventListener("click", addToDo);
+};
