46952646fbd36a925dfbd9aeb9afecfcef42e467
samantabelow <samanta.below@gmail.com>
2020-09-13 16:30:15 -0300

    Deixando cinza item selecionado

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ botaoCriarTarefa.addEventListener("click", function() {
     let novaTarefa = document.createElement("li");
     novaTarefa.innerHTML = inputTarefa;
     document.getElementById('lista-tarefas').appendChild(novaTarefa);
-    inputTarefa.innerHTML = '';
+    document.getElementById('texto-tarefa').value = '';
+    novaTarefa.addEventListener("click", function() {
+        document.querySelectorAll('li').forEach(makeBackgroundColorWhite);
+        novaTarefa.style.backgroundColor = "rgb(128, 128, 128)";
+    });
 });
 
 let botaoApagaTudo = document.getElementById('apaga-tudo');
@@ -12,3 +16,6 @@ botaoApagaTudo.addEventListener("click", function() {
     document.getElementById('lista-tarefas').innerHTML = '';
 });
 
+function makeBackgroundColorWhite(item) {
+    item.style.backgroundColor = "white";
+}
