810dd08563cf08767060f2f5ab22b3550f74e42a
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-12 21:46:12 -0300

    checando erros code climate

--- script.js
+++ script.js
@@ -10,31 +10,31 @@ function selecionado() {
 }
 
 function concluido() {
-  this.classList.remove('selected')
+  this.classList.remove('selected');
   this.classList.add('completed');
 }
 
 criaItem.addEventListener('click', function criaTarefa() {
-  let itemLista = document.createElement('li');
+  const itemLista = document.createElement('li');
   itemLista.innerText = textoTarefa.value;
   itemLista.addEventListener('click', selecionado);
   itemLista.addEventListener('dblclick', concluido);
   lista.appendChild(itemLista);
   textoTarefa.value = '';
-})
+});
 
-apagarLista.addEventListener('click', function apagaLista(){
+apagarLista.addEventListener('click', function apagaLista() {
   const itensDaLista = document.querySelectorAll('li');
   for (let i = 0; i < itensDaLista.length; i += 1) {
-    let remover = itensDaLista[i];
+    const remover = itensDaLista[i];
     lista.removeChild(remover);
   }
-})
+});
 
 apagaFinalizados.addEventListener('click', function apagaConcluidos() {
   const concluidos = document.querySelectorAll('.completed');
-  for(let j = 0; j < concluidos.length; j += 1){
+  for (let j = 0; j < concluidos.length; j += 1) {
     let concluidoRemovido = concluidos[j];
     lista.removeChild(concluidoRemovido);
   }
-})
+});
