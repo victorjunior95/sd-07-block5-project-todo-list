a9e78802646114fab13cb172291d6d0e8967c754
samantabelow <samanta.below@gmail.com>
2020-09-13 15:46:16 -0300

    Botão apaga tudo funcionando

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let botaoCriarTarefa = document.getElementById("criar-tarefa");
-
+let botaoCriarTarefa = document.getElementById('criar-tarefa');
 botaoCriarTarefa.addEventListener("click", function() {
-    let inputTarefa = document.getElementById("texto-tarefa").value;
+    let inputTarefa = document.getElementById('texto-tarefa').value;
     let novaTarefa = document.createElement("li");
     novaTarefa.innerHTML = inputTarefa;
-    document.getElementById("lista-tarefas").appendChild(novaTarefa);
+    document.getElementById('lista-tarefas').appendChild(novaTarefa);
+});
+
+let botaoApagaTudo = document.getElementById('apaga-tudo');
+botaoApagaTudo.addEventListener("click", function() {
+    document.getElementById('lista-tarefas').innerHTML = '';
 });
