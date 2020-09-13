7b413ff8f5be2424d54e6b45fadb601c999c15ac
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-13 19:18:50 -0300

    adicionando cor de fundo para item da lista clicado

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+function focusedItem(event) {
+  let liItem = event.currentTarget;
+  liItem.classList.add('clicked');
+}
+
 function createListItem() {
-    const item = document.createElement('li');
-    const inputText = document.getElementById('texto-tarefa');
-    const orderedList = document.getElementById('lista-tarefas');
-    orderedList.appendChild(item);
-    item.innerText = inputText.value;
+  const item = document.createElement('li');
+  const inputText = document.getElementById('texto-tarefa');
+  const orderedList = document.getElementById('lista-tarefas');
+  orderedList.appendChild(item);
+  item.innerText = inputText.value;
+  item.addEventListener('click', focusedItem);
 }
 
 let buttonAddItem = document.getElementById('criar-tarefa');
