7ed6e09d1a7d78729f62b213596d28a428a94913
Carol Bezerra <carol.arbe@gmail.com>
2020-09-13 17:39:53 -0300

    Adiantando Requisito 11

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 document.querySelector('#criar-tarefa').addEventListener('click', function () {
-  const newTask = document.createElement('li');
-  document.querySelector('ol').appendChild(newTask);
-  newTask.innerText = document.querySelector('input').value;
-  document.querySelector('input').value = '';
+  const newTask = document.createElement('li'); // criando li
+  document.querySelector('ol').appendChild(newTask); // linkando o li ao ol
+  newTask.innerText = document.querySelector('input').value; // escrevendo no li
+  document.querySelector('input').value = ''; // limpando o input
+
   newTask.addEventListener('click', function () {
     newTask.style.backgroundColor = 'rgb(128, 128, 128)';
   });
+  
   newTask.addEventListener('dblclick', function () {
-    if (newTask.classList.contains('completed')) {
-      newTask.classList.remove('completed')
+    if (newTask.classList.contains('completed')) { // saber se contém a classe especificada
+      newTask.classList.remove('completed'); // se sim, remove
     } else {
-      newTask.classList.add('completed')
+      newTask.classList.add('completed'); // se não, adiciona
     }
   });
 });
 
 
-
-
-
-
-
+document.querySelector('#remover-finalizados').addEventListener('click', function () {
+  const arrayCompleted = [];
+  arrayCompleted.push(document.querySelector('.completed'))
+  while (arrayCompleted.length != 0) {
+    document.querySelector('ol').removeChild(document.querySelector('.completed'));
+  }
+})
 
 document.querySelector('#apaga-tudo').addEventListener('click', function () {
   const onlyOl = document.querySelector('ol');
@@ -28,5 +32,3 @@ document.querySelector('#apaga-tudo').addEventListener('click', function () {
     onlyOl.removeChild(onlyOl.firstChild);
   }
 });
-
-
