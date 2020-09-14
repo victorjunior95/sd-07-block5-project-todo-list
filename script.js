5f1759ee82625900da15971e2d4d2956690f26c3
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-14 16:54:12 -0300

    Requisito 7 mudando cor de fundo dos  itens

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const add = document.querySelector('#criar-tarefa')
 const assignment = document.querySelector('#texto-tarefa')
 
 add.addEventListener('click', function () {
-  let iten1 = assignment.value
-  let list = document.querySelector('#lista-tarefas').innerHTML+= "<li>"+iten1+"</li>"
-  assignment.value =""
-      })
+    let iten1 = assignment.value
+    let list = document.querySelector('#lista-tarefas').innerHTML += `<li class="itens">${iten1}</li>`
+    assignment.value = ""
+
+const listItens = document.querySelectorAll('.itens')
+for ( let index = 0; index < listItens.length; index += 1){
+    const iten = listItens[index]
+    iten.addEventListener('click' ,function(){
+         iten.style.backgroundColor = "rgb(128, 128, 128)"
+    })
+}
+})
