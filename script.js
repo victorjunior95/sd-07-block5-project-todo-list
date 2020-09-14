cc2196ba348f896e085b744343b689fbb39a1503
Brune Miguel <bstfreire@gmail.com>
2020-09-14 18:15:00 -0300

    Requisitos do 1 ao 6

---
+++ script.js
@@ -0,0 +1,22 @@
+let botaoAddTarefa = document.querySelector("#criar-tarefa");
+let caixaTextoTarefa = document.querySelector("#texto-tarefa");
+let listaDeTarefas = document.querySelector("#lista-tarefas");
+
+botaoAddTarefa.addEventListener('click',function adicionandoTarefa(){ 
+    let novaTarefa  = document.createElement('li')
+    console.log(novaTarefa)
+    novaTarefa.innerText = caixaTextoTarefa.value
+    novaTarefa.addEventListener('click', selecionar);
+    novaTarefa.addEventListener('dblclick', concluir); 
+    listaDeTarefas.appendChild(novaTarefa);
+    caixaTextoTarefa.value = '';
+});
+
+function selecionar() {
+    this.classList.add('selected');
+}
+  
+function concluir() {
+    this.classList.remove('selected');
+    this.classList.add('completed');
+}
