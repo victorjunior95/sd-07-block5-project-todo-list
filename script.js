241157611c9dbb16821b2e33c745294de703936d
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-14 09:06:59 -0300

    some itens codeclimate

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 const inputTask = document.getElementById('texto-tarefa');
 const buttonAddTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-const buttonRemoveAll= document.getElementById('apaga-tudo');
-const buttonRemoveDone= document.getElementById('remover-finalizados');
-const buttonSaveTaskList= document.getElementById('salvar-tarefas');
-const buttonMoveUp= document.getElementById('mover-cima');
-const buttonMoveDown= document.getElementById('mover-baixo');
-const buttonRemoveSelected= document.getElementById('remover-selecionado');
-
-buttonAddTask.addEventListener('click',clickAddTask);
-buttonRemoveAll.addEventListener('click',removeAll);
-buttonRemoveDone.addEventListener('click',removeAllDone);
-buttonSaveTaskList.addEventListener('click',saveTaskList);
-buttonMoveUp.addEventListener('click',clickMove);
-buttonMoveDown.addEventListener('click',clickMove);
-buttonRemoveSelected.addEventListener('click',removeSelected);
+const buttonRemoveAll = document.getElementById('apaga-tudo');
+const buttonRemoveDone = document.getElementById('remover-finalizados');
+const buttonSaveTaskList = document.getElementById('salvar-tarefas');
+const buttonMoveUp = document.getElementById('mover-cima');
+const buttonMoveDown = document.getElementById('mover-baixo');
+const buttonRemoveSelected = document.getElementById('remover-selecionado');
 
 // function check input
 function checkInput(inputElement) {
+  let result = true;
   if (inputElement.value === '') {
     alert('É necessário informar uma tarefa.');
-    return false;
-  } else {
-    return true;
+    result = false;
   }
+  return result;
 }
 
 // function to clickAddTask
@@ -41,7 +33,7 @@ function addTask(taskName, completed) {
     listItem.innerText = taskName;
     // check if task is done
     if (completed) {
-      addClassName(listItem,' completed');
+      addClassName(listItem, ' completed');
     }
     // add event select - one click
     listItem.addEventListener('click', selectListItem);
@@ -51,7 +43,7 @@ function addTask(taskName, completed) {
     taskList.appendChild(listItem);
     // clear input text
     inputTask.value = '';
-  } 
+  }
 }
 
 // function to unselect all list item
@@ -71,7 +63,7 @@ function selectListItem(event) {
   // unselect all list items
   unselectAll();
   // select list item target
-  addClassName(event.target,' item-selected');
+  addClassName(event.target, ' item-selected');
 }
 
 // function to add a class name to a element
@@ -90,12 +82,11 @@ function doneListItem(event) {
   // check if list item is done
   if (event.target.className.indexOf('completed') >= 0) {
     // un mark/check list item target
-    removeClassName(event.target,' completed')
+    removeClassName(event.target, ' completed')
   } else {
     // mark/check list item target
-    addClassName(event.target,' completed');
+    addClassName(event.target, ' completed');
   }
-  
 }
 
 // function to remove all list item
@@ -110,7 +101,7 @@ function removeAllDone() {
   // remove each one
   if (listItemsDone !== null) {
     for (let index = 0; index < listItemsDone.length; index += 1) {
-      //remove item
+      // remove item
       taskList.removeChild(listItemsDone[index]);
     }
   }
@@ -134,7 +125,7 @@ function saveTaskList() {
       // create the item list
       const itemList = {
         task: task,
-        completed: completed
+        completed: completed,
       };
       // push item list on list
       itemsList.push(itemList);
@@ -148,7 +139,7 @@ function saveTaskList() {
 function loadTaskList() {
   let itemsList = [];
   // get the tasklist on storage
-  itemsList = JSON.parse(localStorage.getItem("taskList"));
+  itemsList = JSON.parse(localStorage.getItem('taskList'));
   // for each item at itemsList
   if (itemsList !== null) {
     for (let index = 0; index < itemsList.length; index += 1) {
@@ -156,9 +147,6 @@ function loadTaskList() {
     }
   }
 }
-
-// load list from storage
-loadTaskList();
 
 // function get selected list item
 function getSelectedListItem() {
@@ -174,26 +162,26 @@ function clickMove(event) {
   }
 }
 
-//function to move list item
+// function to move list item
 function moveListItem(moveUp) {
   // get selected item
   const listItem = getSelectedListItem();
   // check if exist selected item
   if (listItem !== null) {
-    //if moveUp - move up the list Item
+    let beforeListItem;
+    // if moveUp - move up the list Item
     if (moveUp) {
       if (listItem !== taskList.firstChild) {
-        const previousListItem = listItem.previousSibling;
-        taskList.removeChild(listItem);
-        taskList.insertBefore(listItem, previousListItem);
+        beforeListItem = listItem.previousSibling;
       }
-    } else { //if moveDown - move down the list item
+    // if moveDown - move down the list item
+    } else { 
       if (listItem !== taskList.lastChild) {
-        const nextListItem = listItem.nextSibling;
-        taskList.removeChild(listItem);
-        taskList.insertBefore(listItem, nextListItem.nextSibling);
+        beforeListItem = listItem.nextSibling.nextSibling;
       }
     }
+    taskList.removeChild(listItem);
+    taskList.insertBefore(listItem, beforeListItem);
   }
 }
 
@@ -207,3 +195,15 @@ function removeSelected() {
     taskList.removeChild(listItem);
   }
 }
+
+// add events to buttons
+buttonAddTask.addEventListener('click', clickAddTask);
+buttonRemoveAll.addEventListener('click', removeAll);
+buttonRemoveDone.addEventListener('click', removeAllDone);
+buttonSaveTaskList.addEventListener('click', saveTaskList);
+buttonMoveUp.addEventListener('click', clickMove);
+buttonMoveDown.addEventListener('click', clickMove);
+buttonRemoveSelected.addEventListener('click', removeSelected);
+
+// load list from storage
+loadTaskList();
