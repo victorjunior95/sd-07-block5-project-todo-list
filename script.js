ca0026addccd235165d841bb949e68d189695b8f
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-13 22:37:28 -0300

    Adicionando o arquivo do prettier

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input */
-
 let textoTarefa = document.getElementById("texto-tarefa");
 const salvar = document.getElementById("criar-tarefa");
 
