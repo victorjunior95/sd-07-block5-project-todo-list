9bce1d8f35a4b7ce66b019e55bb10389d7d730c4
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-13 22:38:39 -0300

    Arrumando codeclimate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let textoTarefa = document.getElementById("texto-tarefa");
-const salvar = document.getElementById("criar-tarefa");
+let textoTarefa = document.getElementById('texto-tarefa');
+const salvar = document.getElementById('criar-tarefa');
 
-salvar.addEventListener("click", criarTarefa);
+salvar.addEventListener('click', criarTarefa);
 
 function criarTarefa() {
-  if (textoTarefa.value === "") {
-    return console.log("Campo vazio");
+  if (textoTarefa.value === '') {
+    return console.log('Campo vazio');
   }
-  let lista = document.getElementById("lista-tarefas");
-  let novoItem = document.createElement("li");
+  let lista = document.getElementById('lista-tarefas');
+  let novoItem = document.createElement('li');
   novoItem.innerText = textoTarefa.value;
   lista.appendChild(novoItem);
   limparTexto();
@@ -16,47 +16,47 @@ function criarTarefa() {
 }
 
 function limparTexto() {
-  textoTarefa.value = "";
+  textoTarefa.value = '';
 }
 
 function descelecionarItem() {
-  document.querySelectorAll("li").forEach((item) => {
-    item.classList.remove("backgroundItemList");
-    item.classList.remove("selecionado");
+  document.querySelectorAll('li').forEach((item) => {
+    item.classList.remove('backgroundItemList');
+    item.classList.remove('selecionado');
   });
 }
 
 function selecionarItem() {
-  document.querySelectorAll("li").forEach((item) => {
-    item.addEventListener("click", () => {
+  document.querySelectorAll('li').forEach((item) => {
+    item.addEventListener('click', () => {
       descelecionarItem();
-      item.classList.add("backgroundItemList");
-      item.classList.add("selecionado");
+      item.classList.add('backgroundItemList');
+      item.classList.add('selecionado');
     });
-    item.addEventListener("dblclick", () => {
-      item.classList.add("completed");
-      item.addEventListener("dblclick", () => {
-        item.classList.remove("completed");
+    item.addEventListener('dblclick', () => {
+      item.classList.add('completed');
+      item.addEventListener('dblclick', () => {
+        item.classList.remove('completed');
       });
     });
   });
 }
 
-let limparLista = document.getElementById("apaga-tudo");
-limparLista.addEventListener("click", () => {
-  document.querySelectorAll("li").forEach((item) => {
+let limparLista = document.getElementById('apaga-tudo');
+limparLista.addEventListener('click', () => {
+  document.querySelectorAll('li').forEach((item) => {
     item.remove();
   });
 });
 
-document.getElementById("remover-finalizados").addEventListener("click", () => {
-  document.querySelectorAll(".completed").forEach((item) => {
+document.getElementById('remover-finalizados').addEventListener('click', () => {
+  document.querySelectorAll('.completed').forEach((item) => {
     item.remove();
   });
 });
 
-document.getElementById("remover-selecionado").addEventListener("click", () => {
-  document.querySelectorAll(".selecionado").forEach((item) => {
+document.getElementById('remover-selecionado').addEventListener('click', () => {
+  document.querySelectorAll('.selecionado').forEach((item) => {
     item.remove();
   });
 });
