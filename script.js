cfc740b3bada8c2287adc68270ac3b90f89b3dec
Strongreen <strongreen@live.com>
2020-09-14 17:44:53 -0300

    adicionando class ao item

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let campoInsertTarefa = document.querySelector('#texto-tarefa');
 let criarLista = document.querySelector('#lista-tarefas');
-let bntCriaTarefa = document.querySelector('#criar-tarefa')
+let bntCriaTarefa = document.querySelector('#criar-tarefa');
+let classeItem = document.querySelectorAll('.itens');
 
 function CriaItem(){
     let item = 0;
     let lista = [];
     lista[item] = document.createElement('li');
     lista[item].innerText = campoInsertTarefa.value;
+    lista[item].className = 'itens';
+    lista[item].style.backgroundColor = "rgb(255, 255,255 )";
     criarLista.appendChild(lista[item]);
     campoInsertTarefa.value = ""; 
-    item ++;
+    item ++;255
 }
 
 bntCriaTarefa.addEventListener('click',CriaItem);
