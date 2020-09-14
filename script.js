4fcc0b89cba1d7453c35100cab87eaa9fb1ab4a2
Felipe Nascimento <flpnascto@gmail.com>
2020-09-14 20:54:48 -0300

    Adicionando requisito 9.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // destaca item da lista selecionado com background rgb(128,128,128)
-function selectedItenList() {
+function selectedItemList() {
   const listItens = document.querySelectorAll('li');
-  for (let index = 0 ; index < listItens.length ; index += 1) {
+  for (let index = 0; index < listItens.length; index += 1) {
     listItens[index].removeAttribute('style');
   }
   this.style.backgroundColor = 'rgb(128 , 128 , 128)';
+}
+
+// marcação de itens completos
+function completedItemList() {
+  if (this.hasAttribute('class')) {
+    this.removeAttribute('class')
+  } else this.className = 'completed';
 }
 
 // adiciona tarefa à lista ordenada conforme texto digitado
 function adicionarTarefa(element, textInput) {
   const listaTarefa = document.createElement('li');
   listaTarefa.innerText = textInput;
-  listaTarefa.addEventListener('click', selectedItenList);
+  listaTarefa.addEventListener('click', selectedItemList);
+  listaTarefa.addEventListener('dblclick', completedItemList);
   element.insertAdjacentElement('beforeend', listaTarefa);
 }
 
