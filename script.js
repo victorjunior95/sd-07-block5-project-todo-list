5a923f90438f62b96da36d9e93af308b293ed1a4
Strongreen <strongreen@live.com>
2020-09-14 16:52:49 -0300

    Criando varios itens dinamicamente

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let campoInsertTarefa = document.querySelector('#texto-tarefa');
 let criarLista = document.querySelector('#lista-tarefas');
 let bntCriaTarefa = document.querySelector('#criar-tarefa')
-let lista = document.createElement('li');
 
 function CriaItem(){
-    lista.innerText = campoInsertTarefa.value;
-    criarLista.appendChild(lista);
+    let item = 0;
+    let lista = [];
+    lista[item] = document.createElement('li');
+    lista[item].innerText = campoInsertTarefa.value;
+    criarLista.appendChild(lista[item]);
     campoInsertTarefa.value = ""; 
+    item ++;
 }
 
 bntCriaTarefa.addEventListener('click',CriaItem);
