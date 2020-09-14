990ae328f863ef94ffb60d7deda7f80aa8581a6a
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-14 18:44:29 -0300

    requisito 9 riscar tarefa completa

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const add = document.querySelector('#criar-tarefa')
 const assignment = document.querySelector('#texto-tarefa')
 
-add.addEventListener('click', function () {
+window.onload(add.addEventListener('click', function () {
     let iten1 = assignment.value
     let list = document.querySelector('#lista-tarefas').innerHTML += `<li class="itens">${iten1}</li>`
     assignment.value = ""
 
-const listItens = document.querySelectorAll('.itens')
-for ( let index = 0; index < listItens.length; index += 1){
-    const iten = listItens[index]
-    iten.addEventListener('click' ,function(){
-         iten.style.backgroundColor = "rgb(128, 128, 128)"
-    })
-}
+    const listItens = document.querySelectorAll('.itens')
+    
+
+    for (let index = 0; index < listItens.length; index += 1) {
+        const itenSelected = listItens[index]
+
+        itenSelected.addEventListener('click', function (event) {
+            event.preventDefault()
+            itenSelected.className = 'itens itensSelected'
+            
+         itenSelected.addEventListener('dblclick' , function (){
+             itenSelected.className +=' '+'completed'
+         })
+
+
+
+        })
+    }
 })
+)
