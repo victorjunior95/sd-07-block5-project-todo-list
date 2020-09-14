9895b79e430c28b4b50bc0a13e32ca93bd5ffe97
Bruno L Nogueira <nugobruno@gmail.com>
2020-09-14 16:28:46 -0300

    selecionar e riscar funcionam

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let inputTask = document.querySelector("#texto-tarefa");
 let inputTaskBtn = document.querySelector("#criar-tarefa");
 let orderedList = document.querySelector("#lista-tarefas");
 let indexTask = 0;
+let firstTime = true;
 
 inputTaskBtn.addEventListener("click", function() {
   indexTask += 1;
@@ -14,14 +15,39 @@ inputTaskBtn.addEventListener("click", function() {
   inputTask.value = "";
 
   taskLi.addEventListener("click", function() {
-    selectTask(orderedList, setTaskId);
+    selectTask(setTaskId);
+  })
+
+  taskLi.addEventListener('dblclick', function() {
+    doubleClickTask(setTaskId);
   })
 })
+function doubleClickTask(id) {
+  let elem = document.querySelector(id);
+  if (firstTime !== false) {
+    firstTime = false;
+    elem.className += ' completed';
+  } else {
+    firstTime = true;
+    elem.className = 'item-list selected-task';
+  }
+}
+function selectTask(id) {
+  let elem = document.querySelector(id);
+  clicked = true;
+  if (elem.classList.contains('completed')) {
+    elem.className = "item-list selected-task completed";
+  } else {
+    elem.className = "item-list selected-task";
+  }
 
-function selectTask(where, id) {
-  let taskStyle = document.createElement("style");
-  where.appendChild(taskStyle);
-  taskStyle.innerHTML = id + " { background-color: rgb(128, 128, 128) }";
+  let allSelected = document.querySelectorAll('.selected-task');
+
+  for (index = 0; index < allSelected.length; index += 1) {
+    if (allSelected[index] !== elem) {
+      let replaceClass = allSelected[index].classList.remove('selected-task');
+    }
+  }
 }
 
 function createLi(where, id) {
