938daf581fccf2ff64e1e14f0599ad258db4e61e
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-14 18:19:53 -0300

    Requirement 7 - Changing the selected item color

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let click = document.getElementById('criar-tarefa');
 
-click.addEventListener('click', function() {
+click.addEventListener("click", function() {
   let textInput = document.getElementById('texto-tarefa').value;  
   let textList = document.getElementById('lista-tarefas');
   let listItem = document.createElement('li');
   listItem.innerText = textInput;
   textList.appendChild(listItem);
   document.getElementById('texto-tarefa').value = "";
+});
+
+document.getElementById('lista-tarefas').addEventListener("click", function(e) {
+    e.target.style.backgroundColor = 'rgb(128, 128, 128)';
 });
