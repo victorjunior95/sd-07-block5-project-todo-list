e93f13f236e7a158ee98856737a3a61b0bc118c6
Berilo Wanderley <wberilo@gmail.com>
2020-09-14 16:13:40 -0300

    requisites up to 8

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-
+let selected;
 
 document.querySelector('#criar-tarefa').addEventListener('click', function(){
   const li = document.createElement('li')
   li.addEventListener('click', function(){
-    li.style.backgroundColor = 'rgb(128, 128, 128)'
+    li.className = 'selected';
+    if(selected != undefined){
+      selected.className = '';
+    }
+    selected = li;
   })
   li.innerText = document.querySelector('#texto-tarefa').value
   document.querySelector('#lista-tarefas').appendChild(li)
