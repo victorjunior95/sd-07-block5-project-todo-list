116ba64c6d35fbefe253e80f76ee35449a25f81a
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-14 19:10:12 -0300

    bug fixed

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ const elements = [];
 
 function changeBackgroundColor(element) {
   element.addEventListener('click', () => {
-    if (!element.className) {
-      for (let i = 0; i < elements.length; i += 1) {
-        elements[i].className = '';
-      }
-      element.className = 'selected';
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].classList.remove('selected');
     }
+    element.className = 'selected';
   });
 }
 
 function completeTasks(element) {
   element.addEventListener('dblclick', () => {
-    if (!element.className || element.className === 'selected') {
+    if (element.className !== 'completed') {
       element.className = 'completed';
     } else if (element.className === 'completed') {
       element.className = '';
@@ -28,8 +26,8 @@ function insertItem(textItem) {
   const newItem = document.createElement('li');
   textItem = textInput.value;
   newItem.innerHTML = textItem;
-  completeTasks(newItem);
   changeBackgroundColor(newItem);
+  completeTasks(newItem);
   list.appendChild(newItem);
   textInput.value = '';
   elements.push(newItem);
