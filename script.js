42f3c6d88d16666fd9a030098ab8b87359a4420a
vanderson-henrique <vhbenedito@gmail.com>
2020-09-14 16:55:56 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const list = document.getElementById('lista-tarefas');
+let listaCompleta;
 
 function addElement() {
-  let textInput = document.getElementById('texto-tarefa');
-  tarefaDigitada = textInput.value;
+  const textInput = document.getElementById('texto-tarefa');
+  const tarefaDigitada = textInput.value;
   const elementList = document.createElement('li');
-  list.appendChild(elementList);
+  list.appendChild(elementList).classList = 'item-list';
   elementList.textContent = tarefaDigitada;
   textInput.value = '';
+
+  let itensList = document.querySelectorAll('.item-list');
+for (let index = 0; index < itensList.length; index += 1){
+let itemSelecionado = itensList[index];
+itemSelecionado.addEventListener('click', function(){
+    for (let index = 0; index < itensList.length; index += 1){
+      let apagaCor = itensList[index];
+      apagaCor.style.backgroundColor = ''; 
+    }
+    itemSelecionado.style.backgroundColor = 'rgb(128, 128, 128)';
+});  
+}
+
+
 }
 
 const buttonAdd = document.getElementById('criar-tarefa');
 buttonAdd.addEventListener('click', addElement);
+
+
