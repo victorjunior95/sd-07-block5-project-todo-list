b8c8fe2423a78e1cd145e0ff1f841222775b4235
gabrieldeori <deori.pessoal@gmail.com>
2020-09-14 11:39:44 -0300

    select unselect type radio functions

--- script.js
+++ script.js
@@ -6,12 +6,39 @@ const buttonCreateTask = document.getElementById('criar-tarefa');
 
 //  Ordered List Outputs
 const orderedListTasks = document.getElementById('lista-tarefas');
-let liTask = document.querySelectorAll('#lista-tarefas li');
+let liTask = document.getElementsByClassName('task');
+let selectedItem = document.getElementsByClassName('selected');
 
 //  Functions
+function selectLi() {
+  if (selectedItem[0] !== undefined) {
+    selectedItem[0].classList.remove('selected');
+    event.target.classList.add('selected');
+  } else {
+    event.target.classList.add('selected');
+  }
+}
+
+function unselectLi() {
+  selectedItem = document.getElementsByClassName('selected');
+  if (selectedItem[0] === event.target) {
+    selectedItem[0].classList.remove('selected');
+  } else {
+    selectLi();
+  }
+}
+
+function markAsDoneLi() {
+  console.log('markAsDone');
+}
+
 function elementCreator() {
   const element = document.createElement('li');
   element.innerText = inputTextTask.value;
+  element.className = 'task'
+  inputTextTask.value = '';
+  element.addEventListener('click', unselectLi);
+  element.addEventListener('dblclick', markAsDoneLi);
   return element;
 }
 
@@ -21,5 +48,3 @@ function addTaskToList() {
 
 // Event Listeners
 buttonCreateTask.addEventListener('click', addTaskToList);
-
-//  Última linha
