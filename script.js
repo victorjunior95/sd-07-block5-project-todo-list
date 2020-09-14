9862f5855cbceef5c08f37847dad20e592e1876e
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-14 17:18:09 -0300

    requre 10

--- script.js
+++ script.js
@@ -102,11 +102,11 @@ function clearInputValue() {
   document.getElementById('texto-tarefa').value = '';
 }
 
-function getOl(){
+function getOl() {
   return document.getElementById('lista-tarefas');
 }
 
-function getInputValue(){
+function getInputValue() {
   return document.getElementById('texto-tarefa').value;
 }
 
@@ -118,7 +118,11 @@ function getBtnRemoveFinalized() {
   return document.getElementById('remover-finalizados');
 }
 
-function getOl(){
+function getBtnRemoveAll() {
+  return document.getElementById('apaga-tudo');
+}
+
+function getOl() {
   return document.getElementById('lista-tarefas');
 }
 
@@ -126,16 +130,16 @@ function createLi() {
   return document.createElement('li');
 }
 
-function getLiAll(){
+function getLiAll() {
   return document.getElementsByTagName('li');
 }
 
-function removeLi(li){
+function removeLi(li) {
   let ol = getOl();
   ol.removeChild(li);
 }
 
-function insertPropertyInElement({element = '', text = '', classe = ''}){
+function insertPropertyInElement({ element = '', text = '', classe = '' }) {
   if (element) {
     let myElement = element;
     if (text) myElement.innerText = text;
@@ -147,50 +151,56 @@ function insertPropertyInElement({element = '', text = '', classe = ''}){
 let liCurrentSplited;
 
 // adicionando evento de click no li
-function eventLiClick(li){
-  li.addEventListener('click', function(event){
+function eventLiClick(li) {
+  li.addEventListener('click', function (event) {
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 
-    if(liCurrentSplited) liCurrentSplited.style.backgroundColor = '';
+    if (liCurrentSplited) liCurrentSplited.style.backgroundColor = '';
 
     liCurrentSplited = li;
   });
 }
 
 // adicionando evendo de double click no li
-function eventLiDblClick(li){
-  li.addEventListener('dblclick', function(){
+function eventLiDblClick(li) {
+  li.addEventListener('dblclick', function () {
     li.classList.toggle('completed');
   });
 }
 
 // adicionando eventos no li
-function addEventSplitLi(li){
+function addEventSplitLi(li) {
   eventLiClick(li);
 
   eventLiDblClick(li);
 }
 
-function insertLiInOl(text){
+function insertLiInOl(text) {
   let ol = getOl();
   let li = createLi();
-  insertPropertyInElement({text: text, element: li});
+  insertPropertyInElement({ text: text, element: li });
   addEventSplitLi(li);
   ol.appendChild(li);
 }
 
 let btnAdd = getBtnAdd();
-btnAdd.addEventListener('click', function(){
+btnAdd.addEventListener('click', function () {
   insertLiInOl(getInputValue());
   clearInputValue();
 });
 
 // remove finalizados
 let btnRemoveFinalized = getBtnRemoveFinalized();
-btnRemoveFinalized.addEventListener('click', function(){
+btnRemoveFinalized.addEventListener('click', function () {
   let li = getLiAll();
-  for(let index = 0; index < li.length; index += 1) {
+  for (let index = 0; index < li.length; index += 1) {
     if (li[index].className == 'completed') removeLi(li[index]);
   }
 });
 
+// remove todos
+const btnRemoveAll = getBtnRemoveAll();
+btnRemoveAll.addEventListener('click', function () {
+  let ol = getOl();
+  ol.innerHTML = '';
+});
