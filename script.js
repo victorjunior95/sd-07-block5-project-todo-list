490a1ac8f267c60d98f5a92f2478262213458f36
Felipe Nascimento <flpnascto@gmail.com>
2020-09-14 20:15:23 -0300

    Adicionando requisito 6.

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 function adicionarTarefa(element, textInput) {
   const listaTarefa = document.createElement('li');
   listaTarefa.innerText = textInput;
-  element.appendChild(listaTarefa);
+  element.insertAdjacentElement('beforeend',listaTarefa);
 }
 
-window.onload = function() {
+window.onload = function () {
   // adicionar tarefa
   const criarTarefaButton = document.querySelector('#criar-tarefa');
   const textoTarefaInput = document.querySelector('#texto-tarefa');
   const listaTarefasOl = document.querySelector('#lista-tarefas');
   criarTarefaButton.addEventListener('click', function () {
     adicionarTarefa(listaTarefasOl, textoTarefaInput.value);
+    textoTarefaInput.value = '';
   });
-}
+};
