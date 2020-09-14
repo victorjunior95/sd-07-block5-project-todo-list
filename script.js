8bcbfcabafbb1793f7b82891f06ee8a20da55dbc
Brune Miguel <bstfreire@gmail.com>
2020-09-14 19:20:52 -0300

    Requisito 10 botão apaga-tudo

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let botaoAddTarefa = document.querySelector("#criar-tarefa");
 let caixaTextoTarefa = document.querySelector("#texto-tarefa");
 let listaDeTarefas = document.querySelector("#lista-tarefas");
 
+
 botaoAddTarefa.addEventListener('click',function adicionandoTarefa(){ 
     let novaTarefa  = document.createElement('li')
+    novaTarefa.classList.add("itens")
     novaTarefa.innerText = caixaTextoTarefa.value
     novaTarefa.addEventListener('click', selecionar);
     novaTarefa.addEventListener('dblclick', concluir); 
@@ -19,3 +21,11 @@ function concluir() {
     this.classList.remove('selected');
     this.classList.add('completed');
 }
+
+let apagaTudo = document.querySelector("#apaga-tudo");
+apagaTudo.addEventListener('click', function apagarLista() {
+  let itens = document.querySelectorAll(".itens");
+    for (let i = 0; i < itens.length; i += 1){
+          itens[i].remove();
+    }
+});
