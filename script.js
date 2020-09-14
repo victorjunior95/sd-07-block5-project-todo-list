f6637dc107539c92c0a36ee6636aad6c08fc9533
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-14 18:52:39 -0300

    itens

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 const list = document.getElementById('lista-tarefas');
 const addNewItem = document.getElementById('criar-tarefa');
 const textInput = document.getElementById('texto-tarefa');
-const todos = document.getElementsByTagName('li');
+const elements = [];
 
-
-function changeBackgroundColor() {
-  for (let i = 0; i < todos.length; i += 1) {
-    todos[i].addEventListener('click', () => {
-      todos[i].className = 'selected';
-    });
-  }
+function changeBackgroundColor(element) {
+  element.addEventListener('click', () => {
+    if (!element.className) {
+      for (let i = 0; i < elements.length; i += 1) {
+        elements[i].className = '';
+      }
+      element.className = 'selected';
+    }
+  });
 }
 
-function completeTasks() {
-  for (let i = 0; i < todos.length; i += 1) {
-    todos[i].addEventListener('dblclick', () => {
-      todos[i].className = 'completed';
-    });
-  }
+function completeTasks(element) {
+  element.addEventListener('dblclick', () => {
+    if (!element.className || element.className === 'selected') {
+      element.className = 'completed';
+    } else if (element.className === 'completed') {
+      element.className = '';
+    }
+  });
 }
 
 function insertItem(textItem) {
   const newItem = document.createElement('li');
   textItem = textInput.value;
   newItem.innerHTML = textItem;
+  completeTasks(newItem);
+  changeBackgroundColor(newItem);
   list.appendChild(newItem);
   textInput.value = '';
-  changeBackgroundColor();
-  completeTasks();
+  elements.push(newItem);
 }
 
 addNewItem.addEventListener('click', insertItem);
