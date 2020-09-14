ea98a6ee6c1b85ec52cbc36eb939efbadf915b33
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-14 15:12:53 -0300

    code climate

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const list = document.getElementById('lista-tarefas');
 const addNewItem = document.getElementById('criar-tarefa');
 const textInput = document.getElementById('texto-tarefa');
 
-addNewItem.addEventListener('click', insertItem)
-
 function insertItem(textItem) {
-  const newItem = document.createElement('li')
-  textItem = textInput.value
+  const newItem = document.createElement('li');
+  textItem = textInput.value;
   newItem.innerText = textItem;
   list.appendChild(newItem);
 }
+
+addNewItem.addEventListener('click', insertItem);
