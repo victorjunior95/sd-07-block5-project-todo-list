1e090b515e4b02b4c93ef883c14651a146c6af1e
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-14 16:47:50 -0300

    click

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const list = document.getElementById('lista-tarefas');
 const addNewItem = document.getElementById('criar-tarefa');
 const textInput = document.getElementById('texto-tarefa');
+const todos = document.getElementsByTagName('li');
+
+function changeBackgroundColor() {
+  for (let i = 0; i < todos.length; i += 1) {
+    todos[i].addEventListener('click', () => {
+      todos[i].className = 'selected';
+    });
+  }
+}
+
+function completeTasks() {
+  for (let i = 0; i < todos.length; i += 1) {
+    todos[i].addEventListener('dblclick', () => {
+      if(todos[i].className === 'completed') {
+        todos[i].className = '';
+      } else {
+        todos[i].className = 'completed';
+      }
+    });
+  }
+}
 
 function insertItem(textItem) {
   const newItem = document.createElement('li');
@@ -8,12 +29,8 @@ function insertItem(textItem) {
   newItem.innerHTML = textItem;
   list.appendChild(newItem);
   textInput.value = '';
-  const todos = document.getElementsByTagName('li');
-  for (let i = 0; i < todos.length; i += 1) {
-    todos[i].addEventListener('click', () => {
-      todos[i].className = 'selected';
-    });
-  }
+  changeBackgroundColor();
+  completeTasks();
 }
 
 addNewItem.addEventListener('click', insertItem);
