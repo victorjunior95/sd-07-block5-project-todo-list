8fda0b5b5f7e448ab1a7995aad850168cbc92770
Fernando <nando.neves@gmail.com>
2020-09-14 15:16:36 -0300

    falta destachar

--- script.js
+++ script.js
@@ -12,11 +12,16 @@ let botaoEntrada = document.getElementById("criar-tarefa");
     pai.appendChild(item);
     entrada.value = "";
     let lista = document.querySelectorAll(".item");
-    item.id = lista.length
-    criarEventos()   
+    item.id = lista.length;
+    criarEventos();
+    tarefasCompletas();   
        
     }
 })    
+
+
+// // Marcando itens selecionados.  
+
 
 function criarEventos(){
     let lista = document.querySelectorAll(".item");
@@ -27,25 +32,26 @@ for (let i = 0; i < lista.length; i++) {
            if (apagar[0] == null){
            lista[i].classList.add("selected");
            } else{
-               document.getElementsByClassName("selected")[0].classList.remove("selected")
-               lista[i].classList.add("selected");
+                document.getElementsByClassName("selected")[0].classList.remove("selected")
+              lista[i].classList.add("selected");
             }
-
         })
     })(i);
  }
 }
-// // Marcando itens selecionados.  
-function selecionar(index){
-    let lista = document.querySelectorAll(".item");
-    console.log(lista[index])
-    lista[index].classList.add ("selected")
+
+
+function tarefasCompletas(){
+let completos = document.querySelectorAll(".item")
+
+for (let i = 0; i < completos.length; i++) {
+    (function(i) {
+         completos[i].addEventListener("dblclick", function() {
+           console.log("veio")
+            completos[i].classList.add("completed");
+        })
+    })(i);
+ }
 }
-// botaoEntrada.addEventListener("click", function(){
-//     let lista = document.querySelectorAll(".item");
-//     for (let index =0; index<lista.length;index +=1){
-//         lista[i].addEventListener("click", function(){
-//             lista[i].style.backgroundColor = "rgb(128, 128, 128)"
-//         })
-//     }
-// })
+
+tarefasCompletas()
