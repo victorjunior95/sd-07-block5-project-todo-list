c826a8dda00009dbba6c6cbffdb2657d1594f087
sagacello <sagacello@hotmail.com>
2020-09-14 19:46:05 -0300

    aleluia

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 let entradaTexto = document.querySelector('#texto-tarefa')
 let botaoTarefa = document.querySelector("#criar-tarefa")
 let listaTarefas = document.querySelector("#lista-tarefas")
-
+let botaoApagarLista = document.querySelector("#apaga-tudo")
 
 botaoTarefa.addEventListener('click', function(){
     let novoElemento = document.createElement("li")
     novoElemento.innerHTML = entradaTexto.value
     listaTarefas.appendChild(novoElemento)  
+    novoElemento.addEventListener('click', selectItem);
     document.getElementById('texto-tarefa').value='';
-
+        
 })
+
+function clearSelection() {
+    const item = document.querySelector('.selected');
+    if (item !== null) item.classList.remove('selected');
+  }
+  
+  function selectItem(event) {
+    clearSelection();
+    event.target.classList.add('selected');
+  }
+
+
+
+
+botaoApagarLista.addEventListener('click', function(){
+   listaTarefas.innerHTML = ''
+})
+
 
 
 // document.body.insertBefore(divNova, divAtual);
