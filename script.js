92937b2298e712a23a09ce33853c3dc9729ffca3
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-14 15:11:16 -0300

    adicionando funcionalidade que salva tabela no localStorage

--- script.js
+++ script.js
@@ -31,34 +31,62 @@ function createListItem() {
 }
 
 function clearAllItems() {
-    const liItems = document.getElementsByTagName('li');
-    const orderedList = document.getElementById('lista-tarefas');
-    const listLength = liItems.length;
-    for (let i = 0; i < listLength; i += 1) {
-      orderedList.removeChild(liItems[0]);
-    }
+  const liItems = document.getElementsByTagName('li');
+  const orderedList = document.getElementById('lista-tarefas');
+  const listLength = liItems.length;
+  for (let i = 0; i < listLength; i += 1) {
+    orderedList.removeChild(liItems[0]);
+  }
 }
 
 function removeDone() {
-    const selectedItem = document.getElementsByClassName('completed');
-    const listLength = selectedItem.length;
-    const orderedList = document.getElementById('lista-tarefas');
-    for (let i = 0; i < listLength; i += 1) {
-      orderedList.removeChild(selectedItem[0]);
-    }
+  const selectedItem = document.getElementsByClassName('completed');
+  const listLength = selectedItem.length;
+  const orderedList = document.getElementById('lista-tarefas');
+  for (let i = 0; i < listLength; i += 1) {
+    orderedList.removeChild(selectedItem[0]);
+  }
 }
 
 function removeSelected() {
-    const orderedList = document.getElementById('lista-tarefas');
-    const selectedItem = document.getElementsByClassName('clicked');
-    orderedList.removeChild(selectedItem[0]);
+  const orderedList = document.getElementById('lista-tarefas');
+  const selectedItem = document.getElementsByClassName('clicked');
+  orderedList.removeChild(selectedItem[0]);
 }
 
-let buttonAddItem = document.getElementById('criar-tarefa');
+function saveItems() {
+  const liItems = document.getElementsByTagName('li');
+  if (liItems.length === 0) {
+    alert('Não há itens para salvar');
+    return;
+  }
+  for (let i = 0; i < liItems.length; i += 1) {
+    localStorage.setItem(i, [liItems[i].innerText , liItems[i].classList]);
+  }
+}
+
+const buttonAddItem = document.getElementById('criar-tarefa');
 buttonAddItem.addEventListener('click', createListItem);
-let clearButton = document.getElementById('apaga-tudo');
+const clearButton = document.getElementById('apaga-tudo');
 clearButton.addEventListener('click', clearAllItems);
-let removeDoneItem = document.getElementById('remover-finalizados');
+const removeDoneItem = document.getElementById('remover-finalizados');
 removeDoneItem.addEventListener('click', removeDone);
-let removeSelectedItem = document.getElementById('remover-selecionado');
+const removeSelectedItem = document.getElementById('remover-selecionado');
 removeSelectedItem.addEventListener('click', removeSelected);
+const saveItemsButton = document.getElementById('salvar-tarefas');
+saveItemsButton.addEventListener('click', saveItems);
+
+// carrega os itens salvos no localStorage
+if (localStorage.length > 0) {
+  const orderedList = document.getElementById('lista-tarefas');
+  for (let i = 0; i < localStorage.length; i += 1) {
+    const currentStorage = localStorage.getItem(i).split(',');
+    console.log(currentStorage);
+    const localLi = document.createElement('li');
+    localLi.innerText = currentStorage[0];
+    localLi.classList = currentStorage[1];
+    orderedList.appendChild(localLi);
+    localLi.addEventListener('click', focusedItem);
+    localLi.addEventListener('dblclick', markItem);
+  }
+}
