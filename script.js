6b612d79fbc374094769f385c1b0ae4bbd1cad8b
vanderson-henrique <vhbenedito@gmail.com>
2020-09-14 15:32:42 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let list = document.getElementById('lista-tarefas');
+const list = document.getElementById('lista-tarefas');
 
 function addElement() {
   let textInput = document.getElementById('texto-tarefa');
   tarefaDigitada = textInput.value;
-  let elementList = document.createElement('li');
+  const elementList = document.createElement('li');
   list.appendChild(elementList);
   elementList.textContent = tarefaDigitada;
   textInput.value = '';
 }
 
-let buttonAdd = document.getElementById('criar-tarefa');
+const buttonAdd = document.getElementById('criar-tarefa');
 buttonAdd.addEventListener('click', addElement);
