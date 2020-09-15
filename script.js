0a91d1f84605f7388757079d658f7e3c955fd887
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-15 18:18:31 -0300

    requisito 7 e 8: adicionando classe para selecionar item na lista

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ window.onload = function(){
     //Adicionando evento de colocar o input na lista
     btnNewTask.addEventListener("click", function(){
         //Declarando variáveis participantes do evento
-        let textInput = document.getElementById("texto-tarefa");//Vai receber o texto
-        let listMother = document.getElementById("lista-tarefas");//Declarando node mãe
-        let listChild = document.createElement("li");//Criando elemento lista
-        listMother.appendChild(listChild).innerText = textInput.value;//Adicionado a filha/tarefa na lista ordenada
-        textInput.value = "";
-    })
+        let textInput = document.getElementById("texto-tarefa");    //Vai receber o texto
+        let listMother = document.getElementById("lista-tarefas");  //Declarando node mãe
+        let listChild = document.createElement("li");   //Criando elemento lista
+        listMother.appendChild(listChild).innerText = textInput.value;  //Adicionado a filha/tarefa na lista ordenada
+        textInput.value = "";   //Limpando o campo input
+        let li = document.querySelectorAll("li");   //Selecionando as li's criadas
+        for(let listIndex = 0; listIndex < li.length; listIndex += 1){
+            li[listIndex].addEventListener("click", function(){
+                li[listIndex].classList.add("selected");    //Adicionando uma class na li selecionada
+                for(let index = 0; index < li.length; index += 1){
+                    if(li[index] !== li[listIndex]){
+                        li[index].classList.remove("selected"); //Remove a class nas demais li's
+                    }
+                }
+            })
+        }    
+    });
+    
 }
