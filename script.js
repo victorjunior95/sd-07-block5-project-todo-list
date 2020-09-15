5f57de32e3d41f8fe65c5fc0bc527fa355cc0ded
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-15 20:31:42 -0300

    botão criar tarefa

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Projeto TO Do List
+
+let taskInput = document.querySelector('#texto-tarefa');
+console.log(taskInput);
+
+let createTaskButton = document.querySelector('#criar-tarefa');
+createTaskButton.addEventListener("click",addTask);
+
+function addTask () {
+  let ol = document.getElementById("lista-tarefas");
+  let li = document.createElement("li");
+  li.innerHTML = taskInput.value;
+  ol.appendChild(li);
+}
+
