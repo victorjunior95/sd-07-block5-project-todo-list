943e8aa5aa41c10d667c6f44d38acf44f303e5db
LAURO DE SA <itrafor@gmail.com>
2020-09-15 20:15:45 -0300

    alteração 03-15092020

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function() {
     const btCriarTarefa = document.getElementById('criar-tarefa');
     const textoTarefa = document.getElementById('texto-tarefa');
     const listaTarefas = document.getElementById('lista-tarefas');
+    
 
     btCriarTarefa.addEventListener('click', function(){
         let tarefa = textoTarefa.value;
@@ -9,11 +10,28 @@ window.onload = function() {
         novaTarefa.innerText=tarefa;
         listaTarefas.appendChild(novaTarefa);
         textoTarefa.value ="";
-        textoTarefa.focus();
-        
+        textoTarefa.focus(); 
+        /*-------------------------*/
+        let todasTarefas = document.querySelectorAll('li');
+        for(let index=0; index < todasTarefas.length; index +=1){
+            localStorage.setItem(index, todasTarefas[index].innerText);
+            todasTarefas[index].addEventListener('click', function () {      
+                this.style.backgroundColor = 'rgb(128, 128, 128)';
+                localStorage.setItem(index, todasTarefas[index]);                               
+              });
+        }
+
+             
     });
 
-    
-
-
+    /*xxxxxxxxxxxxxxxxxxxxxxx*/
+    // iterate localStorage
+    //https://stackoverflow.com/questions/17745292/how-to-retrieve-all-localstorage-items-without-knowing-the-keys-in-advance
+    for (let index = 0; index < localStorage.length; index += 1) {
+        let key = localStorage.key(index);
+        let value = localStorage.getItem(key);
+        let novaTarefa = document.createElement('li');
+        novaTarefa.innerText=value;
+        listaTarefas.appendChild(novaTarefa);
+    }
   };
