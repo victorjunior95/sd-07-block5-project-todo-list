790db4b20e3a8d4d182f61a1a9ff42796058c9d1
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-14 22:18:20 -0300

    Clear button

--- script.js
+++ script.js
@@ -5,13 +5,35 @@
 // let titulo_02 = document.createTextNode(" - CFB Cursos");
 // titulo.appendChild(titulo_02);
 
-let botao = document.getElementById("criar-tarefa")
-botao.addEventListener("click",function(){
-let lista = document.getElementById("lista-tarefas");
-let itensDigitados = document.getElementById("texto-tarefa");
-let liItensDigitados = document.createElement("li");
-liItensDigitados.innerText=itensDigitados.value;
-lista.appendChild(liItensDigitados);
-itensDigitados.value="";
-itensDigitados.focus();
-});
+const botaoAdd = document.getElementById('criar-tarefa');
+const enter = document.getElementById('texto-tarefa');
+
+// Function to add products with key Enter.
+enter.addEventListener('keyup', function (enterAdd) {
+  if (enterAdd.keyCode === 13) {
+    addProduct ();
+  }
+})
+
+// Function to add products with click.
+botaoAdd.addEventListener('click', addProduct );
+
+function addProduct () {
+  const lista = document.getElementById('lista-tarefas');
+  const itensDigitados = document.getElementById('texto-tarefa');
+  const liItensDigitados = document.createElement('li');
+    liItensDigitados.innerText=itensDigitados.value;
+    lista.appendChild(liItensDigitados);
+    itensDigitados.value='';
+    itensDigitados.focus();
+};
+// Function to remove all itens list.
+const botaoClear = document.getElementById('apaga-tudo');
+botaoClear.addEventListener('click', apagaTudo);
+function apagaTudo () {
+  const lista = document.getElementById('lista-tarefas');
+    while (lista.firstChild) {
+        lista.removeChild(lista.lastChild);   
+      }
+      lista.innerHTML = '<h3>Itens Adicinados</h3>';
+}
