e0cf96473871d0b74a5b8f94b2222c963aadaf7b
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-15 19:36:38 -0300

    criado background ao item selecionado

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//função para alterar o background de um item na lista quando clicado
+
+function changeSelected(event) {
+    let item = document.querySelectorAll('li');
+
+  for (let i = 0; i < item.length;i += 1){
+    if (item[i].classList.contains('selected')){
+      let selected = document.querySelector('.selected');
+      selected.classList.remove('selected');
+    }
+  }
+      event.target.classList.add('selected');
+}
+
+// função para criar items na lista puxando o valor do input
+
 function createListItem(){
   let ordList = document.getElementById('lista-tarefas');
   let input = document.getElementById('texto-tarefa');
@@ -5,13 +21,18 @@ function createListItem(){
   let item = document.createElement('li');
 
   item.innerText = input.value;
+  item.className = 'item';
+  item.addEventListener('click', changeSelected);
 
   ordList.appendChild(item);
 
-  input = "";
+  input.value = "";
 }
 
 let addButton = document.getElementById('criar-tarefa');
 
 addButton.addEventListener('click', createListItem);
+
+
+
 
