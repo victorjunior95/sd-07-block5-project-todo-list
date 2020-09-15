5cf1627c95fb56c46d5f54e27b3f796de8107d56
ericvini <ericcobain3@gmail.com>
2020-09-15 16:03:55 -0300

    Requisito 8-finalizado

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ btnAdd.addEventListener('click', function () {
   const elementeLi = document.createElement('li');
   elementeLi.id = `id${document.getElementById('texto-tarefa').value}`;
   elementeLi.addEventListener('click', function () {
+    removeColor();
     elementeLi.style.backgroundColor = ' rgb(128, 128, 128)';
   });
   elementeLi.innerHTML = getInput;
@@ -19,3 +20,10 @@ btn.addEventListener('click', function () {
     obj.removeChild(obj.lastChild);
   }
 });
+
+function removeColor() {
+  const listaLi = document.getElementsByTagName('li');
+  for (let index = 0; index < listaLi.length; index += 1) {
+    listaLi[index].style.backgroundColor = '';
+  }
+}
