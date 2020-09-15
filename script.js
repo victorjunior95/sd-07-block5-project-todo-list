4bd7119d9e5efe06d4eab32be43935a8d6ee7a84
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-15 20:07:07 -0300

    Tenth and Eleventh mandatory requirement

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-// Add task event
-let addTask = document.querySelector('#criar-tarefa'); // Button add task
-
 // Select item with click
 function selectItem(event) {
   let previousSelectedTask = document.querySelector('.selected');
   // check if any items are selected
-  
+
   if (previousSelectedTask !== null) {
     previousSelectedTask.className = previousSelectedTask.className.replace('selected', '');
   }
 
   if (event.target.className.includes('completed')) {
     event.target.className = 'selected completed';
-  }
-  else {
+  } else {
     event.target.className = 'selected';
   }
-  
+
 }
 
-// Task completed
+// Mark task completed
 function taskCompleted(event) {
   if (event.target.className.includes('completed')) {
-    event.target.className = event.target.className.replace(' completed', '')
+    event.target.className = event.target.className.replace(' completed', '');
+  } else {
+    event.target.className += ' completed';
   }
-  else {
-    event.target.className += ' completed'
-  }
-  
+
 }
+
+// Add task event
+let addTask = document.querySelector('#criar-tarefa'); // Button add task
 
 addTask.addEventListener('click', function () {
   let tasks = document.querySelector('#lista-tarefas'); // List of all tasks to do
@@ -42,3 +40,20 @@ addTask.addEventListener('click', function () {
 
   tasks.appendChild(taskItem); // add task to list
 });
+
+// Clear list
+let clearAll = document.querySelector('#apaga-tudo')
+
+clearAll.addEventListener('click', function () {
+  document.querySelector('#lista-tarefas').innerHTML = ''
+});
+
+// Clear all completed tasks
+let clearAllCompleted = document.querySelector('#remover-finalizados')
+clearAllCompleted.addEventListener('click', function () {
+  let tasksCompleteds = document.querySelectorAll('#lista-tarefas .completed')
+  for (let index = 0; index < tasksCompleteds.length; index += 1){
+    tasksCompleteds[index].remove()
+  }
+})
+
