c1e83970d8758173fbf453b141ad96b8f9fb352c
Arnaelcio <arnaelcio.gomes@gmail.com>
2020-09-15 18:46:31 -0300

    iniciando o projeto. VAMOS COM TUDO

---
+++ script.js
@@ -0,0 +1,16 @@
+let makeTask = document.querySelector('#criar-tarefa');
+let inputTask = document.querySelector('#texto-tarefa');
+let orderedlist = document.querySelector('#lista-tarefas')
+
+ function teste () {
+    alert (inputTask.value)
+    }
+  makeTask.addEventListener('click' , makeTaskList)
+
+  function makeTaskList () {
+    let ul = document.createElement('ul');
+    ul.className = 'task-item';
+    ul.innerText = inputTask.value
+    orderedlist.appendChild(ul);
+    inputTask.value = '';
+    }
