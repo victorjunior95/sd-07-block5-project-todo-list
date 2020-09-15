bb2b799358a4634dc31b1b46927b022cc837a61b
cezar88augusto <cezar88augusto@gmail.com>
2020-09-15 18:53:58 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ let orderedListElement = document.getElementById("lista-tarefas")
 let array = [];
 
 buttonToAdd.addEventListener('click', function () {
+
   let textTasks = document.getElementById("texto-tarefa");
   array.push(textTasks.value);
   let elementLi = document.createElement("li")
-  elementLi.className = "tarefas";
-  elementLi.addEventListener('click', function(event){
+  elementLi.className = "tarefa";
+
+  elementLi.addEventListener('click', function (event) {
     event.target.style.backgroundColor = "rgb(128,128,128)";
+    elementLi.classList.add("selecionada");
   });
+
+  elementLi.addEventListener('dblclick', function(event){
+    event.target.classList.add("completed");
+  })
+
   orderedListElement.appendChild(elementLi).innerHTML = textTasks.value;
   textTasks.value = "";
 })
 
-let textTaskSetBackGroundColor = document.querySelectorAll(".tarefas");
 
+
+
+
+// for (let index = 0; index < selectedItens.length; index++) {
+//   if (index != selectedItens.length) {
+//     selectedItens[index].classList.remove("selecionada");
+//     selectedItens[index].style.backgroundColor = "rgb(173, 216, 230)"
+
+//   }
+// }
