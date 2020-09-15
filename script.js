fee87243337d4023f4d32e3a0648f5fc5a97dac0
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-15 19:16:46 -0300

    Criando li e mudança de background Color

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //criar a lista
 let listaTarefas = document.querySelector('#lista-tarefas');
 let criarTarefaBtn = document.querySelector('#criar-tarefa');
-criarTarefaBtn.addEventListener('click', function () {
-    let criarLista = document.createElement('li');
-    criarLista.innerText = document.querySelector('#texto-tarefa').value;
-    listaTarefas.appendChild(criarLista);
-    document.querySelector('#texto-tarefa').value = '';
-});
+let allItens = '';
+function criarLi () {
+    criarTarefaBtn.addEventListener('click', function () {
+        let criarLista = document.createElement('li');
+        criarLista.innerText = document.querySelector('#texto-tarefa').value;
+        listaTarefas.appendChild(criarLista);
+        document.querySelector('#texto-tarefa').value = '';
+        changeBackgroundColor()    
+    });
+}
+function changeBackgroundColor() {
+    allItens = document.querySelectorAll('li');    
+    for (let i = 0; i < allItens.length; i += 1) {
+        allItens[i].addEventListener('dblclick', function () {
+            if (allItens[i].style.backgroundColor === 'white') {
+                allItens[i].style.backgroundColor = 'rgb(128,128,128)';
+            }
+            else {
+                allItens[i].style.backgroundColor = 'white';
+            }
+        });
+    }
+}
+criarLi();
