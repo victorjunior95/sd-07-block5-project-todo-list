22ad8bd8b7abb571e8d01df640ee1b1f5e70e887
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-15 13:40:28 -0300

    testando o botão de apagar a lista

---
+++ script.js
@@ -0,0 +1,22 @@
+//essa função está encarregada de criar os intens da minha lista ao clicar no botão
+let botao = document.querySelector("#criar-tarefa");
+function criarTarefa(){
+    let lista = document.querySelector("#lista-tarefas");
+    let inputDoTexto = document.querySelector("#texto-tarefa");
+    let botao = document.querySelector("#criar-tarefa");
+    let elemento = document.createElement("li");    
+    let result =lista.appendChild(elemento);
+    let conteudo=result.innerText = inputDoTexto.value;
+}
+botao.addEventListener("click", criarTarefa);
+
+//está função está encarregada de zerar minha lista 
+function limpando (){
+let lista = document.querySelector('ol');
+let li = document.querySelectorAll("li");
+for(let i = 0; i <= li.length; i += 1){
+    let isso= li[i]
+    lista.removeChild(isso);
+}
+}
+document.querySelector("#apaga-tudo").addEventListener("click", limpando);
