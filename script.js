c2230ee909608459fa713a4ddc038f95e109f3bc
Raul <afonsoafn@gmail.com>
2020-09-15 15:56:39 +0000

    add riscado no todo completo

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let inputText = document.getElementById("texto-tarefa")
 let list = document.getElementById("lista-tarefas")
 let button = document.getElementById("criar-tarefa")
 
+// -------------- ADCIONANDO OS TODOS ---------------- //
+
 button.addEventListener("click", function() {
     let novoTodo = document.createElement('li')
     novoTodo.innerHTML = inputText.value
     list.appendChild(novoTodo)
     inputText.value = ""
     inputText.focus()
+    novoTodo.addEventListener("dblclick", duploclick)
     novoTodo.addEventListener("click", function() {    
         const previousItem = document.getElementsByClassName('selected');
         if (previousItem.length > 0) {
@@ -17,5 +20,16 @@ button.addEventListener("click", function() {
     })
 })
 
+// -------------- RISCANDO TODOS COMPLETADOS ---------------- //
 
+function duploclick(event) {
+    const eventList = event.target.classList;
+  for (let i = 0; i < eventList.length; i += 1) {
+    if (eventList[i] === 'completed') {
+      eventList.remove('completed');
+      return;
+    }
+  }
+  event.target.classList.add('completed');
+}
 
