6ecdffd9dbbf5a8ebec77dbe8b77cd3bf24bd95e
Lfocosi <luccafocosi@gmail.com>
2020-09-15 18:47:32 -0300

    selecionando itens da lista

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const CaixaTexto = document.getElementById('texto-tarefa');
 const bot = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
+const filhos = lista.children;
+function apagar() {
+  for (let index = 0; index < filhos.length; index++) {
+    filhos[index].className = 'item';
+  }
+}
+function atributo() {
+  for (let NDA = 0; NDA < filhos.length; NDA += 1) {
+    filhos[NDA].addEventListener('click', function () {
+      apagar();
+      event.target.className = 'slecionado';
+    });
+  }
+}
+
 function criarTarefa() {
   bot.addEventListener('click', function () {
     let texto = CaixaTexto.value;
@@ -8,6 +23,7 @@ function criarTarefa() {
     item.innerText = texto;
     lista.appendChild(item);
     CaixaTexto.value = '';
+    atributo();
   });
 }
 
