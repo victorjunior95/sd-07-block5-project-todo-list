b500fa6a8c75f4e60158ec227209d71d7c19f23e
LAURO DE SA <itrafor@gmail.com>
2020-09-15 18:41:57 -0300

    alteração 02-15092020 - Requisito 5

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 window.onload = function() {
-  
+    const btCriarTarefa = document.getElementById('criar-tarefa');
+    const textoTarefa = document.getElementById('texto-tarefa');
+    const listaTarefas = document.getElementById('lista-tarefas');
+
+    btCriarTarefa.addEventListener('click', function(){
+        let tarefa = textoTarefa.value;
+        let novaTarefa = document.createElement('li');
+        novaTarefa.innerText=tarefa;
+        listaTarefas.appendChild(novaTarefa);
+        textoTarefa.value ="";
+        textoTarefa.focus();
+        
+    });
 
     
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+  };
