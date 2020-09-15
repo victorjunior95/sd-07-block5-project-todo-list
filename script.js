7deaf9df047802997b4adc448797d16ce3692e55
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-15 17:54:36 -0300

    adicionada a classe selected, requisitos 7, 8 e 9

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ const list = document.querySelector("#lista-tarefas");
 const button = document.querySelector("#criar-tarefa");
 const inputText = document.querySelector("#texto-tarefa");
 
-function createListItem(){
-    let listItem = document.createElement("li");
-    listItem.className = "list-item"
+function createListItem (){
+    //creates items
+    const listItem = document.createElement("li");
     listItem.innerText = inputText.value;
     list.appendChild(listItem);
     inputText.value = '';
+    //adds gray background to selected item
+    listItem.addEventListener("click", function(){
+        for (let index = 0; index < listItem.length; index += 1){
+            if (listItem[index].classList.contains('selected')){
+            listItem[index].classList.remove('selected');
+            } 
+        }
+        event.target.classList.add('selected');
+    });
+    //marks item as completed
+    listItem.addEventListener("dblclick", function(){
+        switch (listItem){
+            case listItem.classList.contains("completed"):
+                listItem.className = '';
+            break;
+            default:
+                listItem.className = 'completed';
+        }
+    });
 }
 
 button.addEventListener("click", createListItem);
