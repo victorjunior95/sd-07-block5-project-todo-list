2cc84ff736364828d26286b559a36a10c25223ab
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-15 17:39:27 -0300

    Requisitos 4 e 5 adicionados.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const vqv = '#goTrybe!';
+const btnCreateTask = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+const taskTextInput = document.getElementById('texto-tarefa');
+
+function createTask() {
+  const newTask = document.createElement('li');
+  newTask.innerText = taskTextInput.value;
+  taskList.appendChild(newTask);
+  taskTextInput.value = '';
+}
+
+btnCreateTask.addEventListener('click', createTask);
