4b9bd3d3972a131d99c1456dc6a36abcce0a9640
luc-zago <luc.zago@hotmail.com>
2020-09-15 17:33:09 -0300

    Adicionada função que, ao se clicar em um item da lista, a cor de fundo é alterada para cinza

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+function changeBgColor() {
+  return event.target.style.backgroundColor = "rgb(128, 128, 128)";
+}
+
 function createTask() {
-    let task = document.getElementById('texto-tarefa').value;
-    let newTask = document.createElement('li');
-    newTask.innerText = task;
-    document.getElementById('texto-tarefa').value = "";
-    return document.getElementById('lista-tarefas').appendChild(newTask);
+  const task = document.getElementById('texto-tarefa').value;
+  let newTask = document.createElement('li');
+  newTask.addEventListener('click', changeBgColor);
+  newTask.innerText = task;
+  document.getElementById('texto-tarefa').value = "";
+  return document.getElementById('lista-tarefas').appendChild(newTask);
 }
 
 window.onload = function () {
