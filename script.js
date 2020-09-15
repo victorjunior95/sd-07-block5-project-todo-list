0b4416f308a612c66c2e49b01cb8ce1db7cb34b7
Raphael Caputo <faelelf2@gmail.com>
2020-09-15 18:44:54 -0300

    Adicionando botao apaga tudo

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let buttonAdd = document.getElementById('criar-tarefa');
+let buttonAdd = document.getElementById("criar-tarefa");
 buttonAdd = buttonAdd.addEventListener("click", function lista() {
-let inputAdd = document.getElementById('texto-tarefa');
-let listOl = document.getElementById('lista-tarefas');
-let createLi = document.createElement('li');
-createLi.innerText = inputAdd.value;
-listOl.appendChild(createLi);
-inputAdd.value='';
+  let inputAdd = document.getElementById("texto-tarefa");
+  let listOl = document.getElementById("lista-tarefas");
+  let createLi = document.createElement("li");
+  createLi.innerText = inputAdd.value;
+  listOl.appendChild(createLi);
+  inputAdd.value = "";
+});
 
-})
+let buttonRemove = document.getElementById("apaga-tudo");
+buttonRemove = buttonRemove.addEventListener("click", function remove() {
+  let removeLi = document.getElementById("lista-tarefas");
+  removeLi.innerHTML = "";
+});
+
+
+
