077d892e864269a403d2f84ad4a5db2092790ec1
renatomak <renato.mark.silva@gmail.com>
2020-09-15 19:58:48 -0300

    Requisito 7 - colocar cor de fundo no li selecionado

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const textoTarefa = document.getElementById('texto-tarefa');
-const criarTarefa = document.getElementById('criar-tarefa'); // botão
-const listaTarefas = document.getElementById('lista-tarefas');
+window.onload = function () {
 
+  const textoTarefa = document.getElementById('texto-tarefa');
+  const criarTarefa = document.getElementById('criar-tarefa'); // botão
+  const listaTarefas = document.getElementById('lista-tarefas');
 
-criarTarefa.addEventListener('click', function(){
+  textoTarefa.focus();
+
+  listaTarefas.addEventListener('click', function () {
+    event.stopPropagation();
+    console.log(event.target.className)
+    if (event.target.nodeName === 'LI') {
+      alteraCorFundo(event.target);
+    }
+  })
+
+  function criaLi() {
     let novoLi = document.createElement('li');
+    novoLi.className = 'lista-tarefas-item'
     novoLi.textContent = textoTarefa.value;
     listaTarefas.appendChild(novoLi);
     textoTarefa.value = '';
+    textoTarefa.focus();
+  }
 
-});
+  function alteraCorFundo(elem) {
+    elem.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+
+  criarTarefa.addEventListener('click', criaLi);
+
+  let itensLi = document.querySelectorAll('ol>li');
+}
