ff0637bc3429b89cad5d7275bbbdcecb81762af9
luc-zago <luc.zago@hotmail.com>
2020-09-15 17:58:27 -0300

    Adicionada função que, ao clicar duas vezes em um item da lista, ele recebe um risco, e caso seja novamente clicado duas vezes nesse item, ele perde o risco

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
+function check() {
+  if (event.target.className === '') {
+    event.target.classList.add('completed');
+  }
+  else {
+    event.target.classList.remove('completed');
+  }
+}
+
 function changeBgColor() {
   for (let checker = 0; checker < document.getElementsByTagName('li').length; checker += 1) {
-    let actualTask = document.getElementsByTagName('li')[checker];
-    if (actualTask.style.backgroundColor === "rgb(128, 128, 128)") {
-      actualTask.style.backgroundColor = "";
+    const actualTask = document.getElementsByTagName('li')[checker];
+    if (actualTask.style.backgroundColor === 'rgb(128, 128, 128)') {
+      actualTask.style.backgroundColor = '';
     }
   }
-  return event.target.style.backgroundColor = "rgb(128, 128, 128)";
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 function createTask() {
   const task = document.getElementById('texto-tarefa').value;
-  let newTask = document.createElement('li');
+  const newTask = document.createElement('li');
   newTask.addEventListener('click', changeBgColor);
+  newTask.addEventListener('dblclick', check);
   newTask.innerText = task;
-  document.getElementById('texto-tarefa').value = "";
+  document.getElementById('texto-tarefa').value = '';
   return document.getElementById('lista-tarefas').appendChild(newTask);
 }
 
 window.onload = function () {
-
-document.getElementById('criar-tarefa').addEventListener('click', createTask);
-
+  document.getElementById('criar-tarefa').addEventListener('click', createTask);
 };
