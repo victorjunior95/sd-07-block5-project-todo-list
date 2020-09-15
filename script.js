667f9dad0d57806dacc54620d43f0bea970c038d
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-15 18:17:29 -0300

    risco

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-//Adicionando tarefas na lista
 let listaTarefas = document.getElementById('lista-tarefas');
 let buttonAddTarefa = document.getElementById('criar-tarefa');
+
 buttonAddTarefa.addEventListener('click', function (){
+//Adicionando tarefas na lista
     let itemLista = document.createElement('li');
     itemLista.innerText = document.getElementById('texto-tarefa').value;
     listaTarefas.appendChild(itemLista);
+//Adiciona cor ao elemento clicado   
+    itemLista.addEventListener('click', function(event){
+        let selectedItem = document.querySelector('.selected');
+        if(selectedItem !== null){
+        selectedItem.classList.remove('selected');
+        }      
+        event.target.classList.add('selected')
+    });
+
+    itemLista.addEventListener('dblclick', function(event){
+        if(event.target.classList.contains('completed')){
+            let scratchedItem = document.querySelector('.completed');
+            scratchedItem.classList.remove('completed');
+        } event.target.classList.add('completed')
+    })
 });
-//Adicionar cor ao item selecionado
