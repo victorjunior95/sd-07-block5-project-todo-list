f96e6c6c74c8766f56fd0511700cd73853871624
cezar88augusto <cezar88augusto@gmail.com>
2020-09-15 19:24:07 -0300

    Requisito 9: Try 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let buttonToAdd = document.getElementById("criar-tarefa")
 let orderedListElement = document.getElementById("lista-tarefas")
+
 let array = [];
 
 buttonToAdd.addEventListener('click', function () {
@@ -8,14 +9,21 @@ buttonToAdd.addEventListener('click', function () {
   array.push(textTasks.value);
   let elementLi = document.createElement("li")
   elementLi.className = "tarefa";
+  let counter = 0;
 
   elementLi.addEventListener('click', function (event) {
     event.target.style.backgroundColor = "rgb(128,128,128)";
     elementLi.classList.add("selecionada");
   });
 
-  elementLi.addEventListener('dblclick', function(event){
-    event.target.classList.add("completed");
+  elementLi.addEventListener('dblclick', function (event) {
+    counter +=1;
+    if (counter == 1) {
+      event.target.classList.add("completed");
+    } else {
+      event.target.classList.remove("completed");
+      counter=0;
+    }
   })
 
   orderedListElement.appendChild(elementLi).innerHTML = textTasks.value;
