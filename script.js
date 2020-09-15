b9ae6bd65f94491030e4e67a38bcd5299d4aaa18
dennerpl <denner.souzapl@gmail.com>
2020-09-15 13:36:03 -0300

    adicionando elementos na lista

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("criar-tarefa").addEventListener("click", function () {
+  let inputText = document.getElementById("texto-tarefa").value;
+  let itemList = document.createElement("li");
+  itemList.innerHTML = inputText;
+  document.getElementById("lista-tarefas").appendChild(itemList);
+});
