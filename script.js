d53d61f6d9e1df6d6368a5f16e032625bc23b6f5
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-15 18:56:34 -0300

    Ninth mandatory requirement

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 // Add task event
 let addTask = document.querySelector('#criar-tarefa'); // Button add task
 
-// Event to select item with click
-function selectItem(event){
-  let previousSelectedTask = document.querySelector('.selected')
+// Select item with click
+function selectItem(event) {
+  let previousSelectedTask = document.querySelector('.selected');
   // check if any items are selected
+  
   if (previousSelectedTask !== null) {
-    previousSelectedTask.className = ''
+    previousSelectedTask.className = previousSelectedTask.className.replace('selected', '');
   }
-  event.target.className = 'selected'
+
+  if (event.target.className.includes('completed')) {
+    event.target.className = 'selected completed';
+  }
+  else {
+    event.target.className = 'selected';
+  }
+  
+}
+
+// Task completed
+function taskCompleted(event) {
+  if (event.target.className.includes('completed')) {
+    event.target.className = event.target.className.replace(' completed', '')
+  }
+  else {
+    event.target.className += ' completed'
+  }
+  
 }
 
 addTask.addEventListener('click', function () {
@@ -16,7 +35,8 @@ addTask.addEventListener('click', function () {
   let taskToDo = document.querySelector('#texto-tarefa'); // Input with the new task
 
   let taskItem = document.createElement('li');
-  taskItem.addEventListener('click', selectItem)
+  taskItem.addEventListener('click', selectItem);
+  taskItem.addEventListener('dblclick', taskCompleted);
   taskItem.innerText = taskToDo.value;
   taskToDo.value = '';
 
