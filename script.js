c2278dc2ac8e3b2f9f4504407af29a1fdaca1f23
dennerpl <denner.souzapl@gmail.com>
2020-09-15 13:54:10 -0300

    adicionando função ao botão apagar tudo

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.getElementById("criar-tarefa").addEventListener("click", function () {
   let inputText = document.getElementById("texto-tarefa").value;
   let itemList = document.createElement("li");
+  itemList.className = "listado";
   itemList.innerHTML = inputText;
   document.getElementById("lista-tarefas").appendChild(itemList);
-  document.getElementById("texto-tarefa").value = ""
+  document.getElementById("texto-tarefa").value = "";
+});
+
+document.getElementById("apaga-tudo").addEventListener("click", function () {
+  document.getElementById("lista-tarefas").innerHTML = "";
 });
