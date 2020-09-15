5be888fc6181ebf920483a3dd86a6125970958b3
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-15 16:29:33 -0300

    7. corrigido a funcao para funcionar corretamente quando um novo item e adicionado

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let buttonCreate = document.getElementById('criar-tarefa');
+
 // 5 - Deve haver um botão com id="criar-tarefa" e ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.   
 function createItem(item) {
     let liItem = document.createElement('li');
@@ -13,14 +15,18 @@ function addItem() {
     input.value = '';
 }
 
-let buttonCreate = document.getElementById('criar-tarefa');
 buttonCreate.addEventListener('click', addItem);
 
-// 7 - Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).    
-let items = document.querySelectorAll('li');
-for (index = 0; index < items.length; index += 1) {
-    let item = items[index];
-    item.addEventListener('click', function () {
-        item.style.backgroundColor = 'rgb(128, 128, 128)';
+// 7 - Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).
+//cada vez que houver um click para adicionar um item também será adicionado um eventListener nele
+
+function addEventLastLi() {
+    let lastLi = document.getElementById('lista-tarefas').lastChild;
+    lastLi.addEventListener('click', function () {
+        lastLi.style.backgroundColor = 'rgb(128, 128, 128)';
     })
 }
+
+buttonCreate.addEventListener('click', addEventLastLi);
+
+
