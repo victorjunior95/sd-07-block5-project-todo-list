6a9e947994e70bb56c28db9a041a00b135422d22
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-15 17:36:48 -0300

    Requisito 8 não ser possivel selecionar mais de uma tarefa

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const add = document.querySelector('#criar-tarefa')
 const assignment = document.querySelector('#texto-tarefa')
 
+//Adicionar lista de tarefas
+
 add.addEventListener('click', function () {
     let iten1 = assignment.value
     let list = document.querySelector('#lista-tarefas').innerHTML += `<li class='iten'>${iten1}</li>`
     assignment.value = ""
     
+// Selecionar 
+
 const listItens = document.getElementsByTagName('li')
 
 for ( let index = 0; index < listItens.length; index += 1){
@@ -13,11 +17,10 @@ for ( let index = 0; index < listItens.length; index += 1){
     
     itenSelected.addEventListener('click' ,function(){
      const selected = document.querySelector('.selected')
-
-    if(selected){
+      if(selected){
         selected.classList.remove('selected')
-    }
-    itenSelected.classList.add('selected')
+         }
+        itenSelected.classList.add('selected')
     
 })
 
@@ -33,7 +36,7 @@ itenSelected.addEventListener('dblclick' , function (){
 const removed = document.querySelector('#remover-finalizados')
 removed.addEventListener('click' ,function(){
     let list = document.querySelector('#lista-tarefas')
-    if(itenSelected.id === 'completed'){
+    if(itenSelected.classList.contains ('completed')){
         list.removeChild(itenSelected)
     }
 })
