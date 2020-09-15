7d7c7fd7c3d84d1d1b296eb4cfda7ee041b7d5f6
ericvini <ericcobain3@gmail.com>
2020-09-15 12:51:35 -0300

    Requisito 7-finalizado

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const btnAdd = document.getElementById('criar-tarefa');
 btnAdd.addEventListener('click', function () {
   const getInput = document.getElementById('texto-tarefa').value;
   const listaTarefa = document.getElementById('lista-tarefas');
-  const elementeLi=document.createElement('li');
-  elementeLi.innerHTML=getInput;
+  const elementeLi = document.createElement('li');
+  elementeLi.id = `id${document.getElementById('texto-tarefa').value}`;
+  elementeLi.addEventListener('click', function () {
+    elementeLi.style.backgroundColor = ' rgb(128, 128, 128)';
+  });
+  elementeLi.innerHTML = getInput;
   listaTarefa.appendChild(elementeLi);
-  document.getElementById('texto-tarefa').value='';
+  document.getElementById('texto-tarefa').value = '';
 });
+
