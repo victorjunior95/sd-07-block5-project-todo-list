a2a5388c957568a988f95edd457ba4ec731e6b5b
dennerpl <denner.souzapl@gmail.com>
2020-09-15 15:38:02 -0300

    adicionando as funções de select e finished

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ document.getElementById("criar-tarefa").addEventListener("click", function () {
 document.getElementById("apaga-tudo").addEventListener("click", function () {
   document.getElementById("lista-tarefas").innerHTML = "";
 });
+
+document.getElementById('lista-tarefas').addEventListener("click", function(task) {
+  let allTasks = document.querySelectorAll('.listado')
+  for (let index = 0; index < allTasks.length; index+=1) {
+        allTasks[index].classList.remove('selected')
+  }
+  task.target.classList.add('selected')
+})
+
+document.getElementById('lista-tarefas').addEventListener("dblclick", function(task) {
+  task.target.classList.toggle("finished")
+})
+
+
+
+// document.getElementById('color-palette').addEventListener("click", function(event) {
+//   if (event.target.classList.contains('selected')) {
+//     alert('Esta cor já está selecionada')
+//   } else {
+//     let paints = document.querySelectorAll('.color')
+//     for (let index = 0; index < paints.length; index++) {
+//       paints[index].classList.remove('selected')
+//     }
+//     event.target.classList.add('selected')
+//   }
+// })
