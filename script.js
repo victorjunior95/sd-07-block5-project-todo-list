708b66698358c37e4eb4a55a47425d48d59989e3
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-15 19:08:01 -0300

    Requisito 10 corrigigindo botao apaga tudo

--- script.js
+++ script.js
@@ -4,62 +4,69 @@ const assignment = document.querySelector('#texto-tarefa')
 //Adicionar lista de tarefas
 
 add.addEventListener('click', function () {
-    let iten1 = assignment.value
-     let list = document.querySelector('#lista-tarefas')
-     list.innerHTML += `<li class='iten'>${iten1}</li>`
-    assignment.value = ""
+  let iten1 = assignment.value
+  let list = document.querySelector('#lista-tarefas')
+  list.innerHTML += `<li class='iten'>${iten1}</li>`
+  assignment.value = ""
 
-// Selecionar iten da lista
+  // Selecionar iten da lista
 
-const listItens = document.querySelectorAll('li')
+  const listItens = document.querySelectorAll('li')
 
-for ( let index = 0; index < listItens.length; index += 1){
+  for (let index = 0; index < listItens.length; index += 1) {
     const itenSelected = listItens[index]
-    
-    itenSelected.addEventListener('click' ,function(){
-     const selected = document.querySelector('.selected')
-      if(selected){
+
+    itenSelected.addEventListener('click', function () {
+      const selected = document.querySelector('.selected')
+      if (selected) {
         selected.classList.remove('selected')
-         }
-        itenSelected.classList.add('selected')
-    
-})
+      }
+      itenSelected.classList.add('selected')
 
-//Marca tarefa como completa!
+    })
 
-itenSelected.addEventListener('dblclick' , function (){
-    
-    if(itenSelected.classList.contains('completed') ){
+    //Marca tarefa como completa!
+
+    itenSelected.addEventListener('dblclick', function () {
+
+      if (itenSelected.classList.contains('completed')) {
         itenSelected.classList.remove('completed')
-    }else{
-       itenSelected.classList.add('completed')
-    }
-    
-})
+      } else {
+        itenSelected.classList.add('completed')
+      }
 
-//Remover tarefa completa!
+    })
 
-const removed = document.querySelector('#remover-finalizados')
-removed.addEventListener('click' ,function(){
-    let list = document.querySelector('#lista-tarefas')
-    if(itenSelected.classList.contains ('completed')){
+    //Remover tarefa completa!
+
+    const removed = document.querySelector('#remover-finalizados')
+    removed.addEventListener('click', function () {
+      let list = document.querySelector('#lista-tarefas')
+      if (itenSelected.classList.contains('completed')) {
         list.removeChild(itenSelected)
-    }
-})
+      }
+    })
 
 
-//Apagar lista
+    //Apagar lista
 
-const clear = document.querySelector('#apaga-tudo')
+    const clear = document.querySelector('#apaga-tudo')
 
-clear.addEventListener('click' ,function(){
+    clear.addEventListener('click', function () {
+
+      list.removeChild(itenSelected)
+
+    })
+
+    const save = document.querySelector('#salvar-tarefas')
     
-    list.removeChild(itenSelected)
+    save.addEventListener('click', function () {
+        for (let index1 = 0; index1 < listItens.length; index1 += 1){
+        localStorage.setItem('Tarefas',listItens[index1].innerHTML)
+        }
+    })
+
+  }
+
+
 })
-}
-})
-
-
-    
-       
-
