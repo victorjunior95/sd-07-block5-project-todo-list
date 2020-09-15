103e0d7804275ba16ffcced92beaa9c52ff049a0
Anderson Santana <anderslash@gmail.com>
2020-09-15 17:15:50 -0300

    Função para criar itens funcionando

---
+++ script.js
@@ -0,0 +1,12 @@
+buttonCriarTarefa = document.getElementById("criar-tarefa");
+buttonCriarTarefa.addEventListener("click", criarItem);
+
+function criarItem() {
+    let inputTexto = document.getElementById("texto-tarefa");
+    if (inputTexto.value != "") {
+        let novoItem = document.createElement("li");
+        novoItem.innerText = inputTexto.value;
+        let listaTarefas = document.getElementById("lista-tarefas");
+        listaTarefas.appendChild(novoItem);
+    }
+}
