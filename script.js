2d5bf193a49a15265175451565d7e24110d090be
cezar88augusto <cezar88augusto@gmail.com>
2020-09-15 17:13:34 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ let buttonToAdd = document.getElementById("criar-tarefa")
 let orderedListElement = document.getElementById("lista-tarefas")
 let array = [];
 
-
 buttonToAdd.addEventListener('click', function () {
   let textTasks = document.getElementById("texto-tarefa");
   array.push(textTasks.value);
-  let elementLi = document.createElement("li");
+  let elementLi = document.createElement("li")
+  elementLi.className = "tarefas";
+  elementLi.addEventListener('click', function(event){
+    event.target.style.backgroundColor = "rgb(128,128,128)";
+  });
   orderedListElement.appendChild(elementLi).innerHTML = textTasks.value;
   textTasks.value = "";
 })
+
+let textTaskSetBackGroundColor = document.querySelectorAll(".tarefas");
+
