c5777e1595c8113a9da33dbf3c68c4bf430af8f9
moraisgabri <gabriel8azevedo@gmail.com>
2020-09-15 17:49:46 -0300

    adcionando completed, selected e botão apagar tudo

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
 window.onload = function () {
-  let taskInput = document.querySelector('#texto-tarefa')
-  let taskList = document.querySelector('#lista-tarefas')
-  let taskBtn = document.querySelector('#criar-tarefa')                                                                                                                                                                                                                                                                                             
-  let task = document.createElement('li')
-  
-  function createTask () {
-    let task = document.createElement('li')
-    task.innerText = taskInput.value
-    taskList.appendChild(task)
-    taskInput.value = ""
+  let taskInput = document.querySelector('#texto-tarefa');
+  let taskList = document.querySelector('#lista-tarefas');
+  let taskBtn = document.querySelector('#criar-tarefa');                                                                                                                                                                                                                                                                                           
+  let selected = document.querySelector('.selected')
+  let removeAll = document.querySelector('#apaga-tudo')
+
+  // Add task button function
+
+  taskBtn.addEventListener('click', function () {
+    // Add new task
+    let task = document.createElement('li');
+    task.innerText = taskInput.value;
+    taskList.appendChild(task);
+    taskInput.value = "";
+    
+    // Adding click event selected in task
+    task.addEventListener('click', getSelected);
+
+    // Adding double click event completed in task
+    task.addEventListener('dblclick', getCompleted);
+  });
+
+ 
+
+
+  let getSelected = function (event) {
+    if (selected !== null) {
+      selected.classList.remove('selected');
+      event.target.classList.add('selected');
+      selected = event.target
+    } else {
+      event.target.classList.add('selected');
+      selected = event.target
+    };
   }
 
-  taskBtn.addEventListener('click', createTask)
+  let getCompleted = function (event) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed')
+    } else {
+      event.target.classList.add('completed');;
+    }
+  }
 
+  let cleanAllTasks = function () {
+    for (let index = 0 ; index < taskList.childNodes.length ; index += 1) {
+      taskList.removeChild(index)
+    }
+  }
+
+  removeAll.addEventListener('click', cleanAllTasks)
+  
 }                                                                                                                                                                                                                                                                                                                                                                                                                   
