500a54a6eddaa4abd53ffe84299d9a542c5eb4ef
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-15 17:13:26 -0300

    Salvar estado da lista

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+// Restore information saved at local storage.
+document.getElementById('lista-tarefas').innerHTML = localStorage.getItem('saved');
+
 const botaoAdd = document.getElementById('criar-tarefa');
 const enter = document.getElementById('texto-tarefa');
 // Function to add products with click.
 function addProduct() {
-const lista = document.getElementById('lista-tarefas');
-const itensDigitados = document.getElementById('texto-tarefa');
-const liItensDigitados = document.createElement('li');
+  const lista = document.getElementById('lista-tarefas');
+  const itensDigitados = document.getElementById('texto-tarefa');
+  const liItensDigitados = document.createElement('li');
   liItensDigitados.innerText = itensDigitados.value;
+  liItensDigitados.className = 'itens-list';
   lista.appendChild(liItensDigitados);
   itensDigitados.value = '';
   itensDigitados.focus();
@@ -21,8 +25,8 @@ enter.addEventListener('keyup', function (enterAdd) {
 // Function to remove all itens list.
 const botaoClear = document.getElementById('apaga-tudo');
 function apagaTudo() {
-const lista = document.getElementById('lista-tarefas');
-while (lista.firstChild) {
+  const lista = document.getElementById('lista-tarefas');
+  while (lista.firstChild) {
   lista.removeChild(lista.lastChild);
 }
   lista.innerHTML = '<h3>Itens Adicinados</h3>';
@@ -32,13 +36,13 @@ botaoClear.addEventListener('click', apagaTudo);
 const itensList = document.querySelector('ol');
 itensList.addEventListener('click', function (itensList) {
   if (itensList.target.tagName === 'LI') {
-  itensList.target.classList.toggle('selected');
+    itensList.target.classList.toggle('selected');
   }
 });
 // Function to mark as completed.
 itensList.addEventListener('dblclick', function (itensList) {
   if (itensList.target.tagName === 'LI') {
-  itensList.target.classList.toggle('completed');
+    itensList.target.classList.toggle('completed');
   }
 });
 // Function to remove finished item.
@@ -46,7 +50,7 @@ const removerFinalizados = document.querySelector('#remover-finalizados');
 removerFinalizados.addEventListener('click', function () {
   const linhas = document.querySelectorAll('.completed');
   for (let count = 0; count < linhas.length; count += 1) {
-  linhas[count].remove();
+    linhas[count].remove();
   }
 });
 // Function to remove completed item.
@@ -57,3 +61,12 @@ removerSelecionado.addEventListener('click', function () {
   linhas[count].remove();
   }
 });
+
+const saveTask = document.getElementById('salvar-tarefas');
+saveTask.addEventListener('click', function () {
+  localStorage.setItem('saved', document.getElementById('lista-tarefas').innerHTML);
+});
+const automaticSave = function () {
+  localStorage.setItem('saved', document.getElementById('lista-tarefas').innerHTML);
+}
+window.onchange = automaticSave;
