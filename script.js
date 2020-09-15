5384297c4cb6e635cb80628d92b3471210039ae3
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-15 17:20:38 -0300

    Requirement 11 - Cleaning finalized items

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const click = document.getElementById('criar-tarefa');
+const item = document.getElementById('lista-tarefas');
+const cleanAll = document.querySelector('#apaga-tudo');
+const cleanFinalized = document.querySelector('#remover-finalizados');
 
 click.addEventListener("click", function() {
   let textInput = document.getElementById('texto-tarefa').value;  
@@ -9,9 +12,6 @@ click.addEventListener("click", function() {
   document.getElementById('texto-tarefa').value = "";
 });
 
-
-const item = document.getElementById('lista-tarefas');
-
 item.addEventListener("click", function() {
 
   if (event.target.className === 'completed') {
@@ -21,17 +21,11 @@ item.addEventListener("click", function() {
     event.target.className = 'selected';
     event.target.style.backgroundColor = 'rgb(128, 128, 128';
 
-    
-    
-
     let completedLenth = document.querySelectorAll('#lista-tarefas .completed').length;
-
     for (index = 0; index < completedLenth; index += 1) {
       document.getElementsByClassName('completed')[index].style.backgroundColor = 'white';
-
     }
     
-
   } else {
     document.getElementsByClassName('selected')[0].style.backgroundColor = 'white';
     document.getElementsByClassName('selected')[0].className = undefined;
@@ -48,10 +42,19 @@ item.addEventListener("dblclick", function() {
   }
 });
 
-document.getElementById('apaga-tudo').addEventListener('click', function() {
-  let list = document.getElementById("lista-tarefas")
+cleanAll.addEventListener('click', function() {
+  let list = document.getElementById("lista-tarefas");
   let listLength = list.getElementsByTagName("li").length;
   for (let index = 0; index < listLength; index += 1) {
     list.removeChild(list.childNodes[0]);
   }
 });
+
+cleanFinalized.addEventListener('click', function() {
+  document.querySelectorAll('.completed').forEach(function(a) {
+    a.remove();
+  });
+});
+
+    
+
