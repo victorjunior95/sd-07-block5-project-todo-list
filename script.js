f5446db947cd13e856da4c4c86b8c66c795436d4
Breno Alves <brenoalv3ss@gmail.com>
2020-09-16 20:42:29 -0300

    Finalização das funcionalidades do projeto

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-let listaTarefas = document.querySelector('#lista-tarefas');
-let criarTarefaBotton = document.querySelector('#criar-tarefa');
-let apagaTudoBotton = document.querySelector('apaga-tudo');
-let removerFinalizadosBotton = document.querySelector('#remover-finalizados')
+const listaTarefas = document.querySelector('#lista-tarefas');
+const criarTarefaBtn = document.querySelector('#criar-tarefa');
+const apagarTudoBtn = document.querySelector('#apaga-tudo');
+const apagarFinalizadosBtn = document.querySelector('#remover-finalizados');
+const removerSelecionadoBtn = document.querySelector('#remover-selecionado');
 let itemSelect = '';
-let lista = '';
+let paiLista = '';
 let finalizados = '';
-function criarLi(){
-    criarTarefaBotton.addEventListener('click',function(){
-        let li = document.createElement('li');
-        li.innerText = document.querySelector('#texto-tarefa').value;
-        li.addEventListener('click',function(){
-            intemSelect = document.querySelector('.selected');
-            if (itemSelect != null) {
-                itemSelect.classList.remove('selected');
-            }
-            li.classList.add('selected');
-            itemSelect = li;
-            console.log(itemSelect);
-        });
-        li.addEventListener('dblclick', function () {
-            if (li.classList.contains('completed')) {
-              li.classList.remove('completed');
-            } else {
-              li.classList.add('completed');
-            }
-          }); 
-          listaTarefas.appendChild(li);
-          document.querySelector('#texto-tarefa').value = '';
+function criarLi() {
+  criarTarefaBtn.addEventListener('click', function () {
+    const li = document.createElement('li');
+    li.innerText = document.querySelector('#texto-tarefa').value;
+    li.addEventListener('click', function () {
+      itemSelect = document.querySelector('.selected');
+      if (itemSelect != null) {
+        itemSelect.classList.remove('selected');
+      }
+      li.classList.add('selected');
+      itemSelect = li;
     });
+    li.addEventListener('dblclick', function () {
+      if (li.classList.contains('completed')) {
+        li.classList.remove('completed');
+      } else {
+        li.classList.add('completed');
+      }
+    });
+    listaTarefas.appendChild(li);
+    document.querySelector('#texto-tarefa').value = '';
+  });
 }
+function apagarLi() {
+    paiLista = document.querySelector('li').parentElement;
+    while (paiLista.hasChildNodes()) {
+        paiLista.removeChild(paiLista.childNodes[0]);
+    }
+}
+apagarTudoBtn.addEventListener('click', apagarLi);
+function apagarFinalizados() {
+  finalizados = document.querySelectorAll('.completed');
+  for (let i = 0; i < finalizados.length; i += 1) {
+    finalizados[i].remove();
+  }    
+}
+apagarFinalizadosBtn.addEventListener('click', apagarFinalizados);
+criarLi();
+function removerSelecionado() {
+    itemSelect = document.querySelector('.selected');
+    itemSelect.remove();
+  }
+  removerSelecionadoBtn.addEventListener('click', removerSelecionado);
+  criarLi();
