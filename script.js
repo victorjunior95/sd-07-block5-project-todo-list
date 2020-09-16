a4f7b3be80d7dac5dc8e139f669bbb1836473b65
Raphael Caputo <faelelf2@gmail.com>
2020-09-15 22:00:06 -0300

    Testando nova funçao

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ buttonAdd = buttonAdd.addEventListener("click", function lista() {
   createLi.innerText = inputAdd.value;
   listOl.appendChild(createLi);
   inputAdd.value = "";
-  createLi.setAttribute('class', 'newLi');
-  let paintLi = document.querySelector('.newLi');
-  
 });
 
 let buttonRemove = document.getElementById("apaga-tudo");
@@ -18,4 +15,23 @@ buttonRemove = buttonRemove.addEventListener("click", function remove() {
 });
 
 
+let listLi = document.querySelector("#lista-tarefas");
+function changeColorJob() {
+  listLi.addEventListener("click", function (event) {
+    
+    let li = event.target;
+    let children = listLi.children;
+    children = document.querySelector("li").classList.add('selected');
+    
+    for (let i in children) {
+      children[i].style.backgroundColor = "";
+    }
+    
+    li.style.backgroundColor = "rgb(128, 128, 128)";
+    
+  });
+}
+changeColorJob();
 
+  // classList.remove("selected")
+  // classList.add("selected");
