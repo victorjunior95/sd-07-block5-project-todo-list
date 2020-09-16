022ac6382040fb6b4d2e1876974bc52b2e9eb3ec
jpaula <paula.jfe@gmail.com>
2020-09-15 22:49:16 -0300

    Requisitos 1 a 4

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.onload = function () {
+    let inputText = document.querySelector("#texto-tarefa");
+    let createJob = document.querySelector("#criar-tarefa");
+    let jobList = document.querySelector("#lista-tarefas");
+    let clearAll = document.querySelector("#apaga-tudo");
+    let removeDone = document.querySelector("#remover-finalizados");
+    let saveJobs = document.querySelector("salvar-tarefas");
+    let moveTop = document.querySelector("mover-cima");
+    let moveBottom = document.querySelector("mover-baixo");
+    let removeSelected = document.querySelector("remover-selecionado");
 
-    
 }
