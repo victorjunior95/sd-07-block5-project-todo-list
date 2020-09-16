fbbd0bbc00aa8bc2df2da03fc5d29869e95cc67c
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-16 18:51:57 -0300

    passar pelo code climate

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 //essa função está encarregada de criar os intens da minha lista ao clicar no botão
 let botao = document.querySelector("#criar-tarefa");
-function criarTarefa(){
+function criarTarefa() {
     let lista = document.querySelector("#lista-tarefas");
     let inputDoTexto = document.querySelector("#texto-tarefa");
     let botao = document.querySelector("#criar-tarefa");
-    let elemento = document.createElement("li");    
-    let result =lista.appendChild(elemento);
-    elemento.addEventListener('click',itemSelecionado);
+    let elemento = document.createElement("li");
+    let result = lista.appendChild(elemento);
+    elemento.addEventListener('click', itemSelecionado);
     elemento.addEventListener("dblclick", itemCompleto)
-    let conteudo=result.innerText = inputDoTexto.value;
+    let conteudo = result.innerText = inputDoTexto.value;
     inputDoTexto.value = "";
 }
 botao.addEventListener("click", criarTarefa);
 
 //está função está encarregada de zerar minha lista 
-function limpando (){
-let lista = document.querySelector('ol');
-let li = document.querySelectorAll("li");
-for(let i = 0; i < li.length; i += 1){
-    let isso= li[i]
-    lista.removeChild(isso);
-}
+function limpando() {
+    let lista = document.querySelector('ol');
+    let li = document.querySelectorAll("li");
+    for (let i = 0; i < li.length; i += 1) {
+        let isso = li[i]
+        lista.removeChild(isso);
+    }
 }
 document.querySelector("#apaga-tudo").addEventListener("click", limpando);
 
 //esta função será encarregada de mudar a cor de fundo dos itens da lista 
 let item = document.querySelectorAll("li");
-function itemSelecionado (){
+function itemSelecionado() {
     let selecao = document.querySelector(".selecionado");
-    if(selecao !== null){
+    if (selecao !== null) {
         selecao.classList.remove("selecionado");
     }
     event.target.classList.add("selecionado");
 }
 
-function itemCompleto (event){
-        let i = event.target.classList.contains("completed");
-        if(i === true){
-            event.target.classList.remove("completed");
-        }
-        else{
+function itemCompleto(event) {
+    let i = event.target.classList.contains("completed");
+    if (i === true) {
+        event.target.classList.remove("completed");
+    }
+    else {
         event.target.classList.add("completed");
-        }
+    }
 }
 
 /*function removeFinaliados(){
