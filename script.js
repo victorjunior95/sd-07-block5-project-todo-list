ec1a76767c6f6465ee12bf88e2b17a37c3093405
Lucas Miranda <lucasmirandadev@gmail.com>
2020-09-16 20:37:33 -0300

    requisito 10, botão apagaTudo criado e deixa a lista de tarefas vazia

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const listaTarefas = document.getElementById("lista-tarefas"); 
 const textoTarefa = document.getElementById("texto-tarefa");
 const btnCriarTarefa = document.getElementById("criar-tarefa");
+const apagaTudo = document.getElementById("apaga-tudo")
+const salvarTarefas = document.getElementById('#salvar-tarefas');
 
-btnCriarTarefa.addEventListener('click', function() {
+
+btnCriarTarefa.addEventListener('click', addProduto);
+    
+
+function addProduto() {
     var item = '<li>' + textoTarefa.value + '</li>';
     listaTarefas.innerHTML += item;
     textoTarefa.value = '';
     textoTarefa.focus();
-})
+};
 
-    
+
+
+apagaTudo.addEventListener('click', function () {
+    listaTarefas.innerHTML = '';
+  });
+
