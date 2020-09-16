387bd0f915c26ca12de6cd100e552417be1c6bf5
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-09-16 15:21:39 -0300

    addign events to the items

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const inputText = document.querySelector('#texto-tarefa');
 
 addButton.addEventListener('click', addTask);
 
+document.querySelectorAll('li').forEach((item) => {
+  document.querySelectorAll('li')[item].addEventListener('click', changeItemBackground);
+});
+
 function addTask() {
   const addedItem = document.createElement('li');
   addedItem.innerText = inputText.value;
   taskList.appendChild(addedItem);
   inputText.value = '';
+}
+
+function changeItemBackground() {
+event.
 }
