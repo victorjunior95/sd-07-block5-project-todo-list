6baac7457159bb095e4398f8f3eb7ea181524d76
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-16 18:28:30 -0300

    reformatando html(estava bloqueando o JS)...intens 1,2,3 e 4

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let criarTarefa = document.getElementById("criar-tarefa");
+let texto = document.querySelector("#texto-tarefa");
+let criar = document.querySelector("#criar-tarefa");
+let lista = document.querySelector("#lista-tarefas");
+
+criar.addEventListener("click", function() {
+    let li = document.createElement("li");
+    li.innerText = texto.value;
+    lista.appendChild(li);
+});
