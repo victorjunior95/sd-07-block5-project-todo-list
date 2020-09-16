e88f9471cbe4bdf7920df7d0a5ef22d122f8802e
Leonnardo Lair <leonnardo0316@gmail.com>
2020-09-16 19:54:57 -0300

    more steps

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-let listOrdered = document.querySelector("#lista-tarefas")
+let listOrdered = document.querySelector("#lista-tarefas");
+let selectedjob = document.querySelector(".selected");
 
 document.querySelector("#criar-tarefa").addEventListener("click", function(){
     let inputText = document.createTextNode(document.querySelector("#texto-tarefa").value);
     let listItem = document.createElement("li");
+    selectedjob = document.querySelector(".selected");
     listItem.appendChild(inputText);
     listItem.classList.add("list-item");
     listItem.addEventListener("click",function(){
         if (listItem.classList[1] == "selected") {
             listItem.classList.remove("selected");
         } else {
-            listItem.classList.add("selected");
+            if (selectedjob == null ) {
+            listItem.classList.add("selected")
+            selectedjob = document.querySelector(".selected");
+            } else {
+            selectedjob.classList.remove("selected");
+            listItem.classList.add("selected"); 
+            selectedjob = document.querySelector(".selected");   
+            }
         }
     })
     listOrdered.appendChild(listItem);
     document.querySelector("#texto-tarefa").value = ""
-    
 })
