d2eef4b77cbee01aec4520207b2c4a9ec0ebd691
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-16 16:20:31 -0300

    Implementado a funcionalidade de gravar e recuperar os itens da lista quando a página é recarregada

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload=function(){
     let botaoAdicionar=document.getElementById('criar-tarefa');
     let botaoApagaTudo=document.getElementById('apaga-tudo');
     let botaoApagaFinalizados=document.getElementById('remover-finalizados');
+    let botaoSalvaLista=document.getElementById('salvar-tarefas');    
     let novaTarefa=document.getElementById('texto-tarefa');
     let listaTarefas=document.getElementById('lista-tarefas');    
 
@@ -12,7 +13,7 @@ window.onload=function(){
     })
 
     botaoApagaFinalizados.addEventListener('click',function(){
-        for(i=listaTarefas.childElementCount-1; i>=0; i-=1){
+        for(let i=listaTarefas.childElementCount-1; i>=0; i-=1){
             if (listaTarefas.children[i].classList.contains('completed')){
                 listaTarefas.children[i].remove();
             }
@@ -20,10 +21,50 @@ window.onload=function(){
     })
 
     botaoApagaTudo.addEventListener('click',function(){
-        for(i=listaTarefas.childElementCount-1; i>=0; i-=1){
+        for(let i=listaTarefas.childElementCount-1; i>=0; i-=1){
             listaTarefas.children[i].remove();
         }
     })
+    
+    botaoSalvaLista.addEventListener('click', function(){
+        localStorage.clear();
+        //alert("entrei na function");
+        let todosItens=document.getElementsByTagName('li');
+            for(let index=0; index<todosItens.length; index+=1){
+                //alert("entrei no for " + index);
+                //localStorage.setItem(`item${index+1}-posicao`, index);
+                localStorage.setItem(`item${index+1}-texto`, todosItens[index].innerText);
+                if (todosItens[index].classList.contains('completed')){
+                    localStorage.setItem(`item${index+1}-classe`, 'completed');
+                }
+                //console.log(todosItens[index].innerText)
+            }
+    })
+
+    loadList();
+
+    function loadList(){
+        for(let i=1; i<=localStorage.length; i+=1){
+            let texto='';
+            let classe='';
+                if(localStorage.getItem(`item${i}-texto`)){
+                    texto=localStorage.getItem(`item${i}-texto`);
+                        if(localStorage.getItem(`item${i}-classe`)){
+                            classe=localStorage.getItem(`item${i}-classe`)
+                        }
+                }
+            if (texto.length > 0 || classe.length > 0){                
+                let adicionaTarefa=document.createElement('li');
+                if (texto!==''){
+                    adicionaTarefa.innerText=texto;                    
+                }
+                if (classe!==''){
+                    adicionaTarefa.classList.add(classe);
+                }
+                listaTarefas.appendChild(adicionaTarefa);
+            }
+        }
+    }
 
     function adicionaTarefa(tarefa){
         let adicionaTarefa=document.createElement('li');
@@ -38,7 +79,7 @@ window.onload=function(){
         let item=event.target
 
         //Limpa o nome de classes dos itens da lista
-        for(i=0; i<listaTarefas.childElementCount; i+=1){
+        for(let i=0; i<listaTarefas.childElementCount; i+=1){
             if (listaTarefas.children[i].classList.contains('selecionado')){
                 listaTarefas.children[i].classList.remove('selecionado')
             }
