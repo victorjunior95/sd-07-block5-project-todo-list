e517d331d9c3cf7a875dfad3f203cf1357f05c6a
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-16 13:34:39 -0300

    Passei mal

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const addTarefa = document.querySelector('#criar-tarefa');
 const inputTarefa = document.querySelector('#texto-tarefa');
 const list = document.querySelector('#lista-tarefas');
+const apagar = document.querySelector('#apaga-tudo')
 
 addTarefa.addEventListener('click', function () {
     const elementLi = document.createElement('li');
     elementLi.innerText = inputTarefa.value;
     elementLi.className = 'task';
+    elementLi.classList.add('empty');
     list.appendChild(elementLi);
     addColor();
+    completed();
     inputTarefa.value = ''
+    emptyCheck();
 })
 
+apagar.addEventListener('click', function () {
+    while(list.firstElementChild){
+        list.removeChild(list.firstElementChild)
+    }
+})
+
+
 function addColor () {
-    const item = document.querySelectorAll('li')
+    const item = document.querySelectorAll('.empty')
     item.forEach(key => {
         key.addEventListener('click', function () {
             removeColor()
             key.style.backgroundColor = 'rgb(128 , 128 , 128)';
-            completed ()
         })
 })
 }
@@ -30,17 +40,28 @@ function removeColor () {
 }
 
 function completed () {
-    const item = document.querySelectorAll('.task')
-    item.forEach(key =>{
-    key.addEventListener('dblclick', function () {
+
+    let item = document.querySelectorAll('.empty')
+    item.forEach(key => {
+    key.addEventListener('dblclick', function addEvent () {
+        key.classList.remove('empty')
         key.classList.add('completed');
-        if(key.style.textDecoration == 'line-through'){
+        if(key.style.textDecoration == 'line-through solid black'){
             key.classList.remove('completed')
             key.style.textDecoration = 'none';
         }
         else {
-            key.style.textDecoration = 'line-through';
+            key.style.textDecoration = 'line-through solid black';
         }
         })
     })
+}
+
+function emptyCheck () {
+    let item = document.querySelectorAll('.empty')
+    if(document.querySelectorAll('.empty')){
+        item.forEach(key =>{
+            key.classList.remove('empty')
+        })
+    }
 }
