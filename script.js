3f718344ab66111bfde0b8abcbb44a7c0f488d87
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-09-15 22:07:47 -0300

    Adicionado background ao clicar na task, Through line ao dar double click nas tasks e adicionado botoes de apagar todas as taks, e concluir tasks marcadas

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let pressButton = document.querySelector("#criar-tarefa");
+let pressButtonCreate = document.querySelector("#criar-tarefa");
 let inputValue = document.querySelector("#texto-tarefa");
 let itemList = document.querySelectorAll("li");
+let pressButtonErase = document.querySelector("#apaga-tudo");
+let pressBUttonCompleteTask = document.querySelector("#remover-finalizados")
 
-pressButton.addEventListener('click', function(){
+pressButtonCreate.addEventListener('click', function(){
     if(inputValue.value != ""){
         let newItem = document.createElement('li');
         newItem.innerText = inputValue.value;
         document.querySelector('#lista-tarefas').appendChild(newItem);
         inputValue.value = "";
+        newItem.addEventListener("click", addBackgroundItem);
+        newItem.addEventListener("dblclick", addLineThroughItem);
         itemList = document.querySelectorAll("li");
-        addBackgroundItem();
+        
     }
 });
 
-function eraseBackground(){
+pressBUttonCompleteTask.addEventListener('click', function(){
+    for (let index = 0; index < itemList.length; index += 1){
+        if(itemList[index].className === "completed"){
+            itemList[index].remove();
+        }
+    }
+});
+
+pressButtonErase.addEventListener('click', function(){
+    for (let index = 0; index < itemList.length; index += 1){
+        itemList[index].remove();
+    }
+});
+
+function eraseBackground (){
     for(let index = 0; index < itemList.length; index += 1){
         if(itemList[index].style.backgroundColor === 'rgb(128, 128, 128)'){
             itemList[index].style.backgroundColor = "white";
@@ -22,14 +40,19 @@ function eraseBackground(){
 }
 
 function addBackgroundItem (){
-    for(let index = 0; index < itemList.length; index += 1){
-        itemList[index].addEventListener('click', function(){
-            eraseBackground();   
-        if(itemList[index].style.backgroundColor != 'rgb(128, 128, 128)'){     
-            itemList[index].style.backgroundColor = 'rgb(128, 128, 128)';
-        }else {
-            itemList[index].style.backgroundColor = "white";
-        }  
-        });
-    }
+    eraseBackground();   
+    if(event.target.style.backgroundColor != 'rgb(128, 128, 128)'){       
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }else {
+        event.target.style.backgroundColor = "white";
+    }  
+    
+}
+
+function addLineThroughItem (){
+    if(event.target.className != "completed"){   
+        event.target.classList.add("completed");
+    }else {
+        event.target.classList.remove("completed");
+     }
 }
