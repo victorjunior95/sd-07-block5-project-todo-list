d71031bdbcc77bf1a754acbbe7e8b0a6d88ba35e
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-09-15 23:44:59 -0300

    Opção de selecionar apenas um item por vez

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let textoTarefa = document.getElementById("texto-tarefa").value;
 let botaoAdicionar = document.getElementById("criar-tarefa");
 let listaTarefas = document.getElementById("lista-tarefas");
+let itensLista = document.getElementsByClassName("selecionado")
 
 botaoAdicionar.addEventListener("click", function() {
     let textoTarefa = document.getElementById("texto-tarefa").value;
     let elementoLista = document.createElement("li");
     elementoLista.innerHTML= textoTarefa;
-    listaTarefas.appendChild(elementoLista)
-    document.getElementById("texto-tarefa").value = ""
+    elementoLista.classList.add("item");
+    listaTarefas.appendChild(elementoLista);
+    document.getElementById("texto-tarefa").value = "";
+        elementoLista.addEventListener("click", function() {
+            if (itensLista.length > 0) {
+                for (index = 0; index < itensLista.length; index += 1) {
+                itensLista[index].classList.remove("selecionado")};
+            }
+            elementoLista.classList.add("selecionado")
+        })
 })
+
