c74eee7481cf6acadfda525e1e27c74c4fcaf2de
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-16 12:22:19 -0300

    requisito 7 e 8 envio 4

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const inputTextToDo = document.querySelector(".texto-tarefa");
 const buttonCreateToDo = document.querySelector(".criar-tarefa");
 let olToDoList = document.querySelector(".lista-tarefas");
-let liCount = 0;
 
 function clearInputTextToDo() {
   inputTextToDo.value = "";
@@ -12,20 +11,18 @@ buttonCreateToDo.addEventListener("click", function () {
   li.innerText = inputTextToDo.value;
   olToDoList.appendChild(li);
   clearInputTextToDo();
-  liCount += 1;
   olToDoList.addEventListener("click", changeBGColorListItem);
 });
 
 function changeBGColorListItem(event) {
   for (let index = 0; index < olToDoList.childElementCount; index += 1) {
     olToDoList.children[index].addEventListener("click", function (event) {
-      console.log('entrou')
-      olToDoList.children[index].style.backgroundColor = "rgb(128, 128, 128)";
       for (let j = 0; j < olToDoList.childElementCount; j += 1) {
         if (olToDoList.children[j] !== event.target) {
           olToDoList.children[j].style.backgroundColor = "";
         }
       }
+      olToDoList.children[index].style.backgroundColor = "rgb(128, 128, 128)";
     });
   }
 }
