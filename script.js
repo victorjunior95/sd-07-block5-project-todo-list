ce675ba8e3d7ea728f46137d43c130780fe79dcc
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-15 22:01:27 -0300

    requisito 7 e 8 envio 1

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const inputTextToDo = document.querySelector(".texto-tarefa");
-const buttonCreateToDo = document.querySelector(".criar-tarefa");
+const inputTextToDo = document.querySelector('.texto-tarefa');
+const buttonCreateToDo = document.querySelector('.criar-tarefa');
+const olToDoList = document.querySelector('.lista-tarefas');
 
 function clearInputTextToDo() {
   inputTextToDo.value = '';
 }
 
-buttonCreateToDo.addEventListener("click", function () {
-  const li = document.createElement("li");
+buttonCreateToDo.addEventListener('click', function () {
+  const li = document.createElement('li');
   li.innerText = inputTextToDo.value;
-  document.querySelector(".lista-tarefas").appendChild(li);
+  olToDoList.appendChild(li);
   clearInputTextToDo();
 });
+
+const liCount = olToDoList.children;
+
+for (let index = 0; index < liCount.length; index += 1) {
+    liCount[index].addEventListener('click', function (event) {
+    liCount[index].style.backgroundColor = 'rgb(128, 128, 128)'
+    for (let j = 0; j < liCount.length; j += 1) {
+        if (liCount[j] !== event.target) {
+            liCount[j].style.backgroundColor = ''
+        }
+    }
+  })
+  
+}
