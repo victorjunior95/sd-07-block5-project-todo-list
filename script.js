2eab937806fda8624b17d31c7aa176a1c72ee7f1
richardfazeredo <richardcompletetour@gmail.com>
2020-09-16 13:29:21 -0300

    Refatorando funcionalidade de atribuir cor de fundo a um elemento clicado

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 let criarTarefa = document.getElementById('criar-tarefa');
-let listaTarefas = document.getElementById('lista-tarefas');
-let campoToDo = document.querySelector('input');
 
 // window.onload = renderizarTodos (); 
 
 function renderizarTodos (novoToDo) {
+  let listaTarefas = document.getElementById('lista-tarefas');
   let itemLista = document.createElement('li');
   listaTarefas.appendChild(itemLista)
   .setAttribute('class', 'todo');
   itemLista.innerText = novoToDo;
+  return itemLista;
 };
 
 criarTarefa.addEventListener('click', function () {
+  let campoToDo = document.querySelector('input');
   let outroToDo = campoToDo.value;
   campoToDo.value = '';
-  renderizarTodos(outroToDo);
-  let itemLista = document.querySelectorAll('li');
+  let itemLista = renderizarTodos(outroToDo);
   
-  for(let item of itemLista) {
-    item.addEventListener('click', function () {
-      item.style.backgroundColor = 'rgb(128, 128, 128)';
-    }); 
-  };
+  itemLista.addEventListener('click', function () {
+    itemLista.style.backgroundColor = 'rgb(128, 128, 128)';
+  }); 
 });
+
+
+
+
+
+
 
 
 
