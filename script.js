e0f8053525b21dd0f1aa57994c4df3ffedac9955
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-16 11:52:13 -0300

    requisito 7 e 8 envio 3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputTextToDo = document.querySelector(".texto-tarefa");
 const buttonCreateToDo = document.querySelector(".criar-tarefa");
-const olToDoList = document.querySelector(".lista-tarefas");
+let olToDoList = document.querySelector(".lista-tarefas");
+let liCount = 0;
 
 function clearInputTextToDo() {
   inputTextToDo.value = "";
@@ -11,17 +12,20 @@ buttonCreateToDo.addEventListener("click", function () {
   li.innerText = inputTextToDo.value;
   olToDoList.appendChild(li);
   clearInputTextToDo();
+  liCount += 1;
+  olToDoList.addEventListener("click", changeBGColorListItem);
 });
 
-const liCount = olToDoList.children;
-
-for (let index = 0; index < liCount.length; index += 1) {
-  liCount[index].addEventListener("click", function (event) {
-    liCount[index].style.backgroundColor = "rgb(128, 128, 128)";
-    for (let j = 0; j < liCount.length; j += 1) {
-      if (liCount[j] !== event.target) {
-        liCount[j].style.backgroundColor = "";
+function changeBGColorListItem(event) {
+  for (let index = 0; index < olToDoList.childElementCount; index += 1) {
+    olToDoList.children[index].addEventListener("click", function (event) {
+      console.log('entrou')
+      olToDoList.children[index].style.backgroundColor = "rgb(128, 128, 128)";
+      for (let j = 0; j < olToDoList.childElementCount; j += 1) {
+        if (olToDoList.children[j] !== event.target) {
+          olToDoList.children[j].style.backgroundColor = "";
+        }
       }
-    }
-  });
+    });
+  }
 }
