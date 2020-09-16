bf4820ad32f89858ffab3680a805c637828ecfcc
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-16 16:07:25 -0300

    Fix codeclimate

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-//criar a lista
-let listaTarefas = document.querySelector('#lista-tarefas');
-let criarTarefaBtn = document.querySelector('#criar-tarefa');
-let allItens = '';
+// criar a lista
+const listaTarefas = document.querySelector('#lista-tarefas');
+const criarTarefaBtn = document.querySelector('#criar-tarefa');
 let itemSelect = '';
-let lineThrough = '';
-function criarLi () {
-    criarTarefaBtn.addEventListener('click', function () {
-        let li = document.createElement('li');
-        li.innerText = document.querySelector('#texto-tarefa').value;
-        li.addEventListener('click', function () {
-            itemSelect = document.querySelector('.selected');
-            if (itemSelect != null) {
-                itemSelect.classList.remove('selected');
-            }
-            li.classList.add('selected');
-            itemSelect = li;
-        });
-        li.addEventListener('dblclick', function () {
-            if (li.classList.contains('completed')) {
-                li.classList.remove('completed')
-            }
-            else {
-                li.classList.add('completed');
-            }
-        });
-        listaTarefas.appendChild(li);
-        document.querySelector('#texto-tarefa').value = '';
+function criarLi() {
+  criarTarefaBtn.addEventListener('click', function () {
+    const li = document.createElement('li');
+    li.innerText = document.querySelector('#texto-tarefa').value;
+    li.addEventListener('click', function () {
+      itemSelect = document.querySelector('.selected');
+      if (itemSelect != null) {
+        itemSelect.classList.remove('selected');
+      }
+      li.classList.add('selected');
+      itemSelect = li;
     });
+    li.addEventListener('dblclick', function () {
+      if (li.classList.contains('completed')) {
+        li.classList.remove('completed');
+      } else {
+      li.classList.add('completed');
+      }
+    });
+    listaTarefas.appendChild(li);
+    document.querySelector('#texto-tarefa').value = '';
+  });
 }
 criarLi();
