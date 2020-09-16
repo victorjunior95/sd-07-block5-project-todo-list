278cd5e0c4a7739df88511af89949e5bd0b70686
Galaraz <jvictor.si.2013@gmail.com>
2020-09-16 15:31:01 -0300

    funçao to do list sendo implementada js

--- script.js
+++ script.js
@@ -5,30 +5,108 @@ class TodoListController {
     this.geradorDeId = 0;
   }
 
+  lerTarefa() {
+    let tarefa = {};
+    tarefa.descricao = document.getElementById("texto-tarefa").value;
+    tarefa.concluida = false;
 
-lerTarefa(){}
+    return tarefa;
+  }
 
-validar(tarefa){}
+  validar(tarefa) {
+    if (tarefa.descricao == "") {
+      alert("O campo de descrição da tarefa é obrigatório!");
+      return false;
+    }
 
-adicionarTarefa(tarefa) {
-    console.log('tarefa sendo adiconada!!!')
-}
+    return true;
+  }
 
-salvarEdicao(){}
+  adicionarTarefa() {
+    console.log("tarefa sendo adiconada!!!");
+    let tarefaRetornada = this.lerTarefa();
 
-limpar(){}
+    if (this.validar(tarefaRetornada)) {
+      if (this.idEdicao == null) {
+        this.adicionar(tarefaRetornada);
+      } else {
+        this.salvarEdicao(tarefaRetornada);
+      }
 
-construirLista(){}
+      this.limpar();
+      this.construirLista();
+    }
+  }
 
-excluir(id){}
+  salvarEdicao() {}
 
-editar(id){}
+  adicionar(tarefa) {
+    tarefa.id = this.geradorDeId;
+    this.geradorDeId++;
 
-alterarStatus(id){}
+    this.lista.push(tarefa);
+  }
 
-moverParaCima(id){}
-moverParaBaixo(id){}
+  limpar() {
+    document.getElementById("texto-tarefa").value = "";
+    this.idEdicao = null;
+  }
 
+  construirLista() {
+    let tabela = document.getElementById("tabelaTarefas");
+    tabela.innerHTML = "";
+
+    for (let i = 0; i < this.lista.length; i++) {
+      let linha = tabela.insertRow();
+
+      let celulaConcluida = linha.insertCell();
+      let celulaDescricao = linha.insertCell();
+
+      let imgConcluida = document.createElement("img");
+
+      celulaDescricao.innerText = this.lista[i].descricao;
+
+      if (this.lista[i].concluida) {
+        imgConcluida.src = "img/checked.svg";
+      } else {
+        imgConcluida.src = "img/unchecked.svg";
+      }
+
+      imgConcluida.setAttribute(
+        "onclick",
+        `todoListController.alterarStatus(${this.lista[i].id})`
+      );
+
+      celulaConcluida.appendChild(imgConcluida);
+    }
+  }
+
+  excluir(id) {
+    if (confirm("Tem certeza que deseja excluir essa tarefa?")) {
+      let i = 0;
+      let achou = false;
+
+      while (i < this.lista.length && !achou) {
+        if (this.lista[i].id == id) {
+          achou = true;
+        } else {
+          i++;
+        }
+      }
+
+      if (achou) {
+        this.lista.splice(i, 1);
+        this.construirTabela();
+      }
+    }
+  }
+
+  editar(id) {}
+
+  alterarStatus(id) {}
+
+  moverParaCima(id) {}
+  moverParaBaixo(id) {}
 }
 
 const controller = new TodoListController();
