f9e4a20fb45f681f9120bdccd1453286b580c7e0
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-09-16 18:40:10 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function inserirLi() {
+function criarLi() {
   let minhaLista = document.getElementById('lista-tarefas');
-
   let novaLi = document.createElement('li');
-
   novaLi.innerHTML = document.getElementById('texto-tarefa').value;
-
   minhaLista.appendChild(novaLi);
-
   document.getElementById('texto-tarefa').value = '';
+  mudaCorDoBackground();
+}
+
+
+function mudaCorDoBackground() {
+  let tarefas = '';
+  tarefas = document.querySelectorAll('li');
+  for (let i = 0; i < tarefas.length; i += 1) {
+    tarefas[i].addEventListener('click', function () {
+      if (tarefas[i].style.backgroundColor === 'rgb(255, 255, 255)') {
+        tarefas[i].style.backgroundColor = 'rgb(128, 128, 128)';
+      } else {
+        tarefas[i].style.backgroundColor = 'rgb(128, 128, 128)';
+      }
+    });
+  }
 }
