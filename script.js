f045b8f322d68f0290f332277f555a207637f768
Brune Miguel <bstfreire@gmail.com>
2020-09-16 18:27:23 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ let caixaTextoTarefa = document.querySelector("#texto-tarefa");
 let listaDeTarefas = document.querySelector("#lista-tarefas");
 
 
+
 botaoAddTarefa.addEventListener('click',function adicionandoTarefa(){ 
     let novaTarefa  = document.createElement('li')
-    novaTarefa.classList.add("itens")
     novaTarefa.innerText = caixaTextoTarefa.value
     novaTarefa.addEventListener('click', selecionar);
     novaTarefa.addEventListener('dblclick', concluir); 
     listaDeTarefas.appendChild(novaTarefa);
+    novaTarefa.classList.add("itens")
     caixaTextoTarefa.value = '';
+    novaTarefa.addEventListener('click', function selecionarItem(){
+      let selecionado = document.querySelector('.itens')
+      selecionado.style.backgroundColor = 'rgb(128,128,128)';
+    });
 });
 
 function selecionar() {
     this.classList.add('selected');
+
 }
   
 function concluir() {
