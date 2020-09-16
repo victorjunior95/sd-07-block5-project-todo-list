f39451ef93272179a7bef5d76be59d9b3341865c
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-16 19:08:40 -0300

    testing

--- script.js
+++ script.js
@@ -3,34 +3,16 @@ const textInput = document.getElementById('texto-tarefa');
 const addBtn = document.getElementById('criar-tarefa');
 let orderedList = document.getElementById('lista-tarefas');
 
-// addBtn.addEventListener('click', function () {
-//     let elementLi = document.createElement('li');
-//     elementLi.innerText = textInput.value;
-//     orderedList.appendChild(elementLi);
-    
-//     textInput.value = ' ';
-// });
-
-function addTaskItem () {
+addBtn.addEventListener('click', function () {
     let elementLi = document.createElement('li');
     elementLi.innerText = textInput.value;
     orderedList.appendChild(elementLi);
-}
-
-function clearInput () {
+    
     textInput.value = ' ';
-}
+});
 
-addBtn.addEventListener('click', function () {
-    addTaskItem();
-    clearInput();
-})
 
 
 // let deleteAllBtn = document.getElementById('apaga-tudo');
 
 // deleteAllBtn.addEventListener('click', function () {
-//     for (let i = 0; i < orderedList.length; i += 1) {
-//         orderedList[i].removeChild(elementLi);
-//     }
-//     })
