454e8813529d3dd06d17e842730c0b71e649b1ae
Francisco <franciscoliratorres@gmail.com>
2020-09-16 15:59:11 -0300

    Add atéé parte 8

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ buttonComand.addEventListener("click", function (){
     let addOnOl = document.createElement("li");
     organizedList.appendChild(addOnOl).innerText = inputAdd.value;
     inputAdd.value = "";
+
+    let listSelect = document.querySelectorAll("li");
+
+    for (let i = 0; i < listSelect.length; i += 1) {
+        listSelect[i].addEventListener("click", function (event) {
+            let color = document.querySelector(".color")
+            event.target.className += "color"
+            for (let j = 0; j < listSelect.length; j += 1) {
+                if (listSelect [i] !== listSelect[j]) {
+                    listSelect[j].classList.remove("color");
+                }
+            }
+        })
+      }
 })
