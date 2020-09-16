9db3f3d4e0049739819102c8ad702ee5ba22a900
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-16 17:10:58 -0300

    selecionado de cinza

--- script.js
+++ script.js
@@ -3,13 +3,25 @@
 let taskInput = document.querySelector('#texto-tarefa');
 console.log(taskInput);
 
-let createTaskButton = document.querySelector('#criar-tarefa');
-createTaskButton.addEventListener("click",addTask);
+let buttonCreateTask = document.querySelector('#criar-tarefa');
+buttonCreateTask.addEventListener("click",addTask);
 
 function addTask () {
   let ol = document.getElementById("lista-tarefas");
   let li = document.createElement("li");
   li.innerHTML = taskInput.value;
   ol.appendChild(li);
+  taskInput.value = '';
 }
 
+const listItens = document.querySelectorAll('.listItem');
+let selectedItem = document.querySelector('.selected');
+
+for (let i=0; i<listItens.length; i += 1) {
+  listItens[i].addEventListener('click', graySelected);
+  function graySelected() {
+      selectedItem = document.querySelector('.selected');
+      selectedItem.classList.remove('selected');
+      listItens[i].classList.add('selected');
+  }
+}
