2c17ebfba2af480c826f16815e70e6e910743505
Carol Andrade <a.carolg1@gmail.com>
2020-09-16 10:18:19 -0300

    codeClimate script

--- script.js
+++ script.js
@@ -2,28 +2,28 @@
 const btnNewItem = document.querySelector('#criar-tarefa');
 const toDoList = document.querySelector('#lista-tarefas');
 btnNewItem.addEventListener('click', function () {
-  let textInput = document.querySelector('#texto-tarefa').value;
+  const textInput = document.querySelector('#texto-tarefa').value;
   const listItem = document.createElement('li');
   listItem.className = 'listItem';
   listItem.innerHTML = textInput;
   toDoList.appendChild(listItem);
   document.querySelector('#texto-tarefa').value = '';
 // trata evento de deixar cinza e tirar
-listItem.addEventListener('click', function () {
-  let selectedItem = document.querySelector('.selected');
-  if (selectedItem) {
-    selectedItem.classList.remove('selected');
-  }
-    listItem.classList.add('selected');
+  listItem.addEventListener('click', function () {
+    let selectedItem = document.querySelector('.selected');
+    if (selectedItem) {
+      selectedItem.classList.remove('selected');
+    }
+      listItem.classList.add('selected');
   })
 // trata evento de deixar riscado e tirar
-listItem.addEventListener('dblclick', function () {
-  if (listItem.classList.contains('completed')) {
-    listItem.classList.remove('completed');
-  } else {
-    listItem.classList.add('completed');
-  }
-});
+  listItem.addEventListener('dblclick', function () {
+    if (listItem.classList.contains('completed')) {
+      listItem.classList.remove('completed');
+    } else {
+      listItem.classList.add('completed');
+    }
+  });
 // remove itens com check
 let btnClearDoneList = document.querySelector('#remover-finalizados');
 btnClearDoneList.addEventListener('click', function () {
@@ -63,5 +63,3 @@ btnClearList.addEventListener('click', function () {
 //     localStorage.getItem('toDoList');
 //   }
 // };
-
-
