6583e9c59cd2bf348da99a15fa1ca1c3c8ef740a
Leonardo <lecastro16@gmail.com>
2020-09-16 20:16:32 -0300

    Iniciando o Projeto. Vamos COM TUDO: rocket: Leonardo Sardinha v3

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ const botaoCriarTarefa = document.getElementById("criar-tarefa");
 const listaTarefas = document.getElementById("lista-tarefas");
 let arrayListaComTodasTarefas = [];
 
+// Adicionando itens na lista, apagando o campo de preenchimento
 botaoCriarTarefa.addEventListener("click", function () {
   let itemParaAdicionar = document.createElement("li");
-  console.log(itemParaAdicionar);
+  itemParaAdicionar.classList.add("lista");
   itemParaAdicionar.innerText = textoTarefa.value;
   console.log(itemParaAdicionar);
   arrayListaComTodasTarefas.push(itemParaAdicionar);
   listaTarefas.appendChild(itemParaAdicionar);
   textoTarefa.value = "";
   console.log(textoTarefa.value);
+  for (index = 0; index < classeLista.length; classeLista += 1) {
+     classeLista[index].style.backgroundColor = "yellow";
+   }
+});
+
+listaTarefas.addEventListener("click", function (event) {
+  let classeLista = document.getElementsByClassName("lista");
+  console.log(classeLista.className);
+  
+  event.target.style.backgroundColor = "blue";
 });
