6e67592e87bcc663541725c76cf5707f9a50c73a
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-16 18:31:49 -0300

    testing

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 // Add a li in an ol and reset input text value;
 const textInput = document.getElementById('texto-tarefa');
-const addBtn = document.querySelector('#criar-tarefa');
-const orderedList = document.querySelector('#lista-tarefas');
+const addBtn = document.getElementById('criar-tarefa');
+let orderedList = document.getElementById('lista-tarefas');
+
+// addBtn.addEventListener('click', function () {
+//     let elementLi = document.createElement('li');
+//     elementLi.innerText = textInput.value;
+//     orderedList.appendChild(elementLi);
+    
+//     textInput.value = ' ';
+// });
+
+function addTaskItem () {
+    let elementLi = document.createElement('li');
+    elementLi.innerText = textInput.value;
+    orderedList.appendChild(elementLi);
+}
+
+function clearInput () {
+    textInput.value = ' ';
+}
 
 addBtn.addEventListener('click', function () {
-    let elementLi = document.createElement('li');
-    elementLi.innerHTML = textInput.value;
-    orderedList.appendChild(elementLi);
-    
-    textInput.value = ' ';
-});
+    addTaskItem();
+    clearInput();
+})
 
 
-// Button to delele all items from the list;
-const deleteAllBtn = document.getElementById('apaga-tudo');
+// let deleteAllBtn = document.getElementById('apaga-tudo');
+
+// deleteAllBtn.addEventListener('click', function () {
+//     for (let i = 0; i < orderedList.length; i += 1) {
+//         orderedList[i].removeChild(elementLi);
+//     }
+//     })
