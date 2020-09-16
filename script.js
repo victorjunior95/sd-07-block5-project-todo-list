7433bbc33649f29af757579bbecda88115b0540e
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-16 13:59:45 -0300

    5 requisition

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Add a li in an ol and reset input text value;
 const textInput = document.getElementById('texto-tarefa');
 const addBtn = document.querySelector('#criar-tarefa');
+const orderedList = document.querySelector('#lista-tarefas');
 
 addBtn.addEventListener('click', function () {
+    elementLi = document.createElement('li');
+    orderedList.appendChild(elementLi);
+    elementLi.innerText = textInput.value;
+    
     textInput.value = ' ';
 })
 
