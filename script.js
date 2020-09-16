fa21dc3a51daa09f81f33ced8648a0119b400b66
Francisco <franciscoliratorres@gmail.com>
2020-09-16 11:18:12 -0300

    Parte 5 e 6

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-window.onload = function () {
-
 let buttonComand = document.querySelector("#criar-tarefa");
 let inputAdd = document.getElementById("texto-tarefa");
 let organizedList = document.getElementById("lista-tarefas");
 
-
-buttonComand.addEventListener('click', function (){
+buttonComand.addEventListener("click", function (){
     let addOnOl = document.createElement("li");
-    addOnOl.innerText = inputAdd.value
-    document.organizedList.appendChild(addOnOl)
+    organizedList.appendChild(addOnOl).innerText = inputAdd.value;
+    inputAdd.value = "";
 })
-}
