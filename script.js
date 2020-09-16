e62a2cc1d9bd920933f5fda413b7cb9432b08b21
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-16 09:59:36 -0300

    fix

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ addTarefa.addEventListener('click', function () {
     const elementLi = document.createElement('li');
     elementLi.innerText = inputTarefa.value;
     list.appendChild(elementLi);
+    addColor()
+    inputTarefa.value = ''
 })
+
+function addColor () {
+    const item = document.querySelectorAll('li')
+    item.forEach(key => {
+        key.addEventListener('click', function () {
+            key.style.backgroundColor = 'rgb(128 , 128 , 128)'
+        })
+})
+}
