3abb6a9f2a1a942363498b12d7879201044c2a16
Fernando <nando.neves@gmail.com>
2020-09-16 16:41:31 -0300

    proximo

--- script.js
+++ script.js
@@ -1,150 +1,147 @@
+
 // Criar próxima tarefa
+
 let botaoEntrada = document.getElementById("criar-tarefa");
 
-    botaoEntrada.addEventListener("click", function(){
-    let entrada = document.getElementById("texto-tarefa");
-    
-    if (entrada.value != ""){
-    let item =document.createElement("li");
-    item.classList="item"
-    item.innerText = entrada.value;
-    let pai = document.getElementById("lista-tarefas")
+botaoEntrada.addEventListener("click", function(){
+  let tarefa = document.getElementById("texto-tarefa").value;
+  criarTarefas(tarefa);
+});
+
+function criarTarefas(entrada) {
+  if (entrada != "") {
+    let item = document.createElement("li");
+    item.classList = "item";
+    item.innerHTML = entrada;
+    let pai = document.getElementById("lista-tarefas");
     pai.appendChild(item);
-    entrada.value = "";
+    entrada = "";
     let lista = document.querySelectorAll(".item");
     item.id = lista.length;
     criarEventos();
-    tarefasCompletas();   
-       
-    }
-})    
+    tarefasCompletas();
+  }
+};
 
+// Marcando itens selecionados.
 
-// // Marcando itens selecionados.  
-
-
-function criarEventos(){
-    let lista = document.querySelectorAll(".item");
-for (let i = 0; i < lista.length; i++) {
-    (function(i) {
-         lista[i].addEventListener("click", function() {
-            let apagar = document.getElementsByClassName("selected")
-           if (apagar[0] == null){
-           lista[i].classList.add("selected");
-           } else{
-                document.getElementsByClassName("selected")[0].classList.remove("selected")
-              lista[i].classList.add("selected");
-            }
-        })
+function criarEventos() {
+  let lista = document.querySelectorAll(".item");
+  for (let i = 0; i < lista.length; i++) {
+    (function (i) {
+      lista[i].addEventListener("click", function () {
+        let apagar = document.getElementsByClassName("selected");
+        if (apagar[0] == null) {
+          lista[i].classList.add("selected");
+        } else {
+          document
+            .getElementsByClassName("selected")[0]
+            .classList.remove("selected");
+          lista[i].classList.add("selected");
+        }
+      });
     })(i);
- }
+  }
 }
 
 // MARCAR TAREFAS COMPLETAS
 
-function tarefasCompletas(){
-let completos = document.querySelectorAll(".item")
-let listaCompletos =[];
-for (let i = 0; i < completos.length; i++) {
-    (function(i) {
-         completos[i].addEventListener("dblclick", function() {
-           if(listaCompletos[i]==1){
-               completos[i].classList.remove("completed")
-               console.log("tira taxado")
-               listaCompletos[i]=0;
-           }else{
-            console.log("coloca taxado")
-            completos[i].classList.add("completed");
-            listaCompletos[i] =1;
+function tarefasCompletas() {
+  let completos = document.querySelectorAll(".item");
+  let listaCompletos = [];
+  for (let i = 0; i < completos.length; i++) {
+    (function (i) {
+      completos[i].addEventListener("dblclick", function () {
+        if (listaCompletos[i] == 1) {
+          completos[i].classList.remove("completed");
+          listaCompletos[i] = 0;
+        } else {
+          completos[i].classList.add("completed");
+          listaCompletos[i] = 1;
         }
-        })
+      });
     })(i);
- }
+  }
 }
 
 // APAGAR TUDO
-let pai = document.getElementById("lista-tarefas")
-let apagar = document.getElementById("apaga-tudo")
-apagar.addEventListener("click",function(){
-    pai.innerHTML="";
-})
+let pai = document.getElementById("lista-tarefas");
+let apagar = document.getElementById("apaga-tudo");
+apagar.addEventListener("click", function () {
+  pai.innerHTML = "";
+});
 
 // APAGAR FINALIZADOS
-let apagarCompletos = document.getElementById("remover-finalizados")
-apagarCompletos.addEventListener("click",function(){
 
-    let pai = document.getElementById("lista-tarefas")
-    let lista = document.querySelectorAll(".item");
+let apagarCompletos = document.getElementById("remover-finalizados");
+apagarCompletos.addEventListener("click", function () {
+  let pai = document.getElementById("lista-tarefas");
+  let lista = document.querySelectorAll(".item");
 
-for (let i=0;i<lista.length;i+=1){
-    if(lista[i].classList.contains("completed")==true){
-        pai.removeChild(lista[i])
-        console.log("apagou finalizado")
+  for (let i = 0; i < lista.length; i += 1) {
+    if (lista[i].classList.contains("completed") == true) {
+      pai.removeChild(lista[i]);
+      console.log("apagou finalizado");
     }
-}    
-})
+  }
+});
 
 // APAGAR SELECIONADO
 
-let removerSelecionado = document.getElementById("remover-selecionado")
-removerSelecionado.addEventListener("click",function(){
-    
-    let pai = document.getElementById("lista-tarefas")
-    let lista = document.querySelector(".selected");
-    pai.removeChild(lista);  
-})
+let removerSelecionado = document.getElementById("remover-selecionado");
+removerSelecionado.addEventListener("click", function () {
+  let pai = document.getElementById("lista-tarefas");
+  let lista = document.querySelector(".selected");
+  pai.removeChild(lista);
+});
 
 //SALVAR LISTA EM LOCALSTORAGE
 
-let salvar = document.getElementById("salvar-tarefas")
-salvar.addEventListener("click",function(){
+let salvar = document.getElementById("salvar-tarefas");
+salvar.addEventListener("click", function () {
+  let lista = document.querySelectorAll(".item");
+  localStorage.clear();
+  localStorage.setItem("tamanho", lista.length);
 
-    let lista = document.querySelectorAll(".item");
-    localStorage.clear();
-    localStorage.setItem("tamanho",lista.length)
-
-    for (let i=0;i<lista.length;i+=1){
-        localStorage.setItem(i, lista[i].outerHTML);
-        console.log(lista[i]);
-    }
-
-})
+  for (let i = 0; i < lista.length; i += 1) {
+    localStorage.setItem(i, lista[i].innerText);
+    console.log(lista[i]);
+  }
+});
 
 //CARREGAR LISTA DE LOCALSTORAGE
+let tamanho = localStorage.getItem("tamanho");
+console.log(tamanho)
 
-    let tamanho = localStorage.getItem("tamanho")
+for (let i = 0; i < tamanho; i += 1) {
+  let tarefa = localStorage.getItem(i);
+  criarTarefas(tarefa);
 
-    for(let i=0;i<tamanho;i+=1){
-        let tarefa = localStorage.getItem(i)
-        let novo = document.createElement('li')
-        pai.appendChild(novo);
-        novo.outerHTML = tarefa; 
-        
-    }
+}
+
+
 // MOVER PARA CIMA OU PARA BAIXO
 
-let cima = document.getElementById("mover-cima")
+let cima = document.getElementById("mover-cima");
 
-cima.addEventListener("click",function(){
-
-    let atual = document.querySelector(".selected")
-    if (atual!=null){
-        if(atual.previousElementSibling){
-            let anterior = atual.previousElementSibling;
-            anterior.before(atual)
-        }
+cima.addEventListener("click", function () {
+  let atual = document.querySelector(".selected");
+  if (atual != null) {
+    if (atual.previousElementSibling) {
+      let anterior = atual.previousElementSibling;
+      anterior.before(atual);
     }
-})
+  }
+});
 
-let baixo = document.getElementById("mover-baixo")
+let baixo = document.getElementById("mover-baixo");
 
-baixo.addEventListener("click",function(){
-
-    let atual = document.querySelector(".selected")
-    if (atual!=null){
-        if(atual.nextElementSibling){
-            let proximo = atual.nextElementSibling;
-            atual.before(proximo)
-        }
+baixo.addEventListener("click", function () {
+  let atual = document.querySelector(".selected");
+  if (atual != null) {
+    if (atual.nextElementSibling) {
+      let proximo = atual.nextElementSibling;
+      atual.before(proximo);
     }
-})
+  }
+});
