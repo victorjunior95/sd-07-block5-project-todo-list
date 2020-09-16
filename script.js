c18f5b125137de353897f1fac8cf5f7b860b22c2
vini-lima <vini20.henrique@gmail.com>
2020-09-16 16:53:57 -0300

    Lista de Tarefas Ordenando e apagando corretamente

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const list = document.getElementById('lista-tarefas');
 const taskAdd = document.getElementById('criar-tarefa');
 
 taskAdd.addEventListener('click', function () {
-  let currentTask = document.createElement('li');
+  const currentTask = document.createElement('li');
   currentTask.innerText = task.value;
-  list.appendChild(currentTask); 
+  list.appendChild(currentTask);
+  task.value = '';
 });
