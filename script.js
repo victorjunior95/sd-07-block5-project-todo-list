f7a740c3894e87d58088b37c2b5ebd770ee78de6
Galaraz <jvictor.si.2013@gmail.com>
2020-09-16 12:41:13 -0300

    organizando ideaçao script

---
+++ script.js
@@ -0,0 +1,28 @@
+class TodoListController {
+  constructor() {
+    this.lista = [];
+    this.idEdicao = null;
+    this.geradorDeId = 0;
+  }
+
+
+lerTarefa(){}
+
+validar(tarefa){}
+
+adicionarTarefa(tarefa) {}
+
+salvarEdicao(){}
+
+limpar(){}
+
+construirLista(){}
+
+excluir(id){}
+
+editar(id){}
+
+alterarStatus(id){}
+}
+
+const controller = new TodoListController();
