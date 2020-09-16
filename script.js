720d198855cd1f395296908b9a7c20b0a7a6dcce
Leonardo <lecastro16@gmail.com>
2020-09-16 17:22:34 -0300

    Iniciando o Projeto. Vamos COM TUDO: rocket: Leonardo Sardinha v2

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const botaoCriarTarefa = document.getElementById("criar-tarefa");
 const listaTarefas = document.getElementById("lista-tarefas");
 let arrayListaComTodasTarefas = [];
 
-botaoCriarTarefa.addEventListener("click", function (){
-     let itemParaAdicionar = document.createElement("li");
-     console.log(itemParaAdicionar);
-     itemParaAdicionar.innerText = textoTarefa.value;
-     console.log(itemParaAdicionar);
-     arrayListaComTodasTarefas.push(itemParaAdicionar);
-     listaTarefas.appendChild(itemParaAdicionar);
-     textoTarefa.value="";
-     console.log(textoTarefa.value);
+botaoCriarTarefa.addEventListener("click", function () {
+  let itemParaAdicionar = document.createElement("li");
+  console.log(itemParaAdicionar);
+  itemParaAdicionar.innerText = textoTarefa.value;
+  console.log(itemParaAdicionar);
+  arrayListaComTodasTarefas.push(itemParaAdicionar);
+  listaTarefas.appendChild(itemParaAdicionar);
+  textoTarefa.value = "";
+  console.log(textoTarefa.value);
 });
