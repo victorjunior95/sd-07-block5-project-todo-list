286e8e04acc37fb452ee300f196a4fcb368c9ab9
Bruno <bruno_grb@hotmail.com>
2020-09-16 19:37:21 -0300

    adicionando funcoes de mover elementos para cima e para baixo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Add element to the bottom of the list and clear the text input
-function addList() {
+function addItemInList() {
   //some parts were based on the website examples (https://www.w3schools.com/jsref/met_node_appendchild.asp)
   let clearText = document.getElementById('texto-tarefa');
   let addTask = document.getElementById('lista-tarefas');
@@ -15,16 +15,17 @@ function addList() {
 
 // function to select an item that was clicked and show a background color
 function selected(event) {
-  let items = document.querySelector('.selected');
-  if (items !== null) {
-    items.classList.remove('selected');
+  let item = document.querySelector('.selected');
+  if (item !== null) {
+    item.classList.remove('selected');
   }
   event.target.classList.add('selected');
 }
+
 // function to select an item that was double clicked and show a line through
 function dbclicked(event) {
-  let items = event.target.classList.contains('completed');
-  if (items === true) {
+  let item = event.target.classList.contains('completed');
+  if (item === true) {
     event.target.classList.remove('completed');
   } else {
     event.target.classList.add('completed');
@@ -49,10 +50,34 @@ function clearAllDone() {
 }
 
 // function remove Selected
-function removeSelected(){
+function removeSelected() {
   let selected = document.querySelector('.selected');
-  if (selected !== null){
-  let father = document.getElementById('lista-tarefas');
-  father.removeChild(selected);
+  if (selected !== null) {
+    let father = document.getElementById('lista-tarefas');
+    father.removeChild(selected);
+  }
+}
+
+// fuction move item up
+function moveItemUp() {
+  let item = document.querySelector('.selected');
+  if (item.previousSibling !== null) {
+    let previousItem = item.previousSibling.innerHTML;
+    item.previousSibling.innerHTML = item.innerHTML;
+    item.innerHTML = previousItem;
+    item.classList.remove('selected');
+    item.previousSibling.classList.add('selected');
+  }
+}
+
+// fuction move item up
+function moveItemDown() {
+  let item = document.querySelector('.selected');
+  if (item.nextSibling !== null) {
+    let previousItem = item.nextSibling.innerHTML;
+    item.nextSibling.innerHTML = item.innerHTML;
+    item.innerHTML = previousItem;
+    item.classList.remove('selected');
+    item.nextSibling.classList.add('selected');
   }
 }
