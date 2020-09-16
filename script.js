91f34c954e4e9dbe0aaf2047dd30bfc14a61b84f
Mauro Bergonzoni Junqueira <mauro@pop-os.localdomain>
2020-09-16 15:06:16 -0300

    Input limpo após criar uma tarefa

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let btCreate = document.getElementById("criar-tarefa");
 let inpuText = document.getElementById("texto-tarefa");
-let orderList = document.getElementById("lista-tarefas")
+let orderList = document.getElementById("lista-tarefas");
+
 btCreate.addEventListener("click", function(){
     var node = document.createElement("LI");                 // Create a <li> node
     var textnode = document.createTextNode(inpuText.value);  // Create a text node
     node.appendChild(textnode);                              // Append the text to <li>
-    orderList.appendChild(node); 
+    orderList.appendChild(node);
+    inpuText.value =' ';
     
 });
