efe2de59332468c1a94464c03d747a599a2342ad
Ana Karine <anakarine.ti@gmail.com>
2020-09-16 03:46:03 -0300

    req 7 (correçao

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ document.querySelector('#criar-tarefa').addEventListener('click', function newLi
   document.querySelector('ol').appendChild(newLi);
   newLi.innerText = document.querySelector('input').value;
   document.querySelector('input').value = '';
+
+  newLi.addEventListener('click', function () {
+    let selectItem = document.querySelector('.selected');
+    if (selectItem) {
+      selectItem.classList.remove('selected');
+    }
+    newLi.classList.add('selected');
+    })
 });
+
+
 
