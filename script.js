6fb511263a4bb94eee49852413b38f32cc9b7603
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-16 23:57:22 -0300

    adicionado evento para riscar o elemento na lista

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+'use strict'
 window.onload = function () {
+
     insertTextBuildListField();
     clearField();
-
+    selectItem();
+    listChosen();
 
     function insertTextBuildListField() {
         document.querySelector("#criar-tarefa").addEventListener("click", function () {
             let list = document.querySelector("#lista-tarefas");
             let text = document.querySelector("#texto-tarefa");
             let li = document.createElement('li');
+            li.className = 'item';
             li.textContent = text.value;
             if (text.value === '') {
                 alert("Você precisa adicionar algo!");
@@ -25,10 +29,32 @@ window.onload = function () {
             for (let item = 0; item < li.length; item += 1) {
                 li[item].innerHTML = '';
             }
-            // li.forEach(item => {
-            //     item.value = '';
-            // });
         });
 
+    }
+
+    function selectItem(){
+        let list = document.querySelectorAll('ol');
+        console.log(list);
+        list.forEach(item => { // para cada elemento
+            item.addEventListener('click', function (event) {
+                event.target.classList.add('selected'); //verifica qual elemento recebeu a ação
+                list.forEach(item => { // efetuo loop para identificar se 
+                    if (item !== event.target) {
+                        item.classList.remove('selected');
+                    }
+                });
+            });
+        });
+    }
+
+    function listChosen() {
+        let list = document.querySelectorAll('ol');
+        for (let item = 0; item < list.length; item += 1) {
+            list[item].addEventListener('click', function (event) {
+                    event.target.style.textDecoration = 'line-through';
+              
+            });
+        }
     }
 }
