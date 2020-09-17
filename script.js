5ef298cebce595cd7ef60b02c730dba6459d8671
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-17 16:27:48 -0300

    requisito 12 envio 1

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const buttonCreateToDo = document.querySelector(".criar-tarefa");
 let olToDoList = document.querySelector(".lista-tarefas");
 const buttonClearAllTasksInTheList = document.querySelector(".apaga-tudo");
 const buttonClearDoneTasks = document.querySelector(".remover-finalizados");
+const buttonToSalveList = document.querySelector(".salvar-tarefas");
 
 function clearInputTextToDo() {
   inputTextToDo.value = "";
 }
 
-buttonCreateToDo.addEventListener("click", function () {
+function createLI() {
   const liTask = document.createElement("li");
   liTask.innerText = inputTextToDo.value;
   olToDoList.appendChild(liTask);
   clearInputTextToDo();
   changeBGColorTask(liTask);
   addAndRemoveCompletedTask(liTask);
-});
+}
+
+buttonCreateToDo.addEventListener("click", createLI);
 
 function changeBGColorTask(task) {
   task.addEventListener("click", function (event) {
@@ -43,9 +46,30 @@ buttonClearAllTasksInTheList.addEventListener("click", function () {
 });
 
 buttonClearDoneTasks.addEventListener("click", function () {
-   let doneTasks = document.querySelectorAll('.completed');
-   doneTasks.forEach(item => {
-     // usei esse link pra resolver essa parte: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
-     olToDoList.removeChild(item);
-   })
-})
+  let doneTasks = document.querySelectorAll(".completed");
+  doneTasks.forEach((item) => {
+    // usei esse link pra resolver essa parte: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
+    olToDoList.removeChild(item);
+  });
+});
+
+buttonToSalveList.addEventListener("click", function () {
+  let listToSaveLocalStorage = [];
+  for (let index = 0; index < olToDoList.childElementCount; index += 1) {
+    let innerTextTaskList = olToDoList.children[index].innerText;
+    let taskStorage = { task: innerTextTaskList };
+    listToSaveLocalStorage.push(taskStorage);
+  }
+  localStorage.setItem("Tasks", JSON.stringify(listToSaveLocalStorage));
+});
+
+if (localStorage.getItem("Tasks") !== null) {
+  for (
+    let index = 0;
+    index < JSON.parse(localStorage.getItem("Tasks")).length;
+    index += 1
+  ) {
+    inputTextToDo.value = JSON.parse(localStorage.getItem("Tasks"))[index].task;
+    createLI();
+  }
+}
