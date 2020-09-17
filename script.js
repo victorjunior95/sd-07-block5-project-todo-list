45b64c6cd6513e667c7b9398664a7d3292d79fda
Luise Rios <luisemiranda@gmail.com>
2020-09-17 11:10:25 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -9,20 +9,34 @@ addButton.addEventListener("click", function() {
 	textInput.value = '';
 
 	li.addEventListener("click", function () {
-  	if (li.classList.contains('selected')) {
+			if (li.classList.contains("selected")) {
 			li.classList.remove("selected");
 		} else {
 			li.classList.add("selected"); 
 		}
-  });
-
+	});
+	
   li.addEventListener("dblclick", function () {
-    if (li.classList.contains('completed')) {
+    if (li.classList.contains("completed")) {
       li.classList.remove("completed");
     } else {
       li.classList.add("completed");
     }
-  });
-	
-})
+  })
+});
+
+let clear = document.getElementById("apaga-tudo");
+clear.addEventListener("click", function () {
+  while (ol.firstChild) { 
+    ol.removeChild(ol.firstChild);
+  }
+});
+
+let clearDone = document.getElementById("remover-finalizados");
+clearDone.addEventListener("click", function () {
+  while (document.querySelector(".completed")) { 
+    ol.removeChild(document.querySelector(".completed"));
+  }
+});
+
 
