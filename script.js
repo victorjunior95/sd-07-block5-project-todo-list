ed3857b16bcae5e60eb6ac76034e0b6cf6e7de63
vini-lima <vini20.henrique@gmail.com>
2020-09-16 23:01:11 -0300

    Selecionando Tarefas

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ const taskAdd = document.getElementById('criar-tarefa');
 
 taskAdd.addEventListener('click', function () {
   const currentTask = document.createElement('li');
+  currentTask.className = 'ordered-list';
   currentTask.innerText = task.value;
   list.appendChild(currentTask);
   task.value = '';
+});
+
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('ordered-list')) {
+    const selectedAllTasks = document.getElementsByClassName('ordered-list');
+    for (let index = 0; index < selectedAllTasks.length; index += 1) {
+      selectedAllTasks[index].className = 'ordered-list';
+    }
+    const selectedTask = event.target;
+    selectedTask.className = 'ordered-list selected';
+  }
 });
