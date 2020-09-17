57ac102af63ba96f90d34515fae86f8f634e50d5
Carol Andrade <a.carolg1@gmail.com>
2020-09-17 11:15:04 -0300

    requisito mover sub1

--- script.js
+++ script.js
@@ -47,11 +47,27 @@ btnNewItem.addEventListener('click', function () {
     }
   });
 // trata evento mover item da lista para cima
+// source:https://pt.stackoverflow.com/questions/96648/after-jquery-em-js-puro#:~:text=after()%20insere%20um%20elemento,insertBefore()%20%2C%20segundo%20a%20documenta%C3%A7%C3%A3o%3A&text=N%C3%A3o%20existe%20um%20m%C3%A9todo%20insertAfter,antes%20do%20pr%C3%B3ximo%20elemento%20irm%C3%A3o.
   const btnMoveUp = document.querySelector('#mover-cima');
   btnMoveUp.addEventListener('click', function () {
-    
+    const selected = document.getElementsByClassName('selected')[0];
+    if (selected !== null) {
+    if (selected.previousSibling !== null) {
+      selected.after(selected.previousSibling);
+    }
+  }
   });
-
+  move(this.nextElementSibling, 10)
+  // trata evento mover item da lista para cima
+  const btnMoveDown = document.querySelector('#mover-baixo');
+  btnMoveDown.addEventListener('click', function () {
+    const selected = document.getElementsByClassName('selected')[0];
+    if (selected !== null) {
+    if (selected.nextSibling !== null) {
+      selected.before(selected.nextSibling)
+    }
+  }
+  });
 // trata evento salvar lista
   const btnSaveList = document.querySelector('#salvar-tarefas');
   btnSaveList.addEventListener('click', function () {
