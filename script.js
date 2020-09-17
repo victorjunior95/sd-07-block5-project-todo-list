52eebf0d4c2e1dff0d86b702ed5143f86fdd1afb
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-17 16:04:51 -0300

    arrumando algumas issues

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-let listaTarefas = document.getElementById('lista-tarefas');
-let buttonAddTarefa = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const buttonAddTarefa = document.getElementById('criar-tarefa');
 
-buttonAddTarefa.addEventListener('click', function (){
-    let itemLista = document.createElement('li');
-    itemLista.innerText = document.getElementById('texto-tarefa').value;
-    listaTarefas.appendChild(itemLista);
+buttonAddTarefa.addEventListener('click', function () {
+  const itemLista = document.createElement('li');
+  itemLista.innerText = document.getElementById('texto-tarefa').value;
+  listaTarefas.appendChild(itemLista);
 
-    itemLista.addEventListener('click', function(event){
-        let selectedItem = document.querySelector('.selected');
-        if(selectedItem !== null){
+  itemLista.addEventListener('click', function (event) {
+    let selectedItem = document.querySelector('.selected');
+    if (selectedItem !== null) {
         selectedItem.classList.remove('selected');
-        }      
-        event.target.classList.add('selected')
+        } event.target.classList.add('selected');
     });
 
-    itemLista.addEventListener('dblclick', function(event){
-        if(event.target.classList.contains('completed')){
-            event.target.classList.remove('completed');
-        } 
-        else {
-            event.target.classList.add('completed')
-        }
-    })
-    document.getElementById('texto-tarefa').value ='';
+  itemLista.addEventListener('dblclick', function(event){
+      if(event.target.classList.contains('completed')){
+          event.target.classList.remove('completed');
+      }
+      else {
+          event.target.classList.add('completed');
+      }
+  });
+  document.getElementById('texto-tarefa').value = '';
 });
-//Limpando a lista
-//baseado em https://app.betrybe.com/course/fundamentals/javascript/dom-manipulation/js-part-6
+// baseado em https://app.betrybe.com/course/fundamentals/javascript/dom-manipulation/js-part-6
 
-let buttonClear = document.getElementById('apaga-tudo')
-buttonClear.addEventListener('click', function(){
+const buttonClear = document.getElementById('apaga-tudo')
+buttonClear.addEventListener('click', function () {
     let itemsList = document.querySelectorAll('li')
 
     for(let index = 0; index< itemsList.length; index += 1) {
