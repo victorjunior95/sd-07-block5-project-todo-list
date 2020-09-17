c8639020370108a3b97dbd51bd1db8fad3b848b6
Lucas Miranda <lucasmirandadev@gmail.com>
2020-09-17 11:29:25 -0300

    salvar a lista em localStorage

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ const listaTarefas = document.getElementById("lista-tarefas");
 const textoTarefa = document.getElementById("texto-tarefa");
 const btnCriarTarefa = document.getElementById("criar-tarefa");
 const apagaTudo = document.getElementById("apaga-tudo")
-const salvarTarefas = document.getElementById('#salvar-tarefas');
+const salvarTarefas = document.getElementById('salvar-tarefas');
 
 
 btnCriarTarefa.addEventListener('click', addProduto);
     
-
 function addProduto() {
     var item = '<li>' + textoTarefa.value + '</li>';
     listaTarefas.innerHTML += item;
     textoTarefa.value = '';
     textoTarefa.focus();
+    
 };
 
 
-
-apagaTudo.addEventListener('click', function () {
-    listaTarefas.innerHTML = '';
+document.querySelector('#salvar-tarefas').addEventListener('click', function () {
+    localStorage.clear();
+    const listaSalva = document.querySelector('ol');
+    localStorage.setItem('tarefaSalva', listaSalva.innerHTML);
   });
 
+function mostrarListaAtual() {
+    document.querySelector('ol').innerHTML = localStorage.getItem('tarefaSalva');
+  };
+  window.onload = mostrarListaAtual;
+
+
+  apagaTudo.addEventListener('click', function () {
+    listaTarefas.innerHTML = '';
+  });
