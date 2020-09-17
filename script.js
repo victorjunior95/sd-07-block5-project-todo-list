4acfa57a3060328c28574b8d8f4a49f1bf184250
Eriquy Jhordan <eriquy@gmail.com>
2020-09-17 08:21:08 -0300

    funcionalidade de selecionar tarefa ok

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ buttonAddTarefa.addEventListener('click', () => {
   const textoTarefa = document.querySelector('#texto-tarefa');
   const tarefa = document.createElement('li');
   tarefa.innerText = textoTarefa.value;
+  tarefa.classList = 'item';
+  tarefa.addEventListener('click', (object) => selectItem(object))
   listTarefa.appendChild(tarefa);
   textoTarefa.value = '';
 });
+
+function selectItem(object){
+  const listTarefa = document.querySelectorAll('.item');
+  const tarefa = object.target;
+  for(let i=0; i< listTarefa.length; i+=1){
+    if(listTarefa[i].classList[1]){
+      listTarefa[i].classList = 'item';
+    }
+  }
+  tarefa.classList = 'item selected';
+}
