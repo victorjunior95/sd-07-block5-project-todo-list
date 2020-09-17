4559f22088a5018ab6b9a953d5c9669723593d84
Filipi Firmino <contatoic3web@gmail.com>
2020-09-16 21:33:43 -0300

    Selcionar e deletar item pronto

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const containerList = document.querySelector('#container-list');
 const inputList = document.querySelector('#texto-tarefa');
 const btnNewTask = document.querySelector('#criar-tarefa');
 const btnClearList = document.querySelector('#apaga-tudo');
+const btnRemoveTask = document.querySelector('#remover-selecionado');
 
 const ol = document.createElement('ol');
 ol.id='lista-tarefas';
@@ -21,12 +22,34 @@ btnNewTask.addEventListener('click', function () {
 });
 
 btnClearList.addEventListener('click', function () {
-   if(ol.children.length === 0 ){
-       alert ('Não há tarefas listadas');
-   } else {
-    const taskListed = document.querySelectorAll('.task');
-    for (let indexList = 0; indexList < taskListed.length; indexList += 1) {
-        ol.removeChild(taskListed[indexList]);
-    }
+  if(ol.children.length === 0 ){
+   alert ('Não há tarefas listadas');
+  } else {
+   const taskListed = document.querySelectorAll('.task');
+   for (let indexList = 0; indexList < taskListed.length; indexList += 1) {
+    ol.removeChild(taskListed[indexList]);
    }
+  }
+});
+
+ol.addEventListener ('click', function (event) {
+  if(event.target.className === 'task') {
+   for (let index = 0; index < ol.children.length; index += 1) {
+   ol.children[index].className = 'task';
+   }
+  event.target.className = 'selected';
+  } else {
+  for (let index = 0; index < ol.children.length; index += 1) {
+   ol.children[index].className = 'task';
+  }
+ }
+});
+
+btnRemoveTask.addEventListener('click', function () {  
+  const taskSelected = document.querySelector('.selected');
+  if(document.querySelector('.selected') == null){
+    alert ('Nenhuma tarefa selecionada');
+  }else{
+    ol.removeChild(taskSelected);
+  }
 });
