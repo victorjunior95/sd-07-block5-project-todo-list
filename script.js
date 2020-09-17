cf8d5929120eeb3e89274387ab7a4825412b0c77
Eriquy Jhordan <eriquy@gmail.com>
2020-09-17 09:49:03 -0300

    funcionalidade de marcar como concluido e removera lista inteira completos

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 const buttonAddTarefa = document.querySelector('#criar-tarefa');
+const buttonDeletaTarefas = document.querySelector('#apaga-tudo');
+
+function finishItem(object) {
+  const tarefa = object.target;
+  tarefa.classList.toggle('completed');
+  tarefa.classList.toggle('selected');
+}
+
+function selectItem(object) {
+  const listTarefa = document.querySelectorAll('.item');
+  const tarefa = object.target;
+  for (let i = 0; i < listTarefa.length; i += 1) {
+    if (listTarefa[i].classList.length > 1) {
+      listTarefa[i].classList.remove('selected');
+    }
+  }
+  tarefa.classList.add('selected');
+}
+
+buttonDeletaTarefas.addEventListener('click', () => {
+  const tarefas = document.querySelectorAll('.item');
+  const listTarefa = document.querySelector('#lista-tarefas');
+  for (let i = 0; i < tarefas.length; i += 1) {
+    listTarefa.removeChild(tarefas[i]);
+  }
+
+})
 
 buttonAddTarefa.addEventListener('click', () => {
   const listTarefa = document.querySelector('#lista-tarefas');
   const textoTarefa = document.querySelector('#texto-tarefa');
+  if (textoTarefa.value === '') {
+    return alert('Você não pode inserir uma tarefa sem nome :(');
+  }
   const tarefa = document.createElement('li');
   tarefa.innerText = textoTarefa.value;
   tarefa.classList = 'item';
-  tarefa.addEventListener('click', (object) => selectItem(object))
+  tarefa.addEventListener('click', (object) => selectItem(object));
+  tarefa.addEventListener('dblclick', (object) => finishItem(object));
   listTarefa.appendChild(tarefa);
   textoTarefa.value = '';
 });
 
-function selectItem(object){
-  const listTarefa = document.querySelectorAll('.item');
-  const tarefa = object.target;
-  for(let i=0; i< listTarefa.length; i+=1){
-    if(listTarefa[i].classList[1]){
-      listTarefa[i].classList = 'item';
-    }
-  }
-  tarefa.classList = 'item selected';
-}
+
