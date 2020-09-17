417415c89b5165f7c5b1fd95a695cc85a5d5fc32
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-09-16 21:14:29 -0300

    Requisito 8 + estilos no botão

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ function mudaCorDoBackground() {
   tarefas = document.querySelectorAll('li');
   for (let i = 0; i < tarefas.length; i += 1) {
     tarefas[i].addEventListener('click', function () {
-      if (tarefas[i].style.backgroundColor === 'rgb(255, 255, 255)') {
-        tarefas[i].style.backgroundColor = 'rgb(128, 128, 128)';
+
+      let itemSelecionado = document.querySelector('.selecionado');
+      if (itemSelecionado == null) {
+        tarefas[i].classList.add('selecionado');
+
       } else {
-        tarefas[i].style.backgroundColor = 'rgb(128, 128, 128)';
+        if (itemSelecionado != tarefas[i]) {
+          itemSelecionado.classList.remove('selecionado');
+          tarefas[i].classList.add('selecionado');
+        }
       }
     });
   }
