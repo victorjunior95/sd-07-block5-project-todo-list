37610eefb9f99d96c1e33ede3fca0936a6bc79eb
vitorAmorims <vtamorims@gmail.com>
2020-09-17 10:34:29 -0300

    refactored function button selecionado

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const criarTarefa = document.querySelector("#criar-tarefa");
+const botaoApagarLista = document.querySelector("#apaga-tudo");
+const botaoRemoverSelecionado = document.querySelector("#remover-selecionado");
+const buttoncriarTarefa = document.querySelector("#criar-tarefa");
 let textoTarefa = document.querySelector("#texto-tarefa");
+var listOl = document.querySelector('ol');
 let li = document.createElement('li');
 let listaTarefas = document.querySelector("#lista-tarefas");
-let colorselect = document.querySelector('#lista-tarefas').childNodes;
+
 function InputTextField () {
     textoTarefa.addEventListener('keydown', () => {
         textoTarefa = document.querySelector("#texto-tarefa");
@@ -10,41 +13,38 @@ function InputTextField () {
 }
 
 function listTask () {
-    criarTarefa.addEventListener('click', () => {
+    buttoncriarTarefa.addEventListener('click', () => {
         listaTarefas = document.querySelector("#lista-tarefas");
         li = document.createElement('li');
         li.innerHTML = textoTarefa.value;
         listaTarefas.appendChild(li);
         textoTarefa.value = '';
         li.id ='list'
-        li.className = 'selected';
-        li.addEventListener('click', colorListSelect);
-        li.addEventListener('dblclick', elementoSelecionadoNaLista)
     })
 }
 
-function elementoSelecionadoNaLista (event) {
-    const tarefaCompleta = event.target;
-    if (tarefaCompleta.classList.contains('completed')) {
-        tarefaCompleta.classList.remove('completed');
-    } else {
-        tarefaCompleta.classList.add('completed');
-    }      
+function eventosDaLista () {
+    listOl.addEventListener('dblclick', function(ev) {
+      if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('completed');
+      }
+    });
+    listOl.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+          ev.target.classList.toggle('selected');
+        }
+    });
 }
 
-
-//Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo.
-
-//Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128)
-function colorListSelect (event) {
-    const tarefaColorirElemento = event.target;
-    const removeCorDeOutrosElementos = document.querySelector("li");
-    for (let i = 0; i < removeCorDeOutrosElementos.length; i++) {
-        removeCorDeOutrosElementos[i].classList.remove("seleted");
+botaoRemoverSelecionado.addEventListener('click', apagarSelecionado);
+function apagarSelecionado () {
+    const selecionado = document.querySelectorAll(".selected");
+    for (let index = 0; index < selecionado.length; index += 1) {
+        selecionado[index].remove();
     }
-    tarefaColorirElemento.classList.add("seleted");  
 }
 
+botaoApagarLista.addEventListener("click", apagarTudo);
 function apagarTudo() {
     const todasLi = document.querySelector("#lista-tarefas");
     while (todasLi.firstChild) {
@@ -52,8 +52,8 @@ function apagarTudo() {
     }
 }
 
-const botaoApagarTudo = document.querySelector("#apaga-tudo");
-botaoApagarTudo.addEventListener("click", apagarTudo);
+
+
 /*
 //Adicione um botão com id="remover-selecionado" que, quando clicado, remove o item selecionado
 const removerSelecionado = document.querySelector("#remover-selecionado");
@@ -66,19 +66,8 @@ function removecolorListSelect () {
 //id `remover-finalizados` elementos completos
 
 
-/*//apagar tudo
-function func () {
-    listaTarefas.innerHTML = "";   
-}
-
-function apagarTudo () {
-    const apague = document.querySelector("#apaga-tudo");
-    apague.addEventListener('click', () => {
-        const killLi = func();
-    })
-}
-*/
 window.onload = () => {
     InputTextField ()
+    eventosDaLista ()
     listTask ()
 }
