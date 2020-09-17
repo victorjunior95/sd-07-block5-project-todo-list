caa826b6d658d491aef523bee77ef76b7bbc505d
Luise Rios <luisemiranda@gmail.com>
2020-09-17 10:01:34 -0300

    requisitos 8 e 9

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ addButton.addEventListener("click", function() {
 	let li = document.createElement("li");
 	li.innerText = textInput.value;
 	ol.appendChild(li);
-	textInput.value = "";
+	textInput.value = '';
+
+	li.addEventListener("click", function () {
+  	if (li.classList.contains('selected')) {
+			li.classList.remove("selected");
+		} else {
+			li.classList.add("selected"); 
+		}
+  });
+
+  li.addEventListener("dblclick", function () {
+    if (li.classList.contains('completed')) {
+      li.classList.remove("completed");
+    } else {
+      li.classList.add("completed");
+    }
+  });
+	
 })
-
-
 
