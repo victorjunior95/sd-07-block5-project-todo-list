ecb4d95f103deb88cd59176747350bf1163afb15
Thalys <thalysmatiascarrara@gmail.com>
2020-09-17 18:19:23 -0400

    requisito 7 finalizado

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const buttonCreateTask = document.getElementById('criar-tarefa');
-const inputTextTask = document.getElementById('texto-tarefa')
-const taskList = document.getElementById('lista-tarefas')
+window.onload = function(){
+    const buttonCreateTask = document.getElementById('criar-tarefa');
+    const inputTextTask = document.getElementById('texto-tarefa');
+    const taskList = document.getElementById('lista-tarefas');
+    const itensList = document.getElementsByTagName('li');
 
-buttonCreateTask.addEventListener('click',function(){
-
-    if(inputTextTask.value == ""){
-        alert("adicione um nome ao item da lista")
-    }else{
-        let li = document.createElement("li")
-        li.innerText = inputTextTask.value
-        taskList.appendChild(li)
-        inputTextTask.value = ""
+    function addItemToTaskList(){
+        if(inputTextTask.value == ''){
+            alert('adicione um nome ao item da lista')
+        }else{
+            let li = document.createElement('li');
+            li.addEventListener('click',function(){
+                li.classList.add('selected');
+            })
+            li.innerText = inputTextTask.value;
+            taskList.appendChild(li);
+            inputTextTask.value = '';
+        }
     }
+    
+
+
+
+    buttonCreateTask.addEventListener('click', addItemToTaskList)
    
-})
+    
+    
+}
