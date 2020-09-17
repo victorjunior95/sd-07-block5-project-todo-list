95ebb2250a970248a4c82f7e56e206ccea10b449
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-16 21:36:49 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Projeto TO Do List
 
 let taskInput = document.querySelector('#texto-tarefa');
-console.log(taskInput);
+// console.log(taskInput);
 
 let buttonCreateTask = document.querySelector('#criar-tarefa');
 buttonCreateTask.addEventListener("click",addTask);
@@ -16,27 +16,24 @@ function addTask () {
 
 const listItens = document.querySelectorAll('.listItem');
 let selectedItem = document.querySelector('.selected');
+let completedItem = document.querySelector('.completed');
 
 for (let i=0; i<listItens.length; i += 1) {
   listItens[i].addEventListener('click', graySelected);
-  function graySelected() {
-      selectedItem = document.querySelector('.selected');
-      selectedItem.classList.remove('selected');
-      listItens[i].classList.add('selected');
-  }
+  listItens[i].addEventListener('dblclick', lineThroughSelected);
 }
 
-let selectedItem = '';
-const listItens = document.querySelectorAll('.listItem');
-selectedItem = document.querySelector('.completed');
-
-for (let i=0; i<listItens.length; i += 1) {
-  listItens[i].addEventListener('click', graySelected);
-  function graySelected() {
-      selectedItem = document.querySelector('.selected');
-      selectedItem.classList.remove('selected');
-      listItens[i].classList.add('selected');
-  }
+function graySelected(event) {
+  selectedItem = document.querySelector('.selected');
+  selectedItem.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
+function lineThroughSelected(event) {
+  // console.log(event.target.classList.value);
+  if (event.target.classList.contains('completed')){
+    event.target.classList.remove('completed')}
+  else {
+    event.target.classList.add('completed')}
+}
 
