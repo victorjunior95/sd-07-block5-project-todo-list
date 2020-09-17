c8de83a77bcfcba9c346844a314ad4cff66b98bf
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-17 16:28:24 -0300

    cada vez menos issues

--- script.js
+++ script.js
@@ -7,38 +7,38 @@ buttonAddTarefa.addEventListener('click', function () {
   listaTarefas.appendChild(itemLista);
 
   itemLista.addEventListener('click', function (event) {
-    let selectedItem = document.querySelector('.selected');
+    const selectedItem = document.querySelector('.selected');
     if (selectedItem !== null) {
-        selectedItem.classList.remove('selected');
+      selectedItem.classList.remove('selected');
     } event.target.classList.add('selected');
   });
 
   itemLista.addEventListener('dblclick', function (event) {
-      if (event.target.classList.contains('completed')){
-          event.target.classList.remove('completed');
-      }
-      else {
-        event.target.classList.add('completed');
-      }
+    if (event.target.classList.contains('completed')) {
+       event.target.classList.remove('completed');
+    }
+    else {
+      event.target.classList.add('completed');
+    }
   });
   document.getElementById('texto-tarefa').value = '';
 });
 // baseado em https://app.betrybe.com/course/fundamentals/javascript/dom-manipulation/js-part-6
 
-const buttonClear = document.getElementById('apaga-tudo')
+const buttonClear = document.getElementById('apaga-tudo');
 buttonClear.addEventListener('click', function () {
-  const itemsList = document.querySelectorAll('li')
+  const itemsList = document.querySelectorAll('li');
 
   for (let index = 0; index< itemsList.length; index += 1) {
-      const item = itemsList[index];
-      listaTarefas.removeChild(item);
+    const item = itemsList[index];
+    listaTarefas.removeChild(item);
   }
 });
 
 const buttonFinished = document.getElementById('remover-finalizados');
 buttonFinished.addEventListener('click', function () {
-    const checkList = document.getElementsByClassName('completed')
-  for(let index = checkList.length - 1; index >= 0; index -= 1) {
+  const checkList = document.getElementsByClassName('completed');
+  for (let index = checkList.length - 1; index >= 0; index -= 1) {
       const checkItem = checkList[index];
        listaTarefas.removeChild(checkItem);
     }
