cfa68bc8c8ee4101729a93be25cac45a730f3513
Filipi Firmino <contatoic3web@gmail.com>
2020-09-16 22:07:50 -0300

    CC review 01

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const btnClearList = document.querySelector('#apaga-tudo');
 const btnRemoveTask = document.querySelector('#remover-selecionado');
 
 const ol = document.createElement('ol');
-ol.id='lista-tarefas';
+ol.id = 'lista-tarefas';
 containerList.appendChild(ol);
 
 btnNewTask.addEventListener('click', function () {
   if(inputList.value === '') {
-   alert('Adicione uma tarefa!');
-  }else {
+    alert('Adicione uma tarefa!');
+  } else {
     const li = document.createElement('li') ;
     const listItem = document.createTextNode(inputList.value);
     li.className = 'task';
@@ -22,34 +22,34 @@ btnNewTask.addEventListener('click', function () {
 });
 
 btnClearList.addEventListener('click', function () {
-  if(ol.children.length === 0 ){
-   alert ('Não há tarefas listadas');
+  if (ol.children.length === 0 ) {
+    alert('Não há tarefas listadas');
   } else {
-   const taskListed = document.querySelectorAll('.task');
-   for (let indexList = 0; indexList < taskListed.length; indexList += 1) {
-    ol.removeChild(taskListed[indexList]);
-   }
+    const taskListed = document.querySelectorAll('.task');
+    for (let indexList = 0; indexList < taskListed.length; indexList += 1) {
+     ol.removeChild(taskListed[indexList]);
+    }
   }
 });
 
-ol.addEventListener ('click', function (event) {
-  if(event.target.className === 'task') {
-   for (let index = 0; index < ol.children.length; index += 1) {
-   ol.children[index].className = 'task';
-   }
-  event.target.className = 'selected';
+ol.addEventListener('click', function(event) {
+  if (event.target.className === 'task') {
+    for (let index = 0; index < ol.children.length; index += 1) {
+    ol.children[index].className = 'task';
+    }
+    event.target.className = 'selected';
   } else {
-  for (let index = 0; index < ol.children.length; index += 1) {
-   ol.children[index].className = 'task';
+    for (let index = 0; index < ol.children.length; index += 1) {
+    ol.children[index].className = 'task';
   }
- }
+  }
 });
 
-btnRemoveTask.addEventListener('click', function () {  
+btnRemoveTask.addEventListener('click', function () {
   const taskSelected = document.querySelector('.selected');
-  if(document.querySelector('.selected') == null){
-    alert ('Nenhuma tarefa selecionada');
-  }else{
+  if (document.querySelector('.selected') == null) {
+    alert('Nenhuma tarefa selecionada');
+  } else {
     ol.removeChild(taskSelected);
   }
 });
