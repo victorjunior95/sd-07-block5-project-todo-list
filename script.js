ed249bee920fb5756fa5e02ce5020b41f7e07354
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-16 23:01:34 -0300

    Adicionando a função que altera a cor de fundo do item e a função que limpa a lista

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
+// Seleciona elemento da lista
+function selectionListItem(event) {
+  let selectedItem = event.target;
+  selectedItem.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 // Criar elemento na lista
+const list = document.querySelector('#lista-tarefas');
+
 function createListItem() {
-  let list = document.querySelector('#lista-tarefas')
-  let listItem = document.createElement('li');
+  const listItem = document.createElement('li');
   list.appendChild(listItem);
-  listItem.className = 'tarefa';
+  // listItem.className = 'tarefa';
+  listItem.addEventListener('click', selectionListItem);
+  // listItem.addEventListener('dblclick', )
+  
   return listItem;
 }
 
 // Armazenar o texto escrito na lista
-  let typedText = document.querySelector('#texto-tarefa');
-  let taskButton = document.querySelector('#criar-tarefa');
+const typedText = document.querySelector('#texto-tarefa');
+const taskButton = document.querySelector('#criar-tarefa');
+
 function inputText() {
-  let textListItem = createListItem();
+  const textListItem = createListItem();
   textListItem.innerText = typedText.value;
   typedText.value = '';
 }
 
 taskButton.addEventListener('click', inputText);
+
+
+
+
+
+// Apaga itens da lista
+const clearButton = document.querySelector('#apaga-tudo');
+
+function removeListItem() {
+  list.innerHTML = '';
+}
+
+clearButton.addEventListener('click', removeListItem);
