f8e6c0c72c08f79319f2dd4cc2ab3ec9c95ea28e
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-17 22:47:19 -0400

    função para remover todas as tarefas criadas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para inserir tarefa
 const botaoAddTarefa = document.getElementById('criar-tarefa');
 botaoAddTarefa.addEventListener('click', function () {
   const tarefa = document.getElementById('texto-tarefa').value;
@@ -5,4 +6,12 @@ botaoAddTarefa.addEventListener('click', function () {
   const itemTarefa = document.getElementById('lista-tarefas');
   itemTarefa.appendChild(novoItem).innerHTML = tarefa;
   document.getElementById('texto-tarefa').value = '';
+});
+// Função para remover todos os itens da lista
+const botaoLimpaLista = document.getElementById('apaga-tudo');
+botaoLimpaLista.addEventListener('click', function () {
+  const itemTarefa = document.getElementById('lista-tarefas');
+  while (itemTarefa.firstChild) {
+    itemTarefa.removeChild(itemTarefa.firstChild);
+  }
 });
