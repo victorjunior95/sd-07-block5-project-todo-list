4753cfcf87e515690cf413a5ee0385fa11f72f13
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-17 21:27:20 -0400

    criada função para inserir tarefa

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoAddTarefa = document.getElementById('criar-tarefa');
+botaoAddTarefa.addEventListener('click', function () {
+  const tarefa = document.getElementById('texto-tarefa').value;
+  const novoItem = document.createElement('li');
+  const itemTarefa = document.getElementById('lista-tarefas');
+  itemTarefa.appendChild(novoItem).innerHTML = tarefa;
+});
