ff6b4d04708edb5aad0d30a4044f3f7da22c0d22
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-17 22:19:19 -0300

    commit: 0.0.9

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const backgroundColor = 'rgb(128, 128, 128)';
+const getItensList = document.getElementsByTagName('li');
+const comparedClass = 'completed';
 
 function AddItemList(item) {
   const itemList = document.createElement('li');
@@ -25,7 +27,7 @@ function chanceBackgroundColorItemList(boolean, itensList) {
   }
 }
 
-function checkIfExistItemSelectedInList (itensList) {
+function checkIfExistItemSelectedInList(itensList) {
   for (let index = 0; index < itensList.length; index += 1) {
     if (itensList[index].style.backgroundColor === backgroundColor) {
       return false;
@@ -34,8 +36,29 @@ function checkIfExistItemSelectedInList (itensList) {
   return true;
 }
 
-document.getElementById('lista-tarefas').addEventListener('click', function (event){
-  const getItensList = document.getElementsByTagName('li');
+document.getElementById('lista-tarefas').addEventListener('click', function (event) {
   const resultCheck = checkIfExistItemSelectedInList(getItensList);
   chanceBackgroundColorItemList(resultCheck, event.target);
+});
+
+function existClassCompleted(itemList) {
+  const comparedClass = 'completed';
+  if (itemList.getAttribute('class') === comparedClass) {
+    return true;
+  }
+  return false;
+}
+
+function addClasseCompleted(itemList) {
+  itemList.className = comparedClass;
+}
+
+function removeClasseCompleted(itemList) {
+  itemList.classList.remove(comparedClass);
+}
+document.getElementById('lista-tarefas').addEventListener('dblclick', function (event) {
+  const resultBoolean = existClassCompleted(event.target);
+  if (resultBoolean) {
+    removeClasseCompleted(event.target);
+  } else addClasseCompleted(event.target);
 });
