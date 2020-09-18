aa005f6a014829f4e7962d7475210d8f950db50e
Leonnardo Lair <leonnardo0316@gmail.com>
2020-09-18 16:08:14 -0300

    This is it!

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.querySelector("#criar-tarefa").addEventListener("click", function(){
     let inputText = document.createTextNode(document.querySelector("#texto-tarefa").value);
     let listItem = document.createElement("li");
     selectedjob = document.querySelector(".selected");
-    finishtask = document.querySelector(".completed");
     listItem.appendChild(inputText);
     listItem.classList.add("list-item");
     listItem.addEventListener("click",function(){
@@ -23,20 +22,28 @@ document.querySelector("#criar-tarefa").addEventListener("click", function(){
             }
         }
     })
-    listItem.addEventListener("dblclick", function (){
-        finishtask = document.querySelector(".completed");
-        if (finishtask.classList[1] = "completed" || finishtask.classList[2] = "completed") {
-        listItem.classList.remove("completed");
-        } 
-        else {
-            listItem.classList.add("completed");
-        }})
+    listItem.addEventListener("dblclick", function(){
+        if (document.querySelector(".completed") !== null) {
+        let taskfinished = document.querySelector(".completed");
+        if (listItem.classList[1] == "completed" || listItem.classList[2] == "completed") {
+            listItem.classList.remove("completed");
+            } 
+            else {
+                listItem.classList.add("completed");
+            }
+    } else {
+        listItem.classList.add("completed")
+    }
+        
+    })
     listOrdered.appendChild(listItem);
-    document.querySelector("#texto-tarefa").value = "";})
+    document.querySelector("#texto-tarefa").value = "";
+})
 
 document.querySelector("#apagar-tudo").addEventListener("click", function(){
     document.querySelectorAll("li").forEach(item => {
         item.parentNode.removeChild(item);
+})
 })
 
 document.querySelector("#remover-finalizados").addEventListener("click",function(){
