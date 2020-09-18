560b8d6bf8ed6a610759135eeedc8da0af06b50f
renatomak <renato.mark.silva@gmail.com>
2020-09-18 09:38:29 -0300

    refatoração do codigo2

--- script.js
+++ script.js
@@ -1,86 +1,89 @@
+const tarefaInput = document.getElementById('texto-tarefa');
+const listaDeTarefas = document.getElementById('lista-tarefas');
+const avisos = document.getElementById('avisos');
+let itemClicado = null;
+
+
+const itemSelecionado = function (event) {
+  event.stopPropagation();
+  if (event.target.nodeName === 'LI') {
+    limpaSeleao();
+    alteraCorFundo(event.target);
+    itemClicado = event.target;
+  }
+}
+
+const moveParaBaixo = function () {
+  avisos.textContent = '';
+  let proximo = itemClicado.nextSibling;
+  if (proximo != null) {
+    listaDeTarefas.insertBefore(itemClicado, proximo);
+    listaDeTarefas.insertBefore(proximo, itemClicado);
+    salvarLista;
+  } else {
+    avisos.textContent = 'Fim da lista!';
+  }
+}
+
+const moverParaCima = function () {
+  avisos.textContent = '';
+  let anterior = itemClicado.previousElementSibling;
+  if (anterior != null) {
+    listaDeTarefas.insertBefore(anterior, itemClicado);
+    listaDeTarefas.insertBefore(itemClicado, anterior);
+    salvarLista;
+  } else {
+    avisos.textContent = 'Fim da lista!';
+  }
+}
+
+const marcarCompleto = function (event) {
+  event.stopPropagation();
+  if (event.target.nodeName === 'LI') {
+    if (event.target.className === 'completed') {
+      event.target.className = '';
+    } else {
+      event.target.className = 'completed';
+    }
+  }
+  limpaSeleao();
+};
+
+/**Se criar uma variavel e atribuir a função não vai funcionar o desmarque quando marcar como compelto */
+function limpaSeleao() {
+  let itensLista = document.querySelectorAll('ol>li');
+  itensLista.forEach((element) => {
+    itemClicado = null;
+    element.style.backgroundColor = 'white';
+  });
+}
+
+const criaLi = function () {
+  let novoLi = document.createElement('li');
+  novoLi.textContent = tarefaInput.value;
+  listaDeTarefas.appendChild(novoLi);
+  tarefaInput.value = '';
+  tarefaInput.focus();
+}
+
+const alteraCorFundo = function (elem) {
+  elem.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+const salvarLista = function () {
+  localStorage.clear();
+  localStorage.setItem('listaTarefas', listaDeTarefas.innerHTML);
+}
+
+/**Não consigo fazer essa função ser auto executavel. Por quê? */
+function carregaItensSalvos() {
+  document.querySelector('ol').innerHTML = localStorage.getItem('listaTarefas');
+}
+
 window.onload = function () {
-  const tarefaInput = document.getElementById('texto-tarefa');
-  const listaDeTarefas = document.getElementById('lista-tarefas');
-  const avisos = document.getElementById('avisos');
-  let itemClicado = null;
+  
   carregaItensSalvos();
   tarefaInput.focus();
-
-  const itemSelecionado = function (event) {
-    event.stopPropagation();
-    if (event.target.nodeName === 'LI') {
-      limpaSeleao();
-      alteraCorFundo(event.target);
-      itemClicado = event.target;
-    }
-  }
-
-  const moveParaBaixo = function () {
-    avisos.textContent = '';
-    let proximo = itemClicado.nextSibling;
-    if (proximo != null) {
-      listaDeTarefas.insertBefore(itemClicado, proximo);
-      listaDeTarefas.insertBefore(proximo, itemClicado);
-      salvarLista;
-    } else {
-      avisos.textContent = 'Fim da lista!';
-    }
-  }
-
-  const moverParaCima = function () {
-    avisos.textContent = '';
-    let anterior = itemClicado.previousElementSibling;
-    if (anterior != null) {
-      listaDeTarefas.insertBefore(anterior, itemClicado);
-      listaDeTarefas.insertBefore(itemClicado, anterior);
-      salvarLista;
-    } else {
-      avisos.textContent = 'Fim da lista!';
-    }
-  }
-
-  const marcarCompleto = function (event) {
-    event.stopPropagation();
-    if (event.target.nodeName === 'LI') {
-      if (event.target.className === 'completed') {
-        event.target.className = '';
-      } else {
-        event.target.className = 'completed';
-      }
-    }
-    limpaSeleao();
-  };
-
-  /**Se criar uma variavel e atribuir a função não vai funcionar o desmarque quando marcar como compelto */
-  function limpaSeleao() {
-    let itensLista = document.querySelectorAll('ol>li');
-    itensLista.forEach((element) => {
-      itemClicado = null;
-      element.style.backgroundColor = 'white';
-    });
-  }
-
-  const criaLi = function () {
-    let novoLi = document.createElement('li');
-    novoLi.textContent = tarefaInput.value;
-    listaDeTarefas.appendChild(novoLi);
-    tarefaInput.value = '';
-    tarefaInput.focus();
-  }
-
-  const alteraCorFundo = function (elem) {
-    elem.style.backgroundColor = 'rgb(128, 128, 128)';
-  }
-
-  const salvarLista = function () {
-    localStorage.clear();
-    localStorage.setItem('listaTarefas', listaDeTarefas.innerHTML);
-  }
-
-  /**Não consigo fazer essa função ser auto executavel. Por quê? */
-  function carregaItensSalvos() {
-    document.querySelector('ol').innerHTML = localStorage.getItem('listaTarefas');
-  }
 
   document.getElementById('apaga-tudo').addEventListener('click', function () {
     const listaLi = document.querySelectorAll('ol>li');
