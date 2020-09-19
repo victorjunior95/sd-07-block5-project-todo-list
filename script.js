6ac49a5a654ee190c055866062f1df00378df687
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-19 18:55:53 -0300

    Implementando o botão salvar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const olList = document.getElementById('lista-tarefas');
 const cleanAllButton = document.querySelector('#apaga-tudo');
 const removeFinished = document.querySelector('#remover-finalizados');
 const removeSelected = document.querySelector('#remover-selecionado');
+const save = document.querySelector('#salvar-tarefas');
 function newEventListener(elementList) {
   elementList.addEventListener('click', function () {
     const selectedItem = document.querySelector('.selected');
@@ -13,7 +14,6 @@ function newEventListener(elementList) {
     elementList.classList.add('selected');
   }});
 }
-
 function completedTask(elementList) {
   elementList.addEventListener('dblclick', function () {
     if (elementList.classList.contains('completed')) {
@@ -23,24 +23,21 @@ function completedTask(elementList) {
     }
   });
 }
-
 function launcherTask(task) {
   const elementList = document.createElement('li');
   elementList.innerText = task;
+  elementList.className = 'job'
   olList.appendChild(elementList);
   newEventListener(elementList);
   completedTask(elementList);
   return elementList;
 }
-
 creatorButton.addEventListener('click', function () {
   const elementLi = document.getElementById('texto-tarefa');
   launcherTask(elementLi.value);
   elementLi.value = '';
 });
-
 cleanAllButton.addEventListener('click', function () {
-  const allLi = document.querySelectorAll('li');
   allLi.forEach(element => { olList.removeChild(element) })});
 
 removeFinished.addEventListener('click', function () {
@@ -51,7 +48,30 @@ removeSelected.addEventListener('click', function() {
   const removeSelectedElement = document.querySelector('.selected')
   olList.removeChild(removeSelectedElement);
 })
+save.addEventListener('click', function () {
+  localStorage.clear('');
+  const completed = document.querySelectorAll('.completed');
+  if (completed) {
+    const liList = document.querySelectorAll('li');
+    for (let index = 0; index < liList.length; index += 1) {
+      const value = liList[index].innerText;
+      localStorage.setItem(`item${index}`, value);
+      const NameElement = liList[index].className ;
+      localStorage.setItem(`class${index}`, NameElement);
+    }
+    alert('Lista Salva com sucesso');
+  }
+});
 
+function start() {
+  let getElement;
+  let getClass;
+  for (let elementStore = 0; elementStore < localStorage.length; elementStore += 1) {
+    getElement = localStorage.getItem(`item${elementStore}`);
+    getClass = localStorage.getItem(`class${elementStore}`);
+    const elementList = createNewTask(getItem);
+    elementList.className = getClass;
+  }
+}
 
-
-  
+start()
