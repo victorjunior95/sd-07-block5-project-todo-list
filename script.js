d050c6c7601f0533200f969391f550fd14a879b7
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-19 18:24:55 -0300

    code climate

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
 const textoTarefa = document.querySelector('#texto-tarefa');
 const botaoCriarTarefa = document.querySelector('#criar-tarefa');
-let listaTarefas = document.querySelector('#lista-tarefas');
+const listaTarefas = document.querySelector('#lista-tarefas');
 const botaoApagar = document.querySelector('#apaga-tudo');
 const botaoFinalizadas = document.querySelector('#remover-finalizados');
 const arrayListaTarefas = [];
-
-// adiciona as tarefas na lista e fundo cinza
-botaoCriarTarefa.addEventListener('click', function()
-{
-    let tarefas;
-    tarefas = document.createElement('li');
-    tarefas.className = 'itens-lista';
-    tarefas.innerHTML = textoTarefa.value;
-    listaTarefas.appendChild(tarefas);
-    textoTarefa.value = '';
-    tarefas.addEventListener('dblclick', tarefaCompleta);
-    arrayListaTarefas.push(tarefas);
-    for(let index = 0; index < arrayListaTarefas.length; index += 1){
-        arrayListaTarefas[index].addEventListener('click', function() {
-          for (let j = 0; j < arrayListaTarefas.length; j += 1) {
-              if (arrayListaTarefas[j].classList.contains('classeFundoCinza')) {
-                   arrayListaTarefas[j].classList.remove('classeFundoCinza');
-                }
-              arrayListaTarefas[index].className += ' classeFundoCinza';
-            };
-        });
-    };    
-});
 
 function tarefaCompleta(event){
     let itemSelecionado = event.target;
@@ -37,6 +14,30 @@ function tarefaCompleta(event){
     };
 };
 
+// adiciona as tarefas na lista e fundo cinza
+botaoCriarTarefa.addEventListener ('click', function() {
+  const tarefas;
+  tarefas = document.createElement('li');
+  tarefas.className = 'itens-lista';
+  tarefas.innerHTML = textoTarefa.value;
+  listaTarefas.appendChild(tarefas);
+  textoTarefa.value = '';
+  tarefas.addEventListener('dblclick', tarefaCompleta);
+  arrayListaTarefas.push(tarefas);
+  for(let index = 0; index < arrayListaTarefas.length; index += 1){
+       arrayListaTarefas[index].addEventListener('click', function() {
+        for (let j = 0; j < arrayListaTarefas.length; j += 1) {
+            if (arrayListaTarefas[j].classList.contains('classeFundoCinza')) {
+                 arrayListaTarefas[j].classList.remove('classeFundoCinza');
+            }
+            arrayListaTarefas[index].className += ' classeFundoCinza';
+        };
+        });
+    };
+});
+
+
+
 botaoApagar.addEventListener('click', apagaTudo);
 function apagaTudo(){
     listaTarefas.innerHTML = '';
@@ -45,7 +46,7 @@ function apagaTudo(){
 function apagaFinalizadas(){
     let arrayAtualizada = document.querySelectorAll('.completed');
     for(index = 0; index < arrayAtualizada.length; index += 1){
-        listaTarefas.removeChild(arrayAtualizada[index]);    
+        listaTarefas.removeChild(arrayAtualizada[index]);
     }
 }
 botaoFinalizadas.addEventListener('click', apagaFinalizadas);
@@ -54,6 +55,5 @@ let botaoConsole = document.querySelector('#botao-console');
 botaoConsole.addEventListener('click', function(){
     let body = document.querySelector('body');
     console.log(body);
-    console.log(listaTarefas);
-    
+    console.log(listaTarefas);  
 });
