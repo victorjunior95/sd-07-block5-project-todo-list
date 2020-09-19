a9571edac164b15895c95bc2efacd26ead98d743
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-19 16:30:13 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let textoTarefa = document.querySelector("#texto-tarefa");
 let botaoCriarTarefa = document.querySelector("#criar-tarefa");
 let listaTarefas = document.querySelector("#lista-tarefas");
+let botaoApagar = document.querySelector("#apaga-tudo");
+let botaoFinalizadas = document.querySelector("#remover-finalizados");
 let arrayListaTarefas = [];
 
 // adiciona as tarefas na lista
@@ -33,6 +35,40 @@ function tarefaCompleta(event){
         itemSelecionado.className += " completed";
     };
 };
+
+botaoApagar.addEventListener("click", apagaTudo);
+function apagaTudo(){
+    listaTarefas.innerHTML = "";
+}
+
+function apagaFinalizadas(){
+    let arrayAtualizada = document.querySelectorAll('.completed');
+    for(index = 0; index < arrayAtualizada.length; index += 1){
+        listaTarefas.removeChild(arrayAtualizada[index]);    
+    }
+
+
+}
+/*function apagaFinalizadas(){
+       const arrayAtualizada = document.querySelectorAll('li');
+        for(index = 0; index < arrayAtualizada.length; index += 1){
+            let indice = arrayAtualizada[index];
+            if(indice.classList.contains("completed")){
+                listaTarefas.removeChild(indice);
+            };
+        };        
+    };   */
+botaoFinalizadas.addEventListener("click", apagaFinalizadas);
+
+
+let botaoConsole = document.querySelector("#botao-console");
+botaoConsole.addEventListener('click', function(){
+    let body = document.querySelector("body");
+    console.log(body);
+    console.log(listaTarefas);
+    
+});
+
 
 // clicar em um item e alterar a cor de fundo para cinza
 // criar uma classe com background-color: rgb(128, 128, 128)
