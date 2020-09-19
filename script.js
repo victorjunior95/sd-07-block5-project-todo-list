d3f162d67493cbad4d8e9b0d716b8b21ba06e9e9
lisvalver <lisvalver.atte@gmail.com>
2020-09-19 19:25:09 -0300

    requisitos 7 + 8 + 9

--- script.js
+++ script.js
@@ -7,5 +7,33 @@ function addGoal() {
     let goalTextToAdd = document.createElement('li')
     goalTextToAdd.innerHTML = goalText.value
     goalList.appendChild(goalTextToAdd)
+    goalTextToAdd.addEventListener('click', selectedItem)
+    goalTextToAdd.addEventListener('dblclick', completItem)
     goalText.value = ''
+}
+
+function selectedItem() {
+    let elementTarget = this
+
+    if (elementTarget.classList.contains('selected')) {
+        elementTarget.classList.remove('selected')
+    } else {
+        let listItens = document.getElementsByTagName('li')
+        for (let index = 0; index < listItens.length; index += 1) {
+            if (listItens[index].classList.contains('selected')) {
+                listItens[index].classList.remove('selected')
+            }
+        }
+        elementTarget.classList.add('selected')
+    }
+}
+
+function completItem() {
+    let elementTarget = this
+
+    if (elementTarget.classList.contains('completed')) {
+        elementTarget.classList.remove('completed')
+    } else {
+        elementTarget.classList.add('completed')
+    }
 }
