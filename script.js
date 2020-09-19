ad23c47d1e27a95fb13f52b59ba7c1d074994ba6
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-19 10:11:53 -0300

    Implementando o botão 'All Clean'.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const creatorButton = document.getElementById('criar-tarefa');
 const olList = document.getElementById('lista-tarefas');
+const cleanAllButton = document.querySelector('#apaga-tudo');
+
 
 function newEventListener(elementList) {
   elementList.addEventListener('click', function () {
     const selectedItem = document.querySelector('.selected');
   if (selectedItem) {
     selectedItem.classList.remove('selected');
+    elementList.classList.add('selected');
   } else {
     elementList.classList.add('selected');
   }});
@@ -34,4 +37,11 @@ creatorButton.addEventListener('click', function () {
   const elementLi = document.getElementById('texto-tarefa');
   launcherTask(elementLi.value);
   elementLi.value = '';
+});
+
+cleanAllButton.addEventListener('click', function () {
+  const allLi = document.querySelectorAll('li');
+  for (let element = 0; element < allLi.length; element += 1) {
+    olList.removeChild(allLi[element]);
+  }
 });
