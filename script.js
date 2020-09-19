8d35e007eb9a53093005ab6fca8cf19492b6fe23
Eduardo Pizorno <pizorno@gmail.com>
2020-09-19 18:21:52 -0300

    Todo List 0.0.4 Efeito riscado em elementos li clicados

--- script.js
+++ script.js
@@ -7,13 +7,26 @@ document.getElementById("criar-tarefa").addEventListener("click", function (e){
   let list = document.getElementById("lista-tarefas");
   liElement.innerHTML = task.value;
   task.value = "";
+
   liElement.addEventListener("click", function(e) {
     let origin = document.querySelector(".grey");
     origin !== null ? origin.classList.remove("grey"):'';
     e.target.classList.add("grey");
   });
+
+  liElement.addEventListener("dblclick", function(e) {
+    e.target.classList.toggle("completed");
+  });
+
   taskList.push(liElement);
   list.appendChild(liElement);
 });
 
+// let theButton = document.getElementById('theButton');
+// let theText = document.querySelectorAll('.the-text');
 
+// theButton.onclick = function () { 
+//   for(let x of theText) {
+//     x.classList.toggle('colorized');
+//   }
+// };
