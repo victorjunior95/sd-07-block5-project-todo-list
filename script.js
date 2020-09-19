df80d173e3a40bf6749241e9a6b89cfa32c883ea
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-19 20:11:28 -0300

    Primeiras alterações do script.js para inclusão da função de incluir tarefas do quinto requisito

---
+++ script.js
@@ -0,0 +1,9 @@
+let addTask = document.getElementById("criar-tarefa");
+let list = document.getElementById("lista-tarefas");
+let inputTask = document.getElementById("texto-tarefa");
+
+addTask.addEventListener ("click", function() {
+    let newTask = document.createElement("li")
+    newTask.innerText = inputTask.value;
+    list.appendChild(newTask);
+});
