6f9e550478c262d8ee0d12bf251daefbbde73dc8
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-19 12:34:07 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let textoTarefa = document.querySelector("#texto-tarefa");
 let botaoCriarTarefa = document.querySelector("#criar-tarefa");
 let listaTarefas = document.querySelector("#lista-tarefas");
-let tarefas;
+let arrayListaTarefas = [];
 
+// adiciona as tarefas na lista
 botaoCriarTarefa.addEventListener("click", function(){
+    let tarefas;
     tarefas = document.createElement("li");
+    tarefas.className = "itens-lista";
     tarefas.innerHTML = textoTarefa.value;
     listaTarefas.appendChild(tarefas);
-    console.log(tarefas)
     textoTarefa.value = "";
+    arrayListaTarefas.push(tarefas);
+    for(let index = 0; index < arrayListaTarefas.length; index += 1){
+        arrayListaTarefas[index].addEventListener("click", function(){
+            let item = arrayListaTarefas[index];
+            item.style.backgroundColor = ''; 
+            arrayListaTarefas[index].classList.add("classeFundoCinza");
+        });
+    }
 })
 
-// digitar o texto no input 
-// clicar no botão - evento de pegar o texto de input e colocar na listaTarefas. 
-// Manter todos os itens na lista 
+
+
+// clicar em um item e alterar a cor de fundo para cinza
+// criar uma classe com background-color: rgb(128, 128, 128)
+// criar um for para passar por todos os elementos da ul
+// add evento que exclui a classe de qualquer elemento
+// add evento que, ao clicar no elemento da lista, essa classe seja add a ele.
