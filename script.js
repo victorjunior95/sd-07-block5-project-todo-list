144ea05da030c943d16702d3328a35214754bf25
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-19 18:16:26 -0300

    code climate-2

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const textoTarefa = document.querySelector("#texto-tarefa");
-const botaoCriarTarefa = document.querySelector("#criar-tarefa");
-let listaTarefas = document.querySelector("#lista-tarefas");
-const botaoApagar = document.querySelector("#apaga-tudo");
-const botaoFinalizadas = document.querySelector("#remover-finalizados");
+const textoTarefa = document.querySelector('#texto-tarefa');
+const botaoCriarTarefa = document.querySelector('#criar-tarefa');
+let listaTarefas = document.querySelector('#lista-tarefas');
+const botaoApagar = document.querySelector('#apaga-tudo');
+const botaoFinalizadas = document.querySelector('#remover-finalizados');
 const arrayListaTarefas = [];
 
 // adiciona as tarefas na lista e fundo cinza
-botaoCriarTarefa.addEventListener("click", function()
+botaoCriarTarefa.addEventListener('click', function()
 {
     let tarefas;
-    tarefas = document.createElement("li");
-    tarefas.className = "itens-lista";
+    tarefas = document.createElement('li');
+    tarefas.className = 'itens-lista';
     tarefas.innerHTML = textoTarefa.value;
     listaTarefas.appendChild(tarefas);
-    textoTarefa.value = "";
-    tarefas.addEventListener("dblclick", tarefaCompleta);
+    textoTarefa.value = '';
+    tarefas.addEventListener('dblclick', tarefaCompleta);
     arrayListaTarefas.push(tarefas);
     for(let index = 0; index < arrayListaTarefas.length; index += 1){
-        arrayListaTarefas[index].addEventListener("click", function(){
-            for(let j = 0; j < arrayListaTarefas.length; j += 1){
-                if(arrayListaTarefas[j].classList.contains("classeFundoCinza")){
-                    arrayListaTarefas[j].classList.remove("classeFundoCinza");
-                };
-                arrayListaTarefas[index].className += " classeFundoCinza";
+        arrayListaTarefas[index].addEventListener('click', function() {
+          for (let j = 0; j < arrayListaTarefas.length; j += 1) {
+              if (arrayListaTarefas[j].classList.contains('classeFundoCinza')) {
+                   arrayListaTarefas[j].classList.remove('classeFundoCinza');
+                }
+              arrayListaTarefas[index].className += ' classeFundoCinza';
             };
         });
     };    
@@ -30,16 +30,16 @@ botaoCriarTarefa.addEventListener("click", function()
 
 function tarefaCompleta(event){
     let itemSelecionado = event.target;
-    if(itemSelecionado.classList.contains("completed")){
-        itemSelecionado.classList.remove("completed");
+    if(itemSelecionado.classList.contains('completed')){
+        itemSelecionado.classList.remove('completed');
     } else {
-        itemSelecionado.className += " completed";
+        itemSelecionado.className += ' completed';
     };
 };
 
-botaoApagar.addEventListener("click", apagaTudo);
+botaoApagar.addEventListener('click', apagaTudo);
 function apagaTudo(){
-    listaTarefas.innerHTML = "";
+    listaTarefas.innerHTML = '';
 }
 
 function apagaFinalizadas(){
@@ -48,11 +48,11 @@ function apagaFinalizadas(){
         listaTarefas.removeChild(arrayAtualizada[index]);    
     }
 }
-botaoFinalizadas.addEventListener("click", apagaFinalizadas);
+botaoFinalizadas.addEventListener('click', apagaFinalizadas);
 
-let botaoConsole = document.querySelector("#botao-console");
+let botaoConsole = document.querySelector('#botao-console');
 botaoConsole.addEventListener('click', function(){
-    let body = document.querySelector("body");
+    let body = document.querySelector('body');
     console.log(body);
     console.log(listaTarefas);
     
