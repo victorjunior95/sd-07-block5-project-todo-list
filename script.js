148cada56f96a789395622484175fac50008ea04
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-09-19 23:01:03 -0300

    Alterações no HTML, CSS e JS para a inclusão de OL, LI e seletores

---
+++ script.js
@@ -0,0 +1,20 @@
+let buttonTarefa = document.getElementById(`criar-tarefa`);
+
+let ordinal = document.createElement(`OL`);
+ordinal.setAttribute(`id`, `bloco`);
+document.getElementById(`lista-tarefas`).appendChild(ordinal);
+
+buttonTarefa.addEventListener(`click`, function(){
+  let input = document.getElementById(`texto-tarefa`).value;
+  let linha = document.createElement(`LI`);
+  linha.addEventListener(`click`, function(event){
+    event.target.className += ` selected`;
+  })
+  
+  let texto = document.createTextNode(input);
+  linha.appendChild(texto);
+  document.getElementById(`bloco`).appendChild(linha);  
+  document.getElementById(`texto-tarefa`).value = ``;
+  let clear = document.getElementById(`texto-tarefa`).value;
+})
+
