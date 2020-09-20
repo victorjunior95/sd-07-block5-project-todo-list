6b9a2bb48914c12bdda15ab0de87b95ce6ef0943
Pedro Azevedo <pvas1996@gmail.com>
2020-09-20 19:44:57 -0300

    rev2

--- script.js
+++ script.js
@@ -2,17 +2,11 @@ let inputLista = document.getElementById("texto-tarefa");
 let listaTarefas = document.getElementById("lista-tarefas");
 let bttCriarTarefa = document.getElementById("criar-tarefa");
 
-bttCriarTarefa.addEventListener('click', function() {
-    // testar se o input tem conteudo
-    // criar a lista(li)
-    // colocar o conteudo na lista
-    //  add a li como filha da ol
-    // apagar o input
-    // voltar o foco ao input
+bttCriarTarefa.addEventListener('click', function () {
 
     if (inputLista.value) {
 
-    let item = document.createElement('li');
+        let item = document.createElement('li');
 
         item.innerText = inputLista.value;
 
@@ -24,3 +18,21 @@ bttCriarTarefa.addEventListener('click', function() {
 
     inputLista.focus();
 });
+
+listaTarefas.addEventListener('click', function(event) {
+    let allSelected = document.querySelector('.selected');
+    if (allSelected !== null) {
+       allSelected.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+});
+
+
+
+
+// listaTarefas.addEventListener( 'click', function (event){
+//     if(document.querySelector('.selected')) {
+//         document.querySelector('selected').classList,this.remove('selected')
+//     }
+//     event.target.classList.add('selected');
+// });
