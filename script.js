2c7a631e4a551ca55b33da4dd59d933af7efcf62
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-20 13:02:43 -0300

    commit: 0.0.21

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const backgroundColor = 'rgb(128, 128, 128)';
 const getItemsList = document.getElementsByTagName('li');
 const comparedClass = 'completed';
 const arrayItemsList = [];
-
 function salveItensListOnLocalStorage() {
   if (getItemsList.length === 0) {
     alert('Lista vazia');
@@ -120,40 +119,31 @@ document.getElementById('mover-cima').addEventListener('click', function (event)
 });
 
 function downItemList(itemSelected) {
-  const elementFather = itemSelected.parentNode
+  const elementFather = itemSelected.parentNode;
   const lastElementChild = elementFather.lastElementChild;
   const firstElementChild = elementFather.firstElementChild;
-
-  const textFirsElementChild = firstElementChild.innerText;
-  const textLastElementChild = lastElementChild.innerText;
+  const nextSibling = itemSelected.nextSibling;
 
   if (itemSelected === lastElementChild) {
-    lastElementChild.innerText = textFirsElementChild;
-    firstElementChild.innerText = textLastElementChild;
+    elementFather.insertBefore(itemSelected, firstElementChild);
   } else {
-      const nexDownItem =  itemSelected.nextElementSibling;
-      const textNexDownItem =  nexDownItem.innerText;
-      nexDownItem.innerText = itemSelected.innerText;
-      itemSelected.innerText = textNexDownItem;
+    elementFather.insertBefore(itemSelected, nextSibling.nextSibling);
   }
-  //itemSelected.style.backgroundColor = 'white';
+  itemSelected.style.backgroundColor = 'white';
 }
 
 function upItemList(itemSelected) {
+  const elementFather = itemSelected.parentNode;
+  const lastElementChild = elementFather.lastElementChild;
+  const firstElementChild = elementFather.firstElementChild;
+  const previousSibling = itemSelected.previousElementSibling;
 
-  if (getItemsList[0] === itemSelected) return alert('Não existe item a cima')
-
-  for (let index = 0; index < getItemsList.length; index += 1) {
-    if (getItemsList[index] === itemSelected) {
-      const beforeItemList = getItemsList[index - 1];
-      const textBeforeItemList = beforeItemList.innerText;
-
-      beforeItemList.innerText = itemSelected.innerText;
-      itemSelected.innerText = textBeforeItemList;
-      break;
-    }
+  if (itemSelected === firstElementChild) {
+    itemSelected.style.backgroundColor = 'white';
+    return alert('Não existem itens a cima')
   }
-  //itemSelected.style.backgroundColor = 'white';
+  elementFather.insertBefore(itemSelected, previousSibling);
+  itemSelected.style.backgroundColor = 'white';
 }
 
 document.getElementById('mover-cima').addEventListener('click', function() {
