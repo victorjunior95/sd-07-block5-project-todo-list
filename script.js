899a531ee95edc455ab315dc442a71938ee8e0b6
Thays Costa <thayscosta3@gmail.com>
2020-09-20 18:12:17 -0300

    Arrumando alguns erros no Code Climate #2.

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-
-let toDoList = document.getElementById('lista-tarefas');
-let taskName = document.getElementById('texto-tarefa');
+const toDoList = document.getElementById('lista-tarefas');
+const taskName = document.getElementById('texto-tarefa');
 taskName.focus();
-let createTaskBtn = document.getElementById('criar-tarefa');
+const createTaskBtn = document.getElementById('criar-tarefa');
 
-createTaskBtn.addEventListener('click', function(){
-  let item = document.createElement('li');
+createTaskBtn.addEventListener('click', function () {
+  const item = document.createElement('li');
   item.innerHTML = taskName.value;
   toDoList.appendChild(item);
   taskName.value = '';
   taskName.focus();
-  
-  item.addEventListener('click', function(){
-    let selectedItem = document.querySelector('.selected');
+  item.addEventListener('click', function () {
+    const selectedItem = document.querySelector('.selected');
     if (selectedItem) {
       selectedItem.classList.remove('selected');
-    } 
+    }
     item.classList.add('selected');
   });
 
-  item.addEventListener('dblclick', function(){
-    if (item.classList.contains('completed')){
+  item.addEventListener('dblclick', function () {
+    if (item.classList.contains('completed')) {
       item.classList.remove('completed');
     } else {
       item.classList.add('completed');
     }
   })
 
-  let clearBtn = document.getElementById('apaga-tudo');
-  clearBtn.addEventListener('click', function(){
+  const clearBtn = document.getElementById('apaga-tudo');
+  clearBtn.addEventListener('click', function () {
     toDoList.innerText = '';
-  })
+  });
   
-  let removeCompletedBtn = document.getElementById('remover-finalizados');
-  removeCompletedBtn.addEventListener('click', function(){
-    if (item.classList.contains('completed')){
+  const removeCompletedBtn = document.getElementById('remover-finalizados');
+  removeCompletedBtn.addEventListener('click', function () {
+    if (item.classList.contains('completed')) {
       toDoList.removeChild(item);
     }
   })
 
-  let removeSelectedBtn = document.getElementById('remover-selecionado');
-  removeSelectedBtn.addEventListener('click', function(){
-    if (item.classList.contains('selected')){
+  const removeSelectedBtn = document.getElementById('remover-selecionado');
+  removeSelectedBtn.addEventListener('click', function () {
+    if (item.classList.contains('selected')) {
       toDoList.removeChild(item);
     }
   });
