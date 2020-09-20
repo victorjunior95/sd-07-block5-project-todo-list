717d815b82ecefc27a5576cd09e21a4bd4d12e18
Camila Valentim <camilaifpb@hotmail.com>
2020-09-20 10:48:04 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
 const input =document.querySelector("#texto-tarefa");
 const buttonAdicionar = document.querySelector("#criar-tarefa");
+const buttonClear = document.querySelector("#apaga-tudo");
 
 const ol = document.createElement("ol");
 ol.id ="lista-tarefas";
@@ -16,5 +17,16 @@ buttonAdicionar.addEventListener('click', function(){
     li.appendChild(lista);
     ol.appendChild(li);
     input.value ="";
+  }
+});
+
+buttonClear.addEventListener('click', function () {
+  if (ol.children.length === 0 ) {
+    alert('Não há tarefas listadas');
+  } else {
+    const listas = document.querySelectorAll('.tarefa');
+    for (let index = 0; index < listas.length; index += 1) {
+     ol.removeChild(listas[index]);
+    }
   }
 });
