4162c7314f9f638bcbdf64aa66517ca3111149f2
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-20 15:25:36 -0300

    codeclimate 2

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 //função para alterar o background de um item na lista quando clicado
 
 function changeSelected(event) {
-  let item = document.querySelectorAll('li');
+  const item = document.querySelectorAll('li');
 
-  for (let i = 0; i < item.length;i += 1){
-    if (item[i].classList.contains('selected')){
-      let selected = document.querySelector('.selected');
+  for (let i = 0; i < item.length; i += 1) {
+    if (item[i].classList.contains('selected')) {
+      const selected = document.querySelector('.selected');
       selected.classList.remove('selected');
     }
   }
   event.target.classList.add('selected');
 }
 
-// função para riscar uma tarefa concluida
+//função para riscar uma tarefa concluida
 
 function checkList(event) {
-  console.log("ola");
-
-  if(event.target.classList.contains('completed')){
+  if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
-  }
-  else {
+  } else {
     event.target.classList.add('completed');
   }
 }
 
-// função para criar items na lista puxando o valor do input
+//função para criar items na lista puxando o valor do input
 
 function createListItem() {
   const ordList = document.getElementById('lista-tarefas');
@@ -47,114 +44,113 @@ addButton.addEventListener('click', createListItem);
 
 //função para salvar todos os itens da lista para uma proxima sessão
 
-  //função para salvar a lista
+//função para salvar a lista
 
-  function saveList() {
-    localStorage.clear();
+function saveList() {
+  localStorage.clear();
 
-    let ol = document.getElementById("lista-tarefas");
+  const ol = document.getElementById('lista-tarefas');
 
-    localStorage.setItem('itemList', ol.innerHTML);
-  }
-
-  let saveListButton = document.getElementById('salvar-tarefas');
-
-  saveListButton.addEventListener('click', saveList);
-
-  //função para puxar lista salva na ultima sessão
-
-  function newSession() {
-    let ol = document.getElementById("lista-tarefas");
-
-    ol.innerHTML = localStorage.getItem('itemList');
-  }
-
-  window.onload = newSession;
-
-// Função para apagar toda a lista
-
-function clearAll() {
-  let ol = document.getElementById("lista-tarefas");
-
-    while (ol.firstChild) {
-      ol.removeChild(ol.firstChild);
-  }
-
-  //função baseada no site: https://medium.com/front-end-weekly/remove-all-children-of-the-node-in-javascript-968ad8f120eb
+  localStorage.setItem('itemList', ol.innerHTML);
 }
 
-let clearButton = document.getElementById('apaga-tudo');
+let saveListButton = document.getElementById('salvar-tarefas');
 
-clearButton.addEventListener('click', clearAll)
+saveListButton.addEventListener('click', saveList);
+
+//função para puxar lista salva na ultima sessão
+
+function newSession() {
+  const ol = document.getElementById('lista-tarefas');
+
+  ol.innerHTML = localStorage.getItem('itemList');
+}
+
+window.onload = newSession;
+
+//Função para apagar toda a lista
+
+function clearAll() {
+  const ol = document.getElementById('lista-tarefas');
+
+  while (ol.firstChild) {
+    ol.removeChild(ol.firstChild);
+}
+
+//função baseada no site: https://medium.com/front-end-weekly/remove-all-children-of-the-node-in-javascript-968ad8f120eb
+}
+
+const clearButton = document.getElementById('apaga-tudo');
+
+clearButton.addEventListener('click', clearAll);
 
 //Função para apagar somente tarefas concluidas
 
-function clearCompletedList(){
-  let ol = document.getElementById("lista-tarefas");
-  let completed = document.querySelectorAll('.completed');
+function clearCompletedList() {
+  const ol = document.getElementById('lista-tarefas');
+  const completed = document.querySelectorAll('.completed');
 
   let index = 0;
 
-  while(index < completed.length){
+  while (index < completed.length) {
     ol.removeChild(completed[index]);
     index += 1;
   }
 }
 
-let completedListButton = document.getElementById('remover-finalizados');
+const completedListButton = document.getElementById('remover-finalizados');
 
 completedListButton.addEventListener('click', clearCompletedList);
 
-// função para apagar somente elemento selecionado
+//função para apagar somente elemento selecionado
 
 function clearSelectedItem() {
-  let ol = document.getElementById('lista-tarefas');
-  let selected = document.querySelector('.selected');
+  const ol = document.getElementById('lista-tarefas');
+  const selected = document.querySelector('.selected');
 
   ol.removeChild(selected);
 }
 
-let clearSelectedButton = document.getElementById('remover-selecionado');
+const clearSelectedButton = document.getElementById('remover-selecionado');
 
 clearSelectedButton.addEventListener('click', clearSelectedItem);
 
 //função para mover o elemento selecionado para cima
 
 function moveUp() {
-  let lis = document.querySelectorAll('li');
-  let selected = document.querySelector('.selected');
-  let ol = document.getElementById('lista-tarefas');
+  const lis = document.querySelectorAll('li');
+  const selected = document.querySelector('.selected');
+  const ol = document.getElementById('lista-tarefas');
 
   if (lis.length === 0 || selected === null) {
-    alert ('É necessário adicionar tarefas ou selecionar uma tarefa!');
+    alert('É necessário adicionar tarefas ou selecionar uma tarefa!');
   } else if (selected.previousElementSibling === null) {
-    alert ('Sua tarefa já está no topo da lista!');
+    alert('Sua tarefa já está no topo da lista!');
   } else {
     ol.insertBefore(selected, selected.previousElementSibling);
   }
 }
 
-let moveUpButton = document.getElementById('mover-cima');
+const moveUpButton = document.getElementById('mover-cima');
 
 moveUpButton.addEventListener('click', moveUp);
 
 //função para mover o elemento selecionado para baixo
 
 function moveDown() {
-  let lis = document.querySelectorAll('li');
-  let selected = document.querySelector('.selected');
-  let ol = document.getElementById('lista-tarefas');
+  const lis = document.querySelectorAll('li');
+  const selected = document.querySelector('.selected');
+  const ol = document.getElementById('lista-tarefas');
 
   if (lis.length === 0 || selected === null) {
-    alert ('É necessário adicionar tarefas ou selecionar uma tarefa!');
+    alert('É necessário adicionar tarefas ou selecionar uma tarefa!');
   } else if (selected.nextElementSibling === null) {
-    alert ('Sua tarefa já está no fim da lista!');
+    alert('Sua tarefa já está no fim da lista!');
   } else {
     ol.insertBefore(selected.nextElementSibling, selected);
   }
 }
 
-let moveDownButton = document.getElementById('mover-baixo');
+const moveDownButton = document.getElementById('mover-baixo');
 
 moveDownButton.addEventListener('click', moveDown);
-
