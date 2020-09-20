40af24a46c033338454f03ae540ed7463bce724b
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-20 15:29:52 -0300

    codeclimate 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//função para alterar o background de um item na lista quando clicado
+// Função para alterar o background de um item na lista quando clicado
 
 function changeSelected(event) {
   const item = document.querySelectorAll('li');
@@ -12,7 +12,7 @@ function changeSelected(event) {
   event.target.classList.add('selected');
 }
 
-//função para riscar uma tarefa concluida
+// Função para riscar uma tarefa concluida
 
 function checkList(event) {
   if (event.target.classList.contains('completed')) {
@@ -22,7 +22,7 @@ function checkList(event) {
   }
 }
 
-//função para criar items na lista puxando o valor do input
+// Função para criar items na lista puxando o valor do input
 
 function createListItem() {
   const ordList = document.getElementById('lista-tarefas');
@@ -42,9 +42,9 @@ function createListItem() {
 const addButton = document.getElementById('criar-tarefa');
 addButton.addEventListener('click', createListItem);
 
-//função para salvar todos os itens da lista para uma proxima sessão
+// Função para salvar todos os itens da lista para uma proxima sessão
 
-//função para salvar a lista
+// Função para salvar a lista
 
 function saveList() {
   localStorage.clear();
@@ -54,11 +54,11 @@ function saveList() {
   localStorage.setItem('itemList', ol.innerHTML);
 }
 
-let saveListButton = document.getElementById('salvar-tarefas');
+const saveListButton = document.getElementById('salvar-tarefas');
 
 saveListButton.addEventListener('click', saveList);
 
-//função para puxar lista salva na ultima sessão
+// Função para puxar lista salva na ultima sessão
 
 function newSession() {
   const ol = document.getElementById('lista-tarefas');
@@ -68,23 +68,23 @@ function newSession() {
 
 window.onload = newSession;
 
-//Função para apagar toda a lista
+// Função para apagar toda a lista
 
 function clearAll() {
   const ol = document.getElementById('lista-tarefas');
 
   while (ol.firstChild) {
     ol.removeChild(ol.firstChild);
-}
+  }
 
-//função baseada no site: https://medium.com/front-end-weekly/remove-all-children-of-the-node-in-javascript-968ad8f120eb
+// Função baseada no site: https://medium.com/front-end-weekly/remove-all-children-of-the-node-in-javascript-968ad8f120eb
 }
 
 const clearButton = document.getElementById('apaga-tudo');
 
 clearButton.addEventListener('click', clearAll);
 
-//Função para apagar somente tarefas concluidas
+// Função para apagar somente tarefas concluidas
 
 function clearCompletedList() {
   const ol = document.getElementById('lista-tarefas');
@@ -102,7 +102,7 @@ const completedListButton = document.getElementById('remover-finalizados');
 
 completedListButton.addEventListener('click', clearCompletedList);
 
-//função para apagar somente elemento selecionado
+// Função para apagar somente elemento selecionado
 
 function clearSelectedItem() {
   const ol = document.getElementById('lista-tarefas');
@@ -115,7 +115,7 @@ const clearSelectedButton = document.getElementById('remover-selecionado');
 
 clearSelectedButton.addEventListener('click', clearSelectedItem);
 
-//função para mover o elemento selecionado para cima
+// Função para mover o elemento selecionado para cima
 
 function moveUp() {
   const lis = document.querySelectorAll('li');
@@ -135,7 +135,7 @@ const moveUpButton = document.getElementById('mover-cima');
 
 moveUpButton.addEventListener('click', moveUp);
 
-//função para mover o elemento selecionado para baixo
+// Função para mover o elemento selecionado para baixo
 
 function moveDown() {
   const lis = document.querySelectorAll('li');
