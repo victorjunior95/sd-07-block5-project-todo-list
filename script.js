1317551607c7295a4d59b8c2101f14f80744b771
spawnred <bruno.spawnhell@gmail.com>
2020-09-20 20:51:28 -0300

    item 9

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-let btCriarTarefa = document.getElementById("criar-tarefa");
-let inputTextoTarefa = document.getElementById("texto-tarefa");
-let olListaTarefa = document.getElementById("lista-tarefas");
+let btCriarTarefa = document.getElementById('criar-tarefa');
+let inputTextoTarefa = document.getElementById('texto-tarefa');
+let olListaTarefa = document.getElementById('lista-tarefas');
+let btApagarTudo = document.getElementById('apaga-tudo');
+let btRemoverFinalizados = document.getElementById('remover-finalizados');
 
-btCriarTarefa.addEventListener('click', function(){
-    if(inputTextoTarefa.value){
-        let liTag = document.createElement('li');
-        liTag.innerText = inputTextoTarefa.value
-        olListaTarefa.appendChild(liTag);
-        inputTextoTarefa.value='';
-    }
-    inputTextoTarefa.focus(); 
+btCriarTarefa.addEventListener('click', function () {
+  if (inputTextoTarefa.value) {
+    let liTag = document.createElement('li');
+    liTag.innerText = inputTextoTarefa.value;
+    olListaTarefa.appendChild(liTag);
+    inputTextoTarefa.value = '';
+  }
+  inputTextoTarefa.focus();
 });
 
-olListaTarefa.addEventListener("click",function(event){
-    if(event.target.tagName !== 'OL'){
-    let liSelected = document.querySelector(".selected")
-        if(liSelected){
-        liSelected.classList.remove('selected')
-        }
-    event.target.classList.add('selected')
+olListaTarefa.addEventListener('click', function (event) {
+  if (event.target.tagName !== 'OL') {
+    let liSelected = document.querySelector('.selected');
+    if (liSelected) {
+      liSelected.classList.remove('selected');
     }
-})
+    event.target.classList.add('selected');
+  }
+});
 
-olListaTarefa.addEventListener('dblclick', function(event){
-    if(event.target.tagName !== 'OL'){
-        event.target.classList.toggle('completed')
-    }
-})
+olListaTarefa.addEventListener('dblclick', function (event) {
+  if (event.target.tagName !== 'OL') {
+    event.target.classList.toggle('completed');
+  }
+});
+btApagarTudo('click', function () {});
