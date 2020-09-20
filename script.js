f2532c18db93b26d3466d5a106dee616ceab67b5
Rita Jeveaux <ritajeveaux@gmail.com>
2020-09-20 10:21:28 -0300

    Terminados requisitos 7 e 8 obrigatorios

--- script.js
+++ script.js
@@ -2,26 +2,34 @@ const textTarefa = document.getElementById("texto-tarefa").value;
 const btnAdd = document.getElementById("criar-tarefa");
 
 //adicionando tarefa
-btnAdd.addEventListener("click", function() {
-   const paiLista = document.getElementById("lista-tarefas"); 
-   const novaLi = document.createElement("li");
-   const textTarefa = document.getElementById("texto-tarefa").value;
-   const cxInput = document.getElementById("texto-tarefa");
+
    
-   if(textTarefa.length != ""){
-      paiLista.appendChild(novaLi);
-      novaLi.innerText = textTarefa;
-      document.getElementById("texto-tarefa").value =""
-   }
-   novaLi.addEventListener("click", function(event) {
-      MudaCorFundo(novaLi);
+   function adicionaTarefa(){
+      const paiLista = document.getElementById("lista-tarefas"); 
+      const novaLi = document.createElement("li");
+      const textTarefa = document.getElementById("texto-tarefa").value;
+      const cxInput = document.getElementById("texto-tarefa");
+
+      if(textTarefa.length != ""){
+         paiLista.appendChild(novaLi);
+         novaLi.innerText = textTarefa;
+         document.getElementById("texto-tarefa").value =""
+      }
+      return novaLi;
+}
+   btnAdd.addEventListener("click", function() {
+      const novaLi = adicionaTarefa();
+      novaLi.addEventListener("click", function(event) {
+         mudaCorFundo(novaLi);
+      })
    })
+
+   
       
-})
+
 
 //mudando a cor de fundo da li
-function MudaCorFundo(novaLi){
-//const novaLi = document.getElementsByTagName('li');
+function mudaCorFundo(novaLi){
 novaLi.addEventListener("click", function(event) {
 const selected = document.querySelector(".selected");
    if (selected) {
