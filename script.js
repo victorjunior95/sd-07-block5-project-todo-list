4240fd0b6bd0c2d818b1495fa163588b2f83ebb3
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-20 16:07:25 -0300

    implementação dblclick

--- script.js
+++ script.js
@@ -16,19 +16,23 @@ function addItemLista() {
   }
 
 
-  document.addEventListener("click", function(event){
+document.addEventListener("click", function(event){
     let itens = document.getElementsByClassName("item");
-      if (event.target.className == "item")
-      
+    if (event.target.className == "item")
         if (event.target.style.backgroundColor != "rgb(128, 128, 128)"){
             for (let count = 0; count < itens.length; count+=1){
                 itens[count].style.backgroundColor = "rgb(255, 255, 255)";
             }
             event.target.style.backgroundColor = "rgb(128, 128, 128)";
         }
-            
         else{
             event.target.style.backgroundColor = "rgb(255, 255, 255)";
         }
-            
-  })
+})
+
+document.addEventListener("dblclick", function(event){
+    if (event.target.className == "item")
+        event.target.className = "item completed";
+    else if (event.target.className == "item completed")
+            event.target.className = "item";
+})
