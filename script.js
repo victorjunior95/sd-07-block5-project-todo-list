ce043e1002aea763d191d0e3958eb4a1a395d834
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-09-20 00:04:57 -0300

    Alterações para os itens 7 e 8 do projeto

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ buttonTarefa.addEventListener(`click`, function(){
   let input = document.getElementById(`texto-tarefa`).value;
   let linha = document.createElement(`LI`);
   linha.addEventListener(`click`, function(event){
+    let itens = document.getElementById("lista-tarefas").children;
+    for (i = 0; i < itens.length; i += 1){
+      if (itens[i].classList.contains(`selected`) == true){
+        itens[i].classList.remove(`selected`);
+      }
+    }
     event.target.className += ` selected`;
   })
-  
+  linha.addEventListener(`dblclick`, function(event){
+    if (event.target.classList.contains(`completed`) == true){
+      event.target.classList.remove(`completed`);
+    }
+    else {
+      event.target.className += ` completed`;
+    }
+    
+  })
   let texto = document.createTextNode(input);
   linha.appendChild(texto);
   document.getElementById(`lista-tarefas`).appendChild(linha);  
