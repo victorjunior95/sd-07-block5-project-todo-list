8b85d935482060bec73799c15de0bc71248f0172
Thays Costa <thayscosta3@gmail.com>
2020-09-20 18:36:28 -0300

    Arrumando alguns erros no Code Climate #4.

--- script.js
+++ script.js
@@ -4,45 +4,45 @@ taskName.focus();
 const createTaskBtn = document.getElementById('criar-tarefa');
 
 createTaskBtn.addEventListener('click', function () {
-  if (taskName.value !== ''){
-  const item = document.createElement('li');
-  item.innerHTML = taskName.value;
-  toDoList.appendChild(item);
-  taskName.value = '';
-  }
-  taskName.focus();
-  item.addEventListener('click', function () {
+  if (taskName.value !== '') {
+    const item = document.createElement('li');
+    item.innerHTML = taskName.value;
+    toDoList.appendChild(item);
+    taskName.value = '';
+    item.addEventListener('click', function () {
     const selectedItem = document.querySelector('.selected');
     if (selectedItem) {
       selectedItem.classList.remove('selected');
     }
     item.classList.add('selected');
-  });
-
-  item.addEventListener('dblclick', function () {
-    if (item.classList.contains('completed')) {
-      item.classList.remove('completed');
-    } else {
-      item.classList.add('completed');
-    }
-  });
-
-  const clearBtn = document.getElementById('apaga-tudo');
-  clearBtn.addEventListener('click', function () {
-    toDoList.innerText = '';
-  });
+    });
   
-  const removeCompletedBtn = document.getElementById('remover-finalizados');
-  removeCompletedBtn.addEventListener('click', function () {
-    if (item.classList.contains('completed')) {
-      toDoList.removeChild(item);
-    }
-  });
+    taskName.focus();
+    item.addEventListener('dblclick', function () {
+      if (item.classList.contains('completed')) {
+        item.classList.remove('completed');
+      } else {
+        item.classList.add('completed');
+      };
+    });
 
-  const removeSelectedBtn = document.getElementById('remover-selecionado');
-  removeSelectedBtn.addEventListener('click', function () {
-    if (item.classList.contains('selected')) {
-      toDoList.removeChild(item);
-    }
-  });
+    const clearBtn = document.getElementById('apaga-tudo');
+    clearBtn.addEventListener('click', function () {
+      toDoList.innerText = '';
+    });
+
+    const removeCompletedBtn = document.getElementById('remover-finalizados');
+    removeCompletedBtn.addEventListener('click', function () {
+      if (item.classList.contains('completed')) {
+        toDoList.removeChild(item);
+      };
+    });
+
+    const removeSelectedBtn = document.getElementById('remover-selecionado');
+    removeSelectedBtn.addEventListener('click', function () {
+      if (item.classList.contains('selected')) {
+        toDoList.removeChild(item);
+      }
+    });
+  };
 });
