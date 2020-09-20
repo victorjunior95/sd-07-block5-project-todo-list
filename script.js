a3cc18a1d27ef2a133585af44a386c8f4be1cef4
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-20 20:25:09 -0300

    add estilo rgb(128, 128, 128) em items marcados

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 'use strict'
 window.onload = function () {
 
-    // clearListedField();
+    clearListedField();
     insertTextBuildListField();
     clearField();
-    selectItem();
     RiskListChosen();
     paintListChosenAndTakeIt();
 
@@ -26,18 +25,18 @@ window.onload = function () {
     }
 
 
-    // function clearListedField() {
-    //     let li = document.querySelector('ol');
-    //     for (let item = 0; item < li.length; item += 1) {
-    //         document.querySelector('.clearReadyBtn').addEventListener('click', function () {
-    //             let testAttr = li[item].getAttribute('class');
-    //             if (testAttr === 'item selected') {
-    //                 console.log(li[item]);
-    //                 li[item].removeChild(li[item]);
-    //             }
-    //         });
-    //     }
-    // }
+    function clearListedField() {
+        let list = document.querySelectorAll('ol');
+        let btn = document.querySelector('.clearReadyBtn');
+            list.forEach(item => {
+                btn.addEventListener('click', function () {
+                    console.log(item.className === 'item selected');
+                    if (item.className === 'item selected' ) {
+                        list.removeChild(item);
+                    }
+                });
+            });
+    }
 
 
     function clearField() {
@@ -49,14 +48,14 @@ window.onload = function () {
         });
     }
 
-    function selectItem() {
-        let list = document.querySelectorAll('ol');
-        list.forEach(item => { // para cada elemento
-            item.addEventListener('dblclick', function (event) {
-                event.target.classList.add('selected');
-            });
-        });
-    }
+    // function selectItem() {
+    //     let list = document.querySelectorAll('ol');
+    //     list.forEach(item => { // para cada elemento
+    //         item.addEventListener('dblclick', function (event) {
+    //             event.target.classList.add('selected');
+    //         });
+    //     });
+    // }
 
     // function RiskListChosen() {
     //     let list = document.querySelectorAll('ol');
@@ -68,16 +67,20 @@ window.onload = function () {
     // }
     function RiskListChosen() {
         let list = document.querySelectorAll('ol');
-        for (let item = 0; item < list.length; item += 1) {
-            list[item].addEventListener('dblclick', function (event) {
+        list.forEach(item => {
+            item.addEventListener('dblclick', function (event) {
+                event.target.classList.add('selected');
                 if (event.target.style.textDecoration === 'line-through solid rgb(0, 0, 0)') {
                     event.target.style.textDecoration = '';
+                    event.target.classList.remove('selected');
                 } else
                     event.target.style.textDecoration = 'line-through solid';
-                    event.target.style.textDecorationColor = 'rgb(0,0,0)'
+                event.target.style.textDecorationColor = 'rgb(0,0,0)'
             });
-        }
+        });
+
     }
+
 
     function paintListChosenAndTakeIt() {
         let list = document.querySelectorAll('ol');
