4033955894fe07db7fe330d443b84e0a64b2df62
berchon <berchon@gmail.com>
2020-09-20 20:12:38 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let btCriarTarefa = document.getElementById('criar-tarefa');
 let inpTextoTarefa = document.getElementById('texto-tarefa');
 let olListaTarefa = document.getElementById('lista-tarefas');
 
-console.log(inpTextoTarefa.value);
-
 btCriarTarefa.addEventListener('click', function () {
   // Testar de input tem conteudo
   // criar li
@@ -14,7 +12,7 @@ btCriarTarefa.addEventListener('click', function () {
   if (inpTextoTarefa.value !== '') {
     let liTag = document.createElement('li');
 
-    liTag.innerText = inpTextoTarefa.value
+    liTag.innerText = inpTextoTarefa.value;
     // let text = document.createTextNode(inpTextoTarefa.value);
     // liTag.appendChild(text)
 
@@ -24,3 +22,20 @@ btCriarTarefa.addEventListener('click', function () {
   }
   inpTextoTarefa.focus();
 });
+
+olListaTarefa.addEventListener('click', function (event) {
+  if (event.target.tagName == 'LI') {
+    let todasAsLISelecionadas = document.querySelector('.selected');
+    if (todasAsLISelecionadas != null) {
+      todasAsLISelecionadas.classList.remove('selected')
+    }
+    event.target.classList.add('selected');
+  }
+
+
+})
+
+
+// add escuta li clicado
+// add escuta ol
+// criar uma classe liClicada com a cor
