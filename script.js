5225d84d774d1c1a4dead66c50841f70b1ae4d70
Leonardo <lecastro16@gmail.com>
2020-09-20 20:54:27 -0300

    exercicios 1 ao 9

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ let arrayListaComTodasTarefas = [];
 
 // Adicionando itens na lista, apagando o campo de preenchimento
 botaoCriarTarefa.addEventListener("click", function () {
-  let itemParaAdicionar = document.createElement("li");
-  itemParaAdicionar.classList.add("lista");
-  itemParaAdicionar.innerText = textoTarefa.value;
-  arrayListaComTodasTarefas.push(itemParaAdicionar.innerText);
-  listaTarefas.appendChild(itemParaAdicionar);
-  textoTarefa.value = "";
-  textoTarefa.focus();
+  if (textoTarefa.value) {
+    let itemParaAdicionar = document.createElement("li");
+    itemParaAdicionar.classList.add("lista");
+    itemParaAdicionar.innerText = textoTarefa.value;
+    arrayListaComTodasTarefas.push(itemParaAdicionar.innerText);
+    listaTarefas.appendChild(itemParaAdicionar);
+    textoTarefa.value = "";
+    textoTarefa.focus();
+  }
 });
 
 // Pintando intens da lista de cinza
 listaTarefas.addEventListener("click", function (event) {
   let itemARemover = document.getElementsByClassName("selected")[0];
-  if (document.getElementsByClassName("selected")[0] != null) {
+  if (itemARemover != null) {
     itemARemover.classList.remove("selected");
-  } 
+  }
   event.target.classList.add("selected");
 });
 
 // Riscando os itens
-console.log(arrayListaComTodasTarefas);
+listaTarefas.addEventListener("dblclick", function (event) {
+  event.target.classList.toggle("completed");
+});
