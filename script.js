897c423ac7884d8a548f2b8cebcfa0cef3ee87cb
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-20 17:48:27 -0300

    Criaçao funçao selection e identaçao code climate

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const buttonCriarTarefa = document.querySelector("#criar-tarefa");
-const inputTarefa = document.querySelector("#texto-tarefa");
-const listaTarefas = document.querySelector("#lista-tarefas");
+const buttonCriarTarefa = document.querySelector('#criar-tarefa');
+const inputTarefa = document.querySelector('#texto-tarefa');
+const listaTarefas = document.querySelector('#lista-tarefas');
 
-buttonCriarTarefa.addEventListener("click", function () {
-  const tarefaLista = document.createElement("li");
+buttonCriarTarefa.addEventListener('click', function () {
+  const tarefaLista = document.createElement('li');
+  tarefaLista.setAttribute('id', 'tarefa-salva');
   tarefaLista.innerText = inputTarefa.value;
   listaTarefas.appendChild(tarefaLista);
-  inputTarefa.value = "";
+  inputTarefa.value = '';
+});
+
+listaTarefas.addEventListener('click', function (event) {
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').style.backgroundColor = "white";
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+  event.target.style.backgroundColor = "rgb(128,128,128)";
 });
