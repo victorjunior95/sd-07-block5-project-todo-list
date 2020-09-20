c23fb92533d6c8e58b8e776a179d15c0b0d0218b
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-20 15:59:25 -0300

    Ajustando code climate 08 - 20/09/2020

--- script.js
+++ script.js
@@ -1,91 +1,78 @@
-window.onload = function () {
-  updateList();
-  createJob();
-  changeColorJob();
-  completedJob();
-  removeJob();
-  removeCompletedJob();
-  saveJob();
-  moveUp();
-  moveDown();
-  removeSelected();
-};
-
-const inputText = document.querySelector("#texto-tarefa");
-const buttonCreateJob = document.querySelector("#criar-tarefa");
-const list = document.querySelector("#lista-tarefas");
+const inputText = document.querySelector('#texto-tarefa');
+const buttonCreateJob = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
 
 function createJob() {
-  buttonCreateJob.addEventListener("click", function () {
+  buttonCreateJob.addEventListener('click', function () {
     createLi(inputText.value);
   });
 }
 
 function createLi(valueLi, liClass) {
-  let li = document.createElement("li");
+  let li = document.createElement('li');
   list.appendChild(li);
-  li.classList.add("li", liClass);
+  li.classList.add('li', liClass);
   li.innerText = valueLi;
   clearInput();
   // console.log(li);
 }
 
 function clearInput() {
-  inputText.value = "";
+  inputText.value = '';
 }
 
 function changeColorJob() {
-  list.addEventListener("click", function (event) {
+  list.addEventListener('click', function (event) {
     let li = event.target;
     let children = list.children;
     for (let i = 0; i < children.length; i += 1) {
-      children[i].style.backgroundColor = "";
+      children[i].style.backgroundColor = '';
     }
-    li.style.backgroundColor = "rgb(128, 128, 128)";
+    li.style.backgroundColor = 'rgb(128, 128, 128)';
   });
 }
 
 function completedJob() {
-  list.addEventListener("dblclick", function (event) {
-    let li = event.target;
-    if (li.classList.contains("completed")) {
-      li.classList.remove("completed");
+  list.addEventListener('dblclick', function (event) {
+    const li = event.target;
+    if (li.classList.contains('completed')) {
+      li.classList.remove('completed');
     } else {
-      li.classList.add("completed");
+      li.classList.add('completed');
       // console.log(li.classList);
     }
   });
 }
 
-const buttonRemoveJob = document.querySelector("#apaga-tudo");
+const buttonRemoveJob = document.querySelector('#apaga-tudo');
 
 function removeJob() {
-  buttonRemoveJob.addEventListener("click", function () {
-    list.innerHTML = "";
+  buttonRemoveJob.addEventListener('click', function () {
+    list.innerHTML = '';
     localStorage.clear();
   });
 }
 
-const buttonRemoveCompletedJob = document.querySelector("#remover-finalizados");
+const buttonRemoveCompletedJob = document.querySelector('#remover-finalizados');
 
 function removeCompletedJob() {
-  buttonRemoveCompletedJob.addEventListener("click", function () {
-    let li = document.getElementsByClassName("completed");
+  buttonRemoveCompletedJob.addEventListener('click', function () {
+    let li = document.getElementsByClassName('completed');
     for (let i = li.length - 1; i >= 0; i -= 1) {
       list.removeChild(li[i]);
     }
   });
 }
 
-const buttonSaveJob = document.querySelector("#salvar-tarefas");
+const buttonSaveJob = document.querySelector('#salvar-tarefas');
 
 function saveJob() {
-  buttonSaveJob.addEventListener("click", function () {
-    let li = document.querySelectorAll(".li");
+  buttonSaveJob.addEventListener('click', function () {
+    let li = document.querySelectorAll('.li');
     for (let i = li.length - 1; i >= 0; i -= 1) {
       let object = {
         text: li[i].innerText,
-        classList: li[i].classList.contains("completed"),
+        classList: li[i].classList.contains('completed'),
       };
 
       let stringStorage = JSON.stringify(object);
@@ -100,24 +87,23 @@ function updateList() {
 
   for (let i = 0; i < localStorageLength; i += 1) {
     let objectStorage = JSON.parse(localStorage.getItem(i));
-    
     if (objectStorage.classList == true) {
-      createLi(objectStorage.text, "completed");
+      createLi(objectStorage.text, 'completed');
     } else {
       createLi(objectStorage.text);
     }
   }
 }
 
-const buttonMoveUp = document.querySelector("#mover-cima");
+const buttonMoveUp = document.querySelector('#mover-cima');
 
 function moveUp() {
-  buttonMoveUp.addEventListener("click", function () {
-    let li = document.getElementsByTagName("li");
+  buttonMoveUp.addEventListener('click', function () {
+    let li = document.getElementsByTagName('li');
 
     for (let i = 0; i < li.length; i += 1) {
       if (
-        li[i].style.backgroundColor == "rgb(128, 128, 128)" &&
+        li[i].style.backgroundColor == 'rgb(128, 128, 128)' &&
         li[i].previousElementSibling !== null
       ) {
         list.insertBefore(li[i], li[i].previousElementSibling);
@@ -126,15 +112,15 @@ function moveUp() {
   });
 }
 
-const buttonMoveDown = document.querySelector("#mover-baixo");
+const buttonMoveDown = document.querySelector('#mover-baixo');
 
 function moveDown() {
-  buttonMoveDown.addEventListener("click", function () {
-    let li = document.getElementsByTagName("li");
+  buttonMoveDown.addEventListener('click', function () {
+    let li = document.getElementsByTagName('li');
 
     for (let i = 0; i < li.length; i += 1) {
       if (
-        li[i].style.backgroundColor == "rgb(128, 128, 128)" &&
+        li[i].style.backgroundColor == 'rgb(128, 128, 128)' &&
         li[i].nextElementSibling !== null
       ) {
         list.insertBefore(li[i].nextElementSibling, li[i]);
@@ -144,16 +130,29 @@ function moveDown() {
   });
 }
 
-const buttonRemoveSelected = document.querySelector("#remover-selecionado");
+const buttonRemoveSelected = document.querySelector('#remover-selecionado');
 
 function removeSelected () {
-  buttonRemoveSelected.addEventListener("click", function () {
-    let li = document.getElementsByTagName("li");
+  buttonRemoveSelected.addEventListener('click', function () {
+    let li = document.getElementsByTagName('li');
     for ( let i = 0; i< li.length; i += 1) {
-      if (li[i].style.backgroundColor == "rgb(128, 128, 128)") {
+      if (li[i].style.backgroundColor == 'rgb(128, 128, 128)') {
         list.removeChild(li[i]);
       }
     }
   })
 
 }
+
+window.onload = function () {
+  updateList();
+  createJob();
+  changeColorJob();
+  completedJob();
+  removeJob();
+  removeCompletedJob();
+  saveJob();
+  moveUp();
+  moveDown();
+  removeSelected();
+};
