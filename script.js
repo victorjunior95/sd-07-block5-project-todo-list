e1166d2e070681263832a4f9628fbfc87ee99ca7
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-20 16:43:50 -0400

    criada função para selecionar itens da lista, sendo possível selecionar somente 1 item por vez

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ botaoAddTarefa.addEventListener('click', function () {
   const itemTarefa = document.getElementById('lista-tarefas');
   itemTarefa.appendChild(novoItem).innerHTML = tarefa;
   document.getElementById('texto-tarefa').value = '';
+  elementosLista();
 });
 // Função para remover todos os itens da lista
 const botaoLimpaLista = document.getElementById('apaga-tudo');
@@ -15,3 +16,23 @@ botaoLimpaLista.addEventListener('click', function () {
     itemTarefa.removeChild(itemTarefa.firstChild);
   }
 });
+// Função para mudar a cor de fundo de item selecionado
+function elementosLista () {
+  let selecionaItem = document.getElementById('lista-tarefas').children;
+  for (let index = 0; index < selecionaItem.length; index += 1) {
+    selecionaItem[index].addEventListener('click', function () {
+      for ( let index = 0; index < selecionaItem.length; index += 1) {
+        document.getElementById('lista-tarefas').children[index].removeAttribute('class');
+      }
+      document.getElementById('lista-tarefas').children[index].setAttribute('class', 'selected');
+    });
+  }
+}  
+// Função para remover finalizados
+selecionaItem = document.getElementById('lista-tarefas').children;
+for (let index = 0; index < selecionaItem.length; index += 1) {
+  let selecionado = document.getElementById('lista-tarefas').children[index];
+  if (selecionado.hasAttributes = 'selected') {
+    console.log(selecionado);
+  }
+}
