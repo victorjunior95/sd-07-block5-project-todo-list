86c530c70ad86764e11677bc144dcb2a6db7bdc5
marcelosoliveira <msbobsk8@gmail.com>
2020-09-20 08:33:27 -0300

    code climate todo-list

--- script.js
+++ script.js
@@ -9,138 +9,121 @@ const botaoCima = document.querySelector('#mover-cima');
 const botaoBaixo = document.querySelector('#mover-baixo');
 
 function criaLista(inputText) {
-    if (inputText.value === '') {
-        alert("Preencha os campos");
-    } else {
-        let criaLista = document.createElement('li');
-        criaLista.innerText = inputText.value;
-        lista.appendChild(criaLista);
-        inputText.value = '';
-    }
+  if (inputText.value === '') {
+    alert("Preencha os campos");
+  } else {
+    let criaLista = document.createElement('li');
+    criaLista.innerText = inputText.value;
+    lista.appendChild(criaLista);
+    inputText.value = '';
+  }
 }
 
 function selected() {
-    let listada =  document.querySelectorAll('#lista-tarefas li');
-    for (let index = 0; index < listada.length; index += 1) {
-        listada[index].addEventListener('click', (event) => {
-            for (let sel = 0; sel < listada.length; sel += 1) {
-                if (listada[sel].className === 'selected' ||
-                listada[sel].className === 'completed') {
-                    listada[sel].classList.remove('selected');
-                    listada[sel].style.backgroundColor = '';                        
-                }
-            }
-            if (listada[index].className === 'completed') {
-                listada[index].className = 'completed'
-            } else {
-                listada[index].className = 'selected';
-            }
-            if (listada[index].className === 'completed' ||
-            listada[index].className === 'selected') {
-                listada[index].style.backgroundColor = 'rgb(128,128,128)';
-            }                              
-        });
-    }
+  let listada =  document.querySelectorAll('#lista-tarefas li');
+  for (let index = 0; index < listada.length; index += 1) {
+    listada[index].addEventListener('click', () => {
+      for (let sel = 0; sel < listada.length; sel += 1) {
+        if (listada[sel].className === 'selected' ||
+        listada[sel].className === 'completed') {
+          listada[sel].classList.remove('selected');
+          listada[sel].style.backgroundColor = '';
+        }
+      }
+      if (listada[index].className === 'completed') {
+        listada[index].className = 'completed'
+      } else {
+          listada[index].className = 'selected';
+        }
+        if (listada[index].className === 'completed' ||
+        listada[index].className === 'selected') {
+          listada[index].style.backgroundColor = 'rgb(128,128,128)';
+        }
+    });
+  }
 }
 
 lista.addEventListener('dblclick' , (event) => {
-    if (event.target.className === 'completed') {
-         event.target.className = 'selected';                                              
-    } else  {
-        event.target.className = 'completed'; 
-    }                                                        
+  if (event.target.className === 'completed') {
+    event.target.className = 'selected';
+  } else  {
+    event.target.className = 'completed';
+  }
 });
 
-
 adicionar.addEventListener('click', function () {
-    criaLista(inputText);
-    selected();           
+  criaLista(inputText);
+  selected();
 });
 
 botaoLimpa.addEventListener('click', function () {
-    let listada =  document.querySelectorAll('#lista-tarefas li');
-    for (let index = 0; index < listada.length; index += 1) {
-        if (listada.length > 0) {
-            listada[index].remove();
-        }
-        
+  let listada =  document.querySelectorAll('#lista-tarefas li');
+  for (let index = 0; index < listada.length; index += 1) {
+    if (listada.length > 0) {
+      listada[index].remove();
     }
+  }
 });
 
 botaoCompleto.addEventListener('click', function () {
-    let listada =  document.querySelectorAll('#lista-tarefas li');
-    for (let index = 0; index < listada.length; index += 1) {
-        if (listada[index].className === 'completed') {
-            listada[index].remove(document.getElementsByClassName('completed'));                  
-        }
+  let listada =  document.querySelectorAll('#lista-tarefas li');
+  for (let index = 0; index < listada.length; index += 1) {
+    if (listada[index].className === 'completed') {
+      listada[index].remove(document.getElementsByClassName('completed'));
     }
+  }
 });
 
-botaoSalvar.addEventListener('click', (event) => {
-    let listada = document.querySelectorAll('#lista-tarefas li');
-    let todo = [];
-    for (let index = 0; index < listada.length; index += 1) {
-        /*todo.push(listada[index].outerText);*/
-        todo.push(listada[index].outerHTML); /* Retorna as tags e todos nomes de atributos da tag*/            
-   }
-   localStorage.setItem(`lista-todos`, JSON.stringify(todo));
-})
+botaoSalvar.addEventListener('click', () => {
+  let listada = document.querySelectorAll('#lista-tarefas li');
+  let todo = [];
+  for (let index = 0; index < listada.length; index += 1) {
+    todo.push(listada[index].outerHTML); /*Retorna as tags e todos nomes de atributos da tag*/
+  }
+  localStorage.setItem(`lista-todos`, JSON.stringify(todo));
+});
 
 window.onload = render;
-function render() {    
-    let listTodo = JSON.parse(localStorage.getItem('lista-todos'));
-    if (listTodo !== null) {
-        for (let index = 0; index < listTodo.length; index += 1) {
-            lista.innerHTML += listTodo[index];
-        }
-        selected();
+function render() {
+  let listTodo = JSON.parse(localStorage.getItem('lista-todos'));
+  if (listTodo !== null) {
+    for (let index = 0; index < listTodo.length; index += 1) {
+      lista.innerHTML += listTodo[index];
     }
+    selected();
+  }
 }
 
 botaoRemover.addEventListener('click', function () {
-    let listada =  document.querySelectorAll('#lista-tarefas li');
-    for (let index = 0; index < listada.length; index += 1) {
-        if (listada[index].className === 'selected') {
-            listada[index].remove(document.getElementsByClassName('selected'));                  
-        }
+  let listada =  document.querySelectorAll('#lista-tarefas li');
+  for (let index = 0; index < listada.length; index += 1) {
+    if (listada[index].className === 'selected') {
+      listada[index].remove(document.getElementsByClassName('selected'));
     }
+  }
 });
 
 botaoCima.addEventListener('click', function () {
-    let listada =  document.querySelectorAll('#lista-tarefas li');
-    for (let index = 0; index < listada.length; index += 1) {
-        if (listada[index].className === 'selected') {
-            if (listada[index].previousElementSibling === null) {
-                alert("Você é o primeiro!");
-            } else {
-                listada[index].previousElementSibling.before(listada[index]);  
-            }                           
-        }
+  let listada =  document.querySelectorAll('#lista-tarefas li');
+  for (let index = 0; index < listada.length; index += 1) {
+    if (listada[index].className === 'selected') {
+      if (listada[index].previousElementSibling === null) {
+        alert("Você é o primeiro!");
+      } else {
+        listada[index].previousElementSibling.before(listada[index]);
+      }
     }
+  }
 });
 
-/*botaoBaixo.addEventListener('click', function () {
-    let listada =  document.querySelectorAll('#lista-tarefas li');
-    for (let index = 0; index < listada.length; index += 1) {
-        if (listada[index].className === 'selected') {
-            if (listada[index].nextElementSibling === null) {
-                alert("Você é o ultimo!");
-            } else {
-                listada[index].nextElementSibling.before(listada[index]);  
-            }                           
-        }
-    }
-});*/
-
 botaoBaixo.addEventListener('click', function () {
-    let move =  document.querySelector('.selected');
-         if(move !== null) {
-            if (move.nextElementSibling === null) {
-                alert("Você é o ultimo!");
-            } else {
-                let baixo = move.nextElementSibling;  
-                move.before(baixo);
-            }
-         }
-        
+  let move =  document.querySelector('.selected');
+  if(move !== null) {
+    if (move.nextElementSibling === null) {
+      alert("Você é o ultimo!");
+    } else {
+      let baixo = move.nextElementSibling;
+      move.before(baixo);
+    }
+  }
 });
