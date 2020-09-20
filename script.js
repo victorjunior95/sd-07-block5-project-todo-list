d18edcc0e917ab6c4ad58710a1cb693e0b1eecee
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-20 20:49:14 -0300

    Alterações do script.js para inclusão do comando de limpar o input para o quinto requisito

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ addTask.addEventListener ("click", function() {
     let newTask = document.createElement("li")
     newTask.innerText = inputTask.value;
     list.appendChild(newTask);
+    inputTask.value = "";
 });
