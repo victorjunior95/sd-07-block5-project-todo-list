88f2d8ec64a65ddc05361db0ceca84771266cb28
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-20 15:54:18 -0300

    add estilo rgb(128, 128, 128) em items marcados

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 'use strict'
 window.onload = function () {
 
+    // clearListedField();
     insertTextBuildListField();
     clearField();
     selectItem();
-    listChosen();
+    RiskListChosen();
+    PaintListChosen();
+
+    function printGray() {
+
+    }
 
     function insertTextBuildListField() {
         document.querySelector("#criar-tarefa").addEventListener("click", function () {
@@ -23,6 +29,21 @@ window.onload = function () {
         });
     }
 
+
+    // function clearListedField() {
+    //     let li = document.querySelector('ol');
+    //     for (let item = 0; item < li.length; item += 1) {
+    //         document.querySelector('.clearReadyBtn').addEventListener('click', function () {
+    //             let testAttr = li[item].getAttribute('class');
+    //             if (testAttr === 'item selected') {
+    //                 console.log(li[item]);
+    //                 li[item].removeChild(li[item]);
+    //             }
+    //         });
+    //     }
+    // }
+
+
     function clearField() {
         document.querySelector('.btnClear').addEventListener('click', function () {
             let li = document.getElementsByTagName('ol');
@@ -30,16 +51,14 @@ window.onload = function () {
                 li[item].innerHTML = '';
             }
         });
-
     }
 
-    function selectItem(){
+    function selectItem() {
         let list = document.querySelectorAll('ol');
-        console.log(list);
         list.forEach(item => { // para cada elemento
-            item.addEventListener('click', function (event) {
+            item.addEventListener('dblclick', function (event) {
                 event.target.classList.add('selected'); //verifica qual elemento recebeu a ação
-                list.forEach(item => { // efetuo loop para identificar se 
+                list.forEach(item => { // efetuo loop para identificar se
                     if (item !== event.target) {
                         item.classList.remove('selected');
                     }
@@ -48,12 +67,20 @@ window.onload = function () {
         });
     }
 
-    function listChosen() {
+    function RiskListChosen() {
+        let list = document.querySelectorAll('ol');
+        for (let item = 0; item < list.length; item += 1) {
+            list[item].addEventListener('dblclick', function (event) {
+                event.target.style.textDecoration = 'line-through';
+            });
+        }
+    }
+
+    function PaintListChosen() {
         let list = document.querySelectorAll('ol');
         for (let item = 0; item < list.length; item += 1) {
             list[item].addEventListener('click', function (event) {
-                    event.target.style.textDecoration = 'line-through';
-              
+                event.target.style.color = 'rgb(128, 128, 128)';
             });
         }
     }
