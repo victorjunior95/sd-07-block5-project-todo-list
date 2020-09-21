2997672ec1640c70e64934a6931ef5d7d9ba5ed1
paulohp7 <ph.pereira.7@gmail.com>
2020-09-21 19:43:42 -0300

    Nineth objective

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let botao1 = document.querySelector('#criar-tarefa');
 let caixaTexto = document.querySelector('#texto-tarefa');
 let lista = document.querySelector('#lista-tarefas');
+let vetorLista = lista.getElementsByTagName('li');
 
 function criarTarefa() {
     let item = document.createElement('li');
@@ -8,10 +9,26 @@ function criarTarefa() {
     lista.appendChild(item);
     caixaTexto.value = '';
     item.addEventListener("click", function() {mudarCor(item);});
+    //item.addEventListener("click", function() {tiraCor(item);});
+    item.addEventListener("dblclick", function() {riscaItem(item);});
 }
 
 function mudarCor(item) {
     item.style.backgroundColor = "gray";
 }
+
+function tiraCor(item) {
+    if(item.style.backgroundColor === "gray")  item.style.backgroundColor = "white";
+}
+
+function riscaItem(item) {
+    if(item.style.textDecoration === "none") {
+        item.style.textDecoration = "line-through";
+    }
+    else {
+        item.style.textDecoration = "none";
+    } 
+}
+
 
 botao1.addEventListener("click", criarTarefa);
