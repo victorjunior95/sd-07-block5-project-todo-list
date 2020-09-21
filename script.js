e32d95759bcb06fa5d3e077ba8b5810f285e44e0
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-20 23:10:23 -0300

    Alterações para a chamada da função que troca a cor dos itens da lista

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 let addTask = document.getElementById("criar-tarefa");
 let list = document.getElementById("lista-tarefas");
 let inputTask = document.getElementById("texto-tarefa");
-let color = document.getElementById("selected-item");
 
 addTask.addEventListener ("click", function() {
     let newTask = document.createElement("li")
     newTask.innerText = inputTask.value;
     list.appendChild(newTask);
     inputTask.value = "";
-});
 
-function changeColor() {
-    document.querySelectorAll("li").forEach((item) => {
-        item.addEventListener("click", function () {
-            item.style.backgroundColor = "rgb(128, 128, 128)"
-    });
-  });
-};
-changeColor()
+    function changeColor() {
+        document.querySelectorAll("li").forEach((item) => {
+            item.addEventListener("click", function () {
+                item.style.backgroundColor = "rgb(128, 128, 128)";
+        });
+      });
+    };
+    changeColor()
+});
