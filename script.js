dac68be9df3c38e6a7418d2f1a4d4cc6eb0050cc
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-21 12:45:01 -0400

    criada função para selecionar e marcar como concluído

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Função para mudar a clase com 1 ou 2 cliques
+function elementosLista (parametro) {
+  parametro.addEventListener('click', function () {
+    parametro.setAttribute('class', 'selected');
+  });
+  parametro.addEventListener('dblclick', function () {
+    parametro.setAttribute('class', 'completed');
+  });
+}
 // Função para inserir tarefa
 const botaoAddTarefa = document.getElementById('criar-tarefa');
 botaoAddTarefa.addEventListener('click', function () {
@@ -6,7 +15,8 @@ botaoAddTarefa.addEventListener('click', function () {
   const itemTarefa = document.getElementById('lista-tarefas');
   itemTarefa.appendChild(novoItem).innerHTML = tarefa;
   document.getElementById('texto-tarefa').value = '';
-  elementosLista();
+  removeClasse;
+  elementosLista(novoItem);
 });
 // Função para remover todos os itens da lista
 const botaoLimpaLista = document.getElementById('apaga-tudo');
@@ -16,23 +26,14 @@ botaoLimpaLista.addEventListener('click', function () {
     itemTarefa.removeChild(itemTarefa.firstChild);
   }
 });
-// Função para mudar a cor de fundo de item selecionado
-function elementosLista () {
-  let selecionaItem = document.getElementById('lista-tarefas').children;
-  for (let index = 0; index < selecionaItem.length; index += 1) {
-    selecionaItem[index].addEventListener('click', function () {
-      for ( let index = 0; index < selecionaItem.length; index += 1) {
-        document.getElementById('lista-tarefas').children[index].removeAttribute('class');
-      }
-      document.getElementById('lista-tarefas').children[index].setAttribute('class', 'selected');
-    });
-  }
-}  
 // Função para remover finalizados
-selecionaItem = document.getElementById('lista-tarefas').children;
-for (let index = 0; index < selecionaItem.length; index += 1) {
-  let selecionado = document.getElementById('lista-tarefas').children[index];
-  if (selecionado.hasAttributes = 'selected') {
-    console.log(selecionado);
+removeCompleto = document.getElementById('remover-finalizados');
+totalLista = document.getElementById('lista-tarefas').children.length;
+removeCompleto.addEventListener('click', function () { 
+  for (let index = 0; index < totalLista; index += 1) {
+    const selecionado = document.getElementById('lista-tarefas').children[index];
+    if (selecionado.hasAttributes == 'selected') {
+      console.log("selecionado");
+    }
   }
-}
+});
