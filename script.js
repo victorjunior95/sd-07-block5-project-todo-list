af08583ca0e5e42b9171abfc8ec9f56cf6cb98c3
Alstent <rafaelasltent@gmail.com>
2020-09-22 01:53:00 +0200

    adicinando input tarefas e funcionalidade pra criar as tarefas

---
+++ script.js
@@ -0,0 +1,17 @@
+const addTarefa = document.querySelector('#criar-tarefa');
+const tarefaList = document.querySelector('#lista-tarefas')
+
+addTarefa.addEventListener('click', function() {
+  const tarefaInput = document.querySelector('#texto-tarefa').value;
+  const newTarefa = document.createElement('li');
+  newTarefa.innerHTML = tarefaInput;
+  tarefaList.appendChild(newTarefa);
+  newTarefa.addEventListener('click', function() {
+    const color = newTarefa.style.backgroundColor;
+    if (color === 'rgb(128, 128, 128)') {
+      newTarefa.style.backgroundColor = null;
+    }else {
+      newTarefa.style.backgroundColor = 'rgb(128,128,128)';
+    }
+  });
+});
