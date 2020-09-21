2da2f72af4333d03793d6b5def302cd307c00e88
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-20 22:12:20 -0300

    Revert "outra fatoração afim de solucionar o problema do click"
    This reverts commit f3c9d9c6a4b3f0efef17d45092648813fcfc68f6.

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let texto = document.getElementById('texto-tarefa');
 let criar = document.getElementById('criar-tarefa');
 let lista = document.getElementById('lista-tarefas');
 
-criar.addEventListener('click', function(event) {
-    if (event.target.tagName == 'LI') {
-        let todasAsLISelecionadas = document.querySelector('.selected');
-        if (todasAsLISelecionadas != null) {
-            todasAsLISelecionadas.classList.remove('selected');
-        }
-        event.target.classList.add('selected');
+criar.addEventListener('click', function() {
+    if (texto.value !== '') {
+        let li = document.createElement('li');
+        li.innerText = texto.value;
+        lista.appendChild(li);
+        texto.value = '';
+        texto.focus();
     }
 });
 
@@ -24,10 +24,8 @@ apagarLista.addEventListener('click', function() {
 });
 
 lista.addEventListener('dblclick', function(event) {
-    if (event.target.classList.contains('completed')) {
-        event.target.classList.remove('completed');
-    } else {
-        event.target.classList.add('completed');
+    if (event.target.tagName == 'LI') {
+        event.target.classList.toggle('completed');
     }
 });
 
