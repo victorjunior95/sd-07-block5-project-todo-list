5830ab3eb986c3c90b881b4b9881120e6a7f6c8c
Alstent <rafaelasltent@gmail.com>
2020-09-22 18:23:39 +0200

    adicionando butao de mover up & down e de salvar lista

--- script.js
+++ script.js
@@ -2,12 +2,43 @@ const addTarefa = document.querySelector('#criar-tarefa');
 const tarefaList = document.querySelector('#lista-tarefas');
 const limparList = document.querySelector('#apaga-tudo');
 const removeCompleted = document.querySelector('#remover-finalizados');
+const removeSelected = document.querySelector('#remover-selecionado');
+const moveUpBtn = document.querySelector('#move-up');
+const moveDownBtn = document.querySelector('#move-down');
+const saveList = document.querySelector('#salvar-tarefas')
+
+saveList.addEventListener('click', function() {
+  localStorage.clear();
+  localStorage.setItem('tarefaList', tarefaList.innerHTML);
+});
+
+window.onload = loadSaveList();
+function loadSaveList() {
+  let savedList = localStorage.getItem('tarefaList');
+  if (savedList !== null) {
+    tarefaList.innerHTML = savedList;
+  }
+}
+
+moveUpBtn.addEventListener('click', function () {
+  const tSelected = document.querySelector('.t-selected');
+  if ( tSelected !== null && tSelected.previousElementSibling !== null) {
+    tarefaList.insertBefore(tSelected, tSelected.previousElementSibling);
+  }
+});
+
+moveDownBtn.addEventListener('click', function () {
+  const tSelected = document.querySelector('.t-selected');
+  if (tSelected !== null && tSelected.nextElementSibling !== null) {
+    tarefaList.insertBefore(tSelected.nextElementSibling, tSelected);
+  }
+});
 
 function tSelect(a) {
-  const newTarefa = a
+  const newTarefa = a;
   newTarefa.addEventListener('click', function (select) {
     const color = newTarefa.style.backgroundColor;
-    if (color === 'rgb(128, 128, 128)') {
+    if (color === 'gray') {
       newTarefa.style.backgroundColor = null;
     } else {
       const tSelected = document.querySelector('.t-selected');
@@ -20,11 +51,11 @@ function tSelect(a) {
 }
 
 function tComplete(a) {
-  const newTarefa = a
+  const newTarefa = a;
   newTarefa.addEventListener('dblclick', function (select) {
     const decor = newTarefa.style.textDecoration;
-    if (decor === 'ine-through solid rgb(0, 0, 0)') {
-      tSelected.classList.remove('completed');
+    if (decor === 'ine-through solid black') {
+      newTarefa.classList.remove('completed');
     } else {
       select.target.className += ' completed';
     }
@@ -34,7 +65,7 @@ function tComplete(a) {
 addTarefa.addEventListener('click', function () {
   const tarefaInput = document.querySelector('#texto-tarefa').value;
   const newTarefa = document.createElement('li');
-  if (tarefaInput){
+  if (tarefaInput) {
     newTarefa.setAttribute('class', 'tarefa');
     newTarefa.innerHTML = tarefaInput;
     tarefaList.appendChild(newTarefa);
@@ -48,10 +79,10 @@ limparList.addEventListener('click', function () {
   while (tarefaList.firstChild) {
     tarefaList.removeChild(tarefaList.firstChild);
   }
-})
+});
 
 removeCompleted.addEventListener('click', function () {
-  document.querySelectorAll('.completed').forEach(function(completed) {
+  document.querySelectorAll('.completed').forEach(function (completed) {
     completed.remove();
   });
 });
@@ -60,3 +91,7 @@ removeCompleted.addEventListener('click', function () {
   const completedArray = document.getElementsByClassName("completed");
   while (completedArray.length > 0) elements[0].remove();
 } */
+
+removeSelected.addEventListener('click', function () {
+  document.querySelector('.t-selected').remove()
+})
