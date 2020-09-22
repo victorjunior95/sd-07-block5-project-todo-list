af9a515d598828483a8e92740c8d7d4a3e1346de
Alstent <rafaelasltent@gmail.com>
2020-09-22 11:17:40 +0200

    adicionando butao de limpar lista e remover as tarefas concluidas

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
 const addTarefa = document.querySelector('#criar-tarefa');
 const tarefaList = document.querySelector('#lista-tarefas');
+const limparList = document.querySelector('#apaga-tudo');
+const removeCompleted = document.querySelector('#remover-finalizados');
 
-addTarefa.addEventListener('click', function () {
-  const tarefaInput = document.querySelector('#texto-tarefa').value;
-  const newTarefa = document.createElement('li');
-  newTarefa.innerHTML = tarefaInput;
-  tarefaList.appendChild(newTarefa);
-  document.querySelector('#texto-tarefa').value = ''
-  newTarefa.addEventListener('click', function () {
+function tSelect(a) {
+  const newTarefa = a
+  newTarefa.addEventListener('click', function (select) {
     const color = newTarefa.style.backgroundColor;
     if (color === 'rgb(128, 128, 128)') {
       newTarefa.style.backgroundColor = null;
     } else {
-      newTarefa.style.backgroundColor = 'rgb(128,128,128)';
+      const tSelected = document.querySelector('.t-selected');
+      select.target.className += ' t-selected';
+      if (tSelected) {
+        tSelected.classList.remove('t-selected');
+      }
     }
   });
+}
+
+function tComplete(a) {
+  const newTarefa = a
+  newTarefa.addEventListener('dblclick', function (select) {
+    const decor = newTarefa.style.textDecoration;
+    if (decor === 'ine-through solid rgb(0, 0, 0)') {
+      tSelected.classList.remove('completed');
+    } else {
+      select.target.className += ' completed';
+    }
+  });
+}
+
+addTarefa.addEventListener('click', function () {
+  const tarefaInput = document.querySelector('#texto-tarefa').value;
+  const newTarefa = document.createElement('li');
+  if (tarefaInput){
+    newTarefa.setAttribute('class', 'tarefa');
+    newTarefa.innerHTML = tarefaInput;
+    tarefaList.appendChild(newTarefa);
+    document.querySelector('#texto-tarefa').value = '';
+  }
+  tSelect(newTarefa);
+  tComplete(newTarefa);
 });
+
+limparList.addEventListener('click', function () {
+  while (tarefaList.firstChild) {
+    tarefaList.removeChild(tarefaList.firstChild);
+  }
+})
+
+removeCompleted.addEventListener('click', function () {
+  document.querySelectorAll('.completed').forEach(function(completed) {
+    completed.remove();
+  });
+});
+
+/* function removeCompleted() { !OUTRA FORMA DE ELIMINAR POR CLASS!
+  const completedArray = document.getElementsByClassName("completed");
+  while (completedArray.length > 0) elements[0].remove();
+} */
