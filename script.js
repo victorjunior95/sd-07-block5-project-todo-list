24753c4ae7b6b9a69db38fb28e0dc89f0e116775
Alstent <rafaelasltent@gmail.com>
2020-09-22 02:02:39 +0200

    corrigindo pequenos erros

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const addTarefa = document.querySelector('#criar-tarefa');
-const tarefaList = document.querySelector('#lista-tarefas')
+const tarefaList = document.querySelector('#lista-tarefas');
 
-addTarefa.addEventListener('click', function() {
+addTarefa.addEventListener('click', function () {
   const tarefaInput = document.querySelector('#texto-tarefa').value;
   const newTarefa = document.createElement('li');
   newTarefa.innerHTML = tarefaInput;
   tarefaList.appendChild(newTarefa);
-  newTarefa.addEventListener('click', function() {
+  document.querySelector('#texto-tarefa').value = ''
+  newTarefa.addEventListener('click', function () {
     const color = newTarefa.style.backgroundColor;
     if (color === 'rgb(128, 128, 128)') {
       newTarefa.style.backgroundColor = null;
-    }else {
+    } else {
       newTarefa.style.backgroundColor = 'rgb(128,128,128)';
     }
   });
