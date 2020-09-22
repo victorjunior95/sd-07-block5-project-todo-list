34619648fc33bae8e9f9d7eefa4bb08745dfb49b
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-22 16:44:52 -0300

    implementando função apagar todos

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 criarTarefaButton = document.querySelector('#criar-tarefa');
+removerFinalizadosButton = document.querySelector('#remover-finalizados');
+salvarTarefasButton = document.querySelector('#salvar-tarefas');
+removerSelecionadosButton = document.querySelector('#remover-selecionado');
+apagaTudoButton = document.querySelector('#apaga-tudo');
 listaDeTarefas = document.querySelector('#lista-tarefas');
 tarefaInput = document.querySelector('#texto-tarefa');
 
 criarTarefaButton.addEventListener('click', function () {
   listaDeTarefas.innerHTML += listaDeTarefas.innerHTML = `<li>${tarefaInput.value}</li>`;
   tarefaInput.value = '';
+});
+
+apagaTudoButton.addEventListener('click', function () {
+  $('#lista-tarefas').empty();
 });
