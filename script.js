24fe936bb0f2f456aaa92c14ce1b6d83a031fbd0
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-22 18:17:36 -0300

    adicionando futuras implementações

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 criarTarefaButton = document.querySelector('#criar-tarefa');
-removerFinalizadosButton = document.querySelector('#remover-finalizados');
 salvarTarefasButton = document.querySelector('#salvar-tarefas');
-removerSelecionadosButton = document.querySelector('#remover-selecionado');
 apagaTudoButton = document.querySelector('#apaga-tudo');
 listaDeTarefas = document.querySelector('#lista-tarefas');
 tarefaInput = document.querySelector('#texto-tarefa');
-itemLista = document.querySelectorAll('.item-lista');
-
-//   addEventOnItemLista()
-//   // $('#lista-tarefas').append(function () {
-//   //   return $(this).children().get().reverse()
-//   // });
 
 
+// $('#lista-tarefas').append(function () {
+//   return $(this).children().get().reverse()
+// });
+
+// ADICIONA UM NOVO ITEM
 $('#criar-tarefa').click(function () {
   $('#lista-tarefas').append(`<li class="item-lista">${tarefaInput.value}</li>`);
   tarefaInput.value = '';
@@ -22,6 +19,20 @@ $('#criar-tarefa').click(function () {
   // });
 });
 
+// REMOVE OS FINALIZADOS
+$('#remover-finalizados').click(function () {
+  $('.completed').remove();
+});
+
+// TODO: SALVAR TAREFAS WEBSTORAGE
+
+
+// REMOVE OS SELECIONADOS
+$('#remover-selecionado').click(function () {
+  $('.selected').remove();
+});
+
+// APAGA TUDO
 $('#apaga-tudo').click(function () {
   $('#lista-tarefas').empty();
 });
