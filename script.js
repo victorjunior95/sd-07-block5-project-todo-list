119fc4e45a3d2c43518ea2e77b5fddd4c2c4f9ac
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-22 15:50:03 -0300

    implementação da criação de tarefas

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+criarTarefaButton = document.querySelector('#criar-tarefa');
+listaDeTarefas = document.querySelector('#lista-tarefas');
+tarefaInput = document.querySelector('#texto-tarefa');
 
+criarTarefaButton.addEventListener('click', function () {
+  listaDeTarefas.innerHTML += listaDeTarefas.innerHTML = `<li>${tarefaInput.value}</li>`;
+  tarefaInput.value = '';
+});
