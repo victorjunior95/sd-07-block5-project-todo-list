e24abfc841bddec9ce901222af4a557f55efd748
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-22 13:43:32 -0300

    feito ate o 100%

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-const buttonAdd = document.getElementById("criar-tarefa");
-const inputText = document.getElementById("texto-tarefa");
-const outList = document.getElementById("lista-tarefas");
-const buttonRemoveAll = document.getElementById('apaga-tudo')
+const buttonAdd = document.getElementById('criar-tarefa');
+const inputText = document.getElementById('texto-tarefa');
+const outList = document.getElementById('lista-tarefas');
+const buttonRemoveAll = document.getElementById('apaga-tudo');
+const buttonRemoveEnd = document.getElementById('remover-finalizados')
 
-buttonAdd.addEventListener("click", function () {
-  if (!inputText.value == "") {
-    let liElement = document.createElement("li");
+buttonAdd.addEventListener('click', function () {
+  if (!inputText.value == '') {
+    let liElement = document.createElement('li');
     liElement.innerText = inputText.value;
     outList.appendChild(liElement);
-    inputText.value = "";
+    inputText.value = '';
   } else {
-    alert("campo de texto vazio");
+    alert('campo de texto vazio');
   }
   inputText.focus();
 });
 
-outList.addEventListener("click", function (event) {
-  let selectedClass = document.querySelector(".selected");
+outList.addEventListener('click', function (event) {
+  let selectedClass = document.querySelector('.selected');
   if (selectedClass != null) {
-    selectedClass.classList.remove("selected");
+    selectedClass.classList.remove('selected');
   }
 
-  event.target.classList.add("selected");
+  event.target.classList.add('selected');
 });
 
-outList.addEventListener('dblclick', function(event) {
-
-  event.target.classList.add('completed')
-  event.target.classList.add('completed')
-})
-buttonRemoveAll.addEventListener('click',function() {
-  outList.innerHTML = ''
+outList.addEventListener('dblclick', function (event) {
+  if (event.target.tagName == 'LI') {
+    event.target.classList.toggle('completed');
+  }
+});
+buttonRemoveAll.addEventListener('click', function () {
+  outList.innerHTML = '';
+});
+buttonRemoveEnd.addEventListener('click', function() {
+  let finalizados = document.querySelectorAll('.completed');
+  for(index = 0;index < finalizados.length;index += 1) {
+    finalizados[index].remove();
+  }
 })
