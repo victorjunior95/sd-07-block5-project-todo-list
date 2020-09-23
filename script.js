f559f154d75ee3d13a7736f362419eb58586cd23
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-23 00:42:20 -0300

    refatorando

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-criarTarefaButton = document.querySelector('#criar-tarefa');
-salvarTarefasButton = document.querySelector('#salvar-tarefas');
-apagaTudoButton = document.querySelector('#apaga-tudo');
-listaDeTarefas = document.querySelector('#lista-tarefas');
-tarefaInput = document.querySelector('#texto-tarefa');
-
-
-// $('#lista-tarefas').append(function () {
-//   return $(this).children().get().reverse()
-// });
-
 // ADICIONA UM NOVO ITEM
-$('#criar-tarefa').click(function () {
-  $('#lista-tarefas').append(`<li class="item-lista">${tarefaInput.value}</li>`);
-  tarefaInput.value = '';
+$("#criar-tarefa").click(function () {
+  if ($("#texto-tarefa").val() === "") {
+    alert("Você deve digitar algo para ser adicionado a lista!");
+  } else {
+    $("#lista-tarefas").append(
+      `<li class="item-lista">${$("#texto-tarefa").val()}</li>`
+    );
+    $("#texto-tarefa").val("");
+  }
+});
 
-  // $('.item-lista').on('dblclick', function () {
-  //   $(".item-lista").addClass('completed');
-  // });
+$("#lista-tarefas").on("click", "li", function (e) {
+  $(".selected").removeClass("selected");
+  $(e.target).addClass("selected");
 });
 
 // REMOVE OS FINALIZADOS
-$('#remover-finalizados').click(function () {
-  $('.completed').remove();
+$("#remover-finalizados").click(function () {
+  $(".completed").remove();
 });
 
 // TODO: SALVAR TAREFAS WEBSTORAGE
-
+$("#salvar-tarefas").click(function () {
+  for (let i = 0; i < $(".item-lista").length; i += 1) {
+    console.log($(".item-lista")[i]);
+  }
+});
 
 // REMOVE OS SELECIONADOS
-$('#remover-selecionado').click(function () {
-  $('.selected').remove();
+$("#remover-selecionado").click(function () {
+  $(".selected").remove();
 });
 
 // APAGA TUDO
-$('#apaga-tudo').click(function () {
-  $('#lista-tarefas').empty();
+$("#apaga-tudo").click(function () {
+  $("#lista-tarefas").empty();
+  $("#texto-tarefa").val("");
 });
