f818e5a9e9c22dd37c8b33c2b59d4bfd97ca2f3b
Israel Menon <zezomenon@gmail.com>
2020-09-23 12:07:36 -0300

    Fazendo requisito 9 para marcar tarefa concluida

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ btnCriarTarefa.addEventListener('click', function () {
 const itemListaBG = document.getElementById('lista-tarefas');
 itemListaBG.addEventListener('click', function (event) {
   if (event.target.tagName === 'LI') {
-    event.target.classList.toggle('selecionado');
+    event.target.classList = 'selected';
   }
   // source: https://www.w3schools.com/howto/howto_js_todolist.asp
+});
+itemListaBG.addEventListener('dblclick', function (event) {
+  if (event.target.tagName === 'LI' && event.target.classList.value !== 'completed' ) {
+    event.target.classList = 'completed';
+  } else {
+    event.target.classList = '';
+  }
 });
 
 const btnLimpaTudo = document.getElementById('apaga-tudo');
 const lista = document.getElementById('lista-tarefas');
 btnLimpaTudo.addEventListener('click', function () {
   lista.innerHTML = '';
-})
+});
