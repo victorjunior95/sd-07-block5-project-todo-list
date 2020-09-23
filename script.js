a9169d3020eaeae1431b2045b6d297cc89735df2
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-23 13:30:02 -0300

    commit 4 - to-do-list

--- script.js
+++ script.js
@@ -2,4 +2,39 @@ let botaoCriarTarefa = document.getElementById('criar-tarefa');
 let botaoApagaTudo = document.getElementById('apaga-tudo');
 let botaoRemoverFinalizados = document.getElementById('remover-finalizados');
 let botaoSalvarTarefas = document.getElementById('salvar-tarefas');
-let inputTarefa = document.getElementById('texto-tarefa')
+let inputTarefa = document.getElementById('texto-tarefa');
+let listaTarefas = document.getElementById('lista-tarefas');
+let textoInput = document.getElementById('texto-tarefa')
+
+
+function criarTarefa()
+{
+    
+    let topicoLista = document.createElement("li");
+    listaTarefas.appendChild(topicoLista);
+    listaTarefas.innerText = textoInput.value;
+    textoInput.value="";
+    conteudo.addEventListener("click", seleçãoTarefa);
+    conteudo.addEventListener("click", tarefaCompleta);
+    
+}
+
+function apagaTudo()
+{
+
+}
+
+function removerFinalizados()
+{
+
+}
+
+function salvarTarefas()
+{
+
+}
+
+function limpaTexto()
+{
+
+}
