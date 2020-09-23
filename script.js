f275d9fbfc0b328abf896ba988ac928b534e4227
menezesandre <dehhmenezes87@gmail.com>
2020-09-22 22:31:22 -0300

    click seleçao

--- script.js
+++ script.js
@@ -7,42 +7,47 @@ let remove = document.querySelector("#remover-finalizados");
 
 // Botão adiciona tarefa
 botaoAdicionar.addEventListener("click", function () {
-  let itemTarefa = document.createElement("li");
-  itemTarefa.innerText = campoTexto.value;
-  itemTarefa.className = "tarefa";
-  listaOrdenada.appendChild(itemTarefa);
-  campoTexto.value = "";
-  campoTexto.focus();
-  // Clique seleciona/desseleciona tarefa
-  itemTarefa.addEventListener("click", function () {
-    let selecionado = document.querySelector(".selecao");
-    if (selecionado !== null) {
-      selecionado.classList.remove("selecao");
-    }
-    itemTarefa.classList.add("selecao");
-    itemTarefa.addEventListener("click", function(){
-      itemTarefa.classList.remove("selecao");
-    })
-  });
-  
-  // Duplo clique tarefa completa
-  itemTarefa.addEventListener("dblclick", function () {
-    itemTarefa.classList.add("completed");
-    itemTarefa.classList.remove("selecao");
+      let itemTarefa = document.createElement("li");
+      itemTarefa.innerText = campoTexto.value;
+      itemTarefa.className = "tarefa";
+      listaOrdenada.appendChild(itemTarefa);
+      campoTexto.value = "";
+      campoTexto.focus();
 
-    // Botão remove tarefas completas
-    remove.addEventListener("click", function () {
-      let itensLista = document.querySelectorAll(".tarefa");
-      for (let indiceItensLista = 0; indiceItensLista < itensLista.length; indiceItensLista += 1) {
-        if (itensLista[indiceItensLista].classList.contains("completed")) {
-          listaOrdenada.removeChild(itensLista[indiceItensLista]);
+
+      // Adiciona e remove seleção
+      itemTarefa.addEventListener("click", function () {
+        let selecionado = document.querySelector(".selecao");
+        if (selecionado !== null) {
+          selecionado.classList.remove("selecao");
         }
-      }
+        itemTarefa.classList.add("selecao");
+        itemTarefa.addEventListener("click", function () {
+          itemTarefa.classList.remove("selecao");
+        })
+      });
     });
-  });
-});
 
-// Botão apaga lista de tarefas
-botaoApaga.addEventListener("click", function () {
-  listaOrdenada.innerHTML = "";
-});
+      // Tarefa completa
+      listaOrdenada.addEventListener('dblclick', function (event) {
+        if (event.target.classList.contains('completed')) {
+          event.target.classList.remove('completed');
+        } else {
+          event.target.classList.add('completed');
+        }
+      });
+
+      // Botão remove tarefas completas
+      remove.addEventListener("click", function () {
+        let itensLista = document.querySelectorAll(".tarefa");
+        for (let indiceItensLista = 0; indiceItensLista < itensLista.length; indiceItensLista += 1) {
+          if (itensLista[indiceItensLista].classList.contains("completed")) {
+            listaOrdenada.removeChild(itensLista[indiceItensLista]);
+          }
+        }
+      });
+
+      // Botão apaga lista de tarefas
+      botaoApaga.addEventListener("click", function () {
+        listaOrdenada.innerHTML = "";
+      });
