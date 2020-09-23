2be794b0c8aa279dc851383bc55bb519e381d252
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-23 13:01:45 -0300

    commit 3 - to-do-list

---
+++ script.js
@@ -0,0 +1,5 @@
+let botaoCriarTarefa = document.getElementById('criar-tarefa');
+let botaoApagaTudo = document.getElementById('apaga-tudo');
+let botaoRemoverFinalizados = document.getElementById('remover-finalizados');
+let botaoSalvarTarefas = document.getElementById('salvar-tarefas');
+let inputTarefa = document.getElementById('texto-tarefa')
