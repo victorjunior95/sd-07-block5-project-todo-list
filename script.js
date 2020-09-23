35f8cd814c04b7e3131dfa0722dffcd1b38b87a8
MarceloBorges <melombm@hotmail.com>
2020-09-23 12:07:14 -0500

    adicionar botão apagar

--- script.js
+++ script.js
@@ -28,9 +28,22 @@ taskList.addEventListener('click',function(event){
    }
 });
 
+// adicionar dublo click 
 taskList.addEventListener('dblclick', function(event){
 
     if(event.target.tagName == 'LI'){
         event.target.classList.toggle('completed');
+    }
+});
+
+// adiconar o botão de apagar
+
+let btApagaTudo = document.getElementById('apaga-tudo');
+let btRemoverFinalizados = document.getElementById('remover-finalizados');
+
+btApagaTudo.addEventListener('click', function(){
+    let newLine = document.querySelectorAll('li');
+    for(let index = 0; index < newLine.length;  index += 1){
+        newLine[index].remove();
     }
 });
