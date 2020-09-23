aac16c03bd45c5ac8e6d7a8d2c1b68b1708e43e1
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-09-23 17:21:13 -0300

    passando projeto final parte 1

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const textoTarefa = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 const tarefas = document.getElementsByTagName('li');
 const criarTarefa = document.getElementById('criar-tarefa');
+const deletarTudo = document.getElementById('apaga-tudo');
+const removerFinalizados = document.getElementById('remover-finalizados');
+const completed = document.getElementsByClassName('completed');
+const valInicial = 0;
 
 
 function addTarefa() {
@@ -11,6 +15,27 @@ function addTarefa() {
   this.style.backgroundColor = 'rgb(128, 128, 128';
 }
 
+function removerTudo() {
+  do {
+  listaTarefas.removeChild(tarefas[valInicial]);
+  }
+  while (valInicial < tarefas.length);
+  }
+  
+  function removerFinal() {
+  do {
+  listaTarefas.removeChild(completed[valInicial]);
+  }
+  while (valInicial < completed.length);
+  }
+  
+  function tarefaRealizada() {
+  if (this.className === '') {
+  this.className = 'completed';
+  } else {
+  this.className = '';
+  }
+  }
 
 function addLista() {
   const listLine = document.createElement('li');
@@ -22,3 +47,5 @@ function addLista() {
 }
 
 criarTarefa.addEventListener('click', addLista);
+deletarTudo.addEventListener('click', removerTudo);
+removerFinalizados.addEventListener('click', removerFinal);
