27e765532fbfde9ef1f374bc9c7fca7ca1e6a454
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-26 00:35:01 -0300

    commit 9 - to-do-list

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-window.onload = function() {
-    let botaoCriarTarefa = document.getElementById("criar-tarefa");
-    let botaoApagaTudo = document.getElementById("apaga-tudo");
-    let botaoRemoverFinalizados = document.getElementById("remover-finalizados");
-    let botaoSalvarTarefas = document.getElementById("salvar-tarefas");
-    let inputTarefa = document.getElementById("texto-tarefa");
-    let listaTarefas = document.getElementById("lista-tarefas");
-    let textoInput = document.getElementById("texto-tarefa");
-    let count = 0;
+let botaoCriarTarefa = document.getElementById("criar-tarefa");
+let botaoApagaTudo = document.getElementById("apaga-tudo");
+let botaoRemoverFinalizados = document.getElementById("remover-finalizados");
+let botaoSalvarTarefas = document.getElementById("salvar-tarefas");
+let inputTarefa = document.getElementById("texto-tarefa");
+let listaTarefas = document.getElementById("lista-tarefas");
+let textoInput = document.getElementById("texto-tarefa");
+let count = 0;
 
-    function criarTarefa() {
-        let tarefa = document.createElement("li");
-        tarefa.innerText = inputTarefa.value;
-        listaTarefas.appendChild(tarefa);
-        inputTarefa.value = "";
-        count++;
+function criarTarefa() {
+    let tarefa = document.createElement("li");
+    tarefa.innerText = inputTarefa.value;
+    listaTarefas.appendChild(tarefa);
+    inputTarefa.value = "";
+}
+
+function apagaTudo() {
+    let listaTarefasApagar = listaTarefas.querySelectorAll("li");
+    for (let index = 0; index < listaTarefasApagar.length; index++) {
+        listaTarefas.removeChild(listaTarefasApagar[index]);
     }
+}
 
-    function apagaTudo() {
-        let listaTarefasApagar = listaTarefas.querySelectorAll("li");
-        for (
-            let index = 0; index < listaTarefasApagar.length; index++
-        ) {
-            listaTarefas.removeChild(listaTarefas.querySelectorAll("li")[index]);
-        }
-        count = 0;
-    }
+function selecaoTarefa() {}
 
-    function selecaoTarefa() {}
+function removerFinalizados() {}
 
-    function removerFinalizados() {}
-
-    function salvarTarefas() {}
-};
+function salvarTarefas() {}
