b4a8f6fb1064c7a4782ca607729214f31562b634
Breenolf <blf@ic.ufal.br>
2020-09-30 01:10:27 -0300

    requisitos 08 e 09

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ document.getElementById('criar-tarefa').addEventListener('click', () => {
   lista.appendChild(itemLista).innerText = tarefa;
   caixaTexto.value = '';
 })
+
+document.getElementById('lista-tarefas').addEventListener('click', (event) => {
+  const tarefas = document.getElementsByTagName('li');
+  for (let i = 0; i < tarefas.length; i += 1) {
+      tarefas[i].style.backgroundColor = '';
+  }
+  const tarefaSelecionada = event.target;
+  tarefaSelecionada.style.backgroundColor = 'rgb(128, 128, 128)'
+})
