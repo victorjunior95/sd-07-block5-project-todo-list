7f8cea66c9215ed8733fb57053c7ace01e46b10f
vicoliveir <victorhugoqxd@gmail.com>
2020-10-05 12:59:27 -0300

    requeriments css selected

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ let inputTask = document.getElementById("texto-tarefa");
 let olList = document.getElementById("lista-tarefas");
 let buttonAdd = document.getElementById("criar-tarefa");
 
+
 buttonAdd.addEventListener("click", function() {
     let todoList = document.createElement("li");
     todoList.innerText = inputTask.value;
     olList.appendChild(todoList);
     inputTask.value = "";
+});
+
+olList.addEventListener("click", function(event) {
+    if (event.target.tagName == "LI") {
+        let selectedItem = document.querySelector(".selected");
+        if (selectedItem !== null) {
+            selectedItem.classList.remove("selected");    
+        }
+    event.target.classList.add("selected");
+    }
 });
