42202a1017ff703f60da655d30eb736bc130d92e
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-10-09 14:26:15 -0300

    Envio atv bonus remover selecionado

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const textoTarefa = document.getElementById('texto-tarefa');
-const listaTarefas = document.getElementById('lista-tarefas');
+const listaTarefas = document.querySelector('#lista-tarefas');
 const criarTarefa = document.getElementById('criar-tarefa');
 const deletarTudo = document.getElementById('apaga-tudo');
 const removerFinalizados = document.getElementById('remover-finalizados');
 const salvarTarefa = document.getElementById('salvar-tarefas');
-const removerSelecionado = document.getElementById('remover-selecionato');
+const removerSelecionado = document.getElementById('remover-selecionado');
 const moverCima = document.getElementById('mover-cima');
 const moverBaixo = document.getElementById('mover-baixo');
 
@@ -15,7 +15,7 @@ criarTarefa.addEventListener('click', () => {
   textoTarefa.value = '';
 });
 
-listaTarefas.addEventListener('click', function (event) {
+listaTarefas.addEventListener('click', (event) => {
   if (event.target !== listaTarefas) {
     const clearSelecionado = () => {
       const items = document.querySelectorAll('#lista-tarefas li');
@@ -29,7 +29,7 @@ listaTarefas.addEventListener('click', function (event) {
   }
 });
 
-listaTarefas.addEventListener('dblclick', function (event) {
+listaTarefas.addEventListener('dblclick', (event) => {
   if (event.target !== listaTarefas) {
     const addCompleted = (target) => target.classList.toggle('completed');
     addCompleted(event.target);
@@ -37,3 +37,17 @@ listaTarefas.addEventListener('dblclick', function (event) {
 });
 
 deletarTudo.addEventListener('click', () => listaTarefas.innerHTML = '');
+
+removerFinalizados.addEventListener('click', () => {
+  const selectedCompleted = listaTarefas.querySelectorAll('.completed');
+  for (let index = 0; index < selectedCompleted.length; index += 1) {
+    listaTarefas.removeChild(selectedCompleted[index]);
+  }
+});
+
+removerSelecionado.addEventListener('click', () => {
+  const itemSelecionado = listaTarefas.querySelectorAll('.selecionado');
+  for (let index = 0; index < itemSelecionado.length; index += 1) {
+    listaTarefas.removeChild(itemSelecionado[index]);
+  }
+});
