029ebfc567f801be1d182b857e864522cc6ba559
Geziel <geziel.fernandez@pucp.pe>
2020-10-15 01:56:06 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttonAdd = document.getElementById('criar-tarefa');
 const textTask = document.getElementById('texto-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
+const delButton = document.getElementById('apaga-tudo');
+const finalized = document.getElementById('remover-finalizados');
 
 function addingTask() {
   if (textTask.value !== '') {
@@ -27,4 +29,25 @@ listTasks.addEventListener('dblclick', (event) => {
   } else {
     event.target.classList.add('completed');
   }
+});
+
+// Delete all button
+delButton.addEventListener('click', () => listTasks.innerHTML = '');
+
+// Remover Finalizados
+finalized.addEventListener('click', () => {
+  const completed = document.querySelectorAll('.completed');
+  completed.forEach((item) => {
+    listTasks.removeChild(item);
+  });
+});
+
+
+// Remover Selecionados
+const removeButton = document.getElementById('remover-selecionado');
+
+removeButton.addEventListener('click', () => {
+  const selectedItem = document.querySelector('.selected');
+  if (!selectedItem) return;
+  listTasks.removeChild(selectedItem);
 });
