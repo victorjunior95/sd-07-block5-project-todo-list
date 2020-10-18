29d5f532cb6c8fd4debf0d19e1c8fa2d02c23d55
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-10-18 15:56:17 -0300

    commit 11

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let botaoCriarTarefa = document.getElementById("criar-tarefa");
-let botaoApagaTudo = document.getElementById("apaga-tudo");
+let botaoApagaTudo = document.getElementById("apaga-tudo"); 
 let botaoRemoverFinalizados = document.getElementById("remover-finalizados");
 let botaoSalvarTarefas = document.getElementById("salvar-tarefas");
 let inputTarefa = document.getElementById("texto-tarefa");
