98124dffaa43f52b65c03f60b68c6851d147c1f0
wellington <profwellington17@gmail.com>
2020-10-24 14:01:02 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('oi como vai vc?');
+const btn = document.querySelector("#criar-tarefa");
+btn.addEventListener("click", function () {
+
+    let inputItem = document.querySelector("#texto-tarefa").value;
+    createLi(inputItem);
+    document.querySelector("#texto-tarefa").value = "";
+       
+});
+
+function createLi(inputValue){
+    const ol = document.querySelector("#lista-tarefas");
+    const li = document.createElement("li")
+    li.innerText = inputValue;
+    ol.appendChild(li);
+
+}
